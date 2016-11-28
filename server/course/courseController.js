b69247afc546f15572b53101b717080db529da56
Matej Salkovic <matej@extensionengine.com>
2016-11-28 17:16:31 +0100

    Implement course controller with CRUD operations

---
+++ server/course/courseController.js
@@ -0,0 +1,58 @@
+'use strict';
+const courseModel = require('./courseModel').model;
+
+class CourseController {
+  constructor(model) {
+    this.model = model;
+    this.create = this.create.bind(this);
+    this.get = this.get.bind(this);
+    this.getAll = this.getAll.bind(this);
+    this.update = this.update.bind(this);
+    this.replace = this.replace.bind(this);
+    this.remove = this.remove.bind(this);
+  }
+
+  create(req, res, next) {
+    this.model.create(req.body)
+      .then(course => res.status(201).json({ data: course }))
+      .catch(next);
+  }
+
+  get(req, res, next) {
+    this.model.get(req.params.courseKey)
+      .then(course => res.status(200).json({ data: course }))
+      .catch(next);
+  }
+
+  getAll(req, res, next) {
+    this.model.getAll()
+      .then(courses => res.status(200).json({ data: courses || [] }))
+      .catch(next);
+  }
+
+  update(req, res, next) {
+    this.model
+      .update(req.params.courseKey, req.body)
+      .then(course => res.status(200).json({ data: course }))
+      .catch(next);
+  }
+
+  replace(req, res, next) {
+    this.model
+      .replace(req.params.courseKey, req.body)
+      .then(course => res.status(200).json({ data: course }))
+      .catch(next);
+  }
+
+  remove(req, res, next) {
+    this.model
+      .remove(req.params.courseKey)
+      .then(course => res.status(200).json({ data: course }))
+      .catch(next);
+  }
+}
+
+module.exports = {
+  Controller: CourseController,
+  controller: new CourseController(courseModel)
+};
