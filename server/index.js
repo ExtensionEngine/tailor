beeb1fd8be3fe4167bcb1472eb5e42349b1bb0e0
Matej Salkovic <matej@extensionengine.com>
2016-11-28 11:58:55 +0100

    Connect to ArangoDB and launch the Express app

--- server/index.js
+++ server/index.js
@@ -1,13 +1,27 @@
 'use strict';
+const arangojs = require('arangojs');
 const app = require('./app');
+const logger = require('./logger');
 
 const serverPort = process.env.SERVER_PORT;
-app.listen(serverPort, err => {
-  if (err) {
-    console.log(err);
-    process.exit(1);
-    return;
-  }
+const dbUri = process.env.ARANGODB_URI;
+const dbName = process.env.DB_NAME;
 
-  console.log(`Server listening on port ${serverPort}`);
-});
+function connectToDb() {
+  const db = arangojs(dbUri);
+  return db.listUserDatabases()
+    .then(names => names.includes(dbName) ? null : db.createDatabase(dbName))
+    .then(result => db.useDatabase(dbName));
+}
+
+function runApp() {
+  return new Promise((resolve, reject) => {
+    app.listen(serverPort, err => err ? reject(err) : resolve());
+  });
+}
+
+connectToDb()
+.then(() => logger.info(`Connected to database ${dbName}`))
+.then(runApp)
+.then(() => logger.info(`Server listening on port ${serverPort}`))
+.catch(err => logger.error({ err }));
