856829dcb2d91af1c82b2e00a8a76414a367ea9a
Matej Salkovic <matej@extensionengine.com>
2016-11-28 10:46:46 +0100

    Remove server/src directory

---
+++ server/index.js
@@ -0,0 +1,13 @@
+'use strict'
+const app = require('./app')
+
+const serverPort = process.env.SERVER_PORT
+app.listen(serverPort, err => {
+  if (err) {
+    console.log(err)
+    process.exit(1)
+    return
+  }
+
+  console.log(`Server listening on port ${serverPort}`)
+})
