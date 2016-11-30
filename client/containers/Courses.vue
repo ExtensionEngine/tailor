7deaf09cae1c9d8373ff0346174abd47c6855c2a
Marko Vrbanec <marko.vrbanec@protonmail.com>
2016-11-30 14:30:12 +0100

    Create CoursesSearch component and add it to Courses layout

--- client/containers/Courses.vue
+++ client/containers/Courses.vue
@@ -1,15 +1,27 @@
 <template>
-  <div>
-    Courses Placeholder
+  <div class="courses">
+    <div class="row">
+      <div class="col-md-6 col-md-offset-3">
+        <courses-search></courses-search>
+      </div>
+    </div>
   </div>
 </template>
 
 <script>
+  import CoursesSearch from '../components/courses/Search';
+
   export default {
-    name: 'courses'
+    name: 'courses',
+
+    components: {
+      CoursesSearch
+    }
   };
 </script>
 
 <style lang="scss">
-
+  .courses {
+    padding-top: 40px;
+  }
 </style>
