9ff5a600b3c506ca1477d0f4753564d6d4f12b75
underscope <underscope@gmail.com>
2016-12-01 23:23:57 +0100

    Made activities collapsable

--- client/components/course/Activity.vue
+++ client/components/course/Activity.vue
@@ -1,16 +1,23 @@
 <template>
   <div>
-    <div class="activity" v-if="name">
+    <div class="activity"
+      v-if="name"
+      @click="collapsed =! collapsed">
       <div>{{ name }}</div>
     </div>
-    <draggable :activities="activities" @start="dragging=true" @end="dragging=false">
-      <activity
-        v-bind:class="{ 'sub-activity': name }"
-        v-for="it in activities"
-        :name="it.name"
-        :activities="it.activities">
-      </activity>
-    </draggable>
+    <transition name="fade">
+      <draggable
+        v-if="!collapsed"
+        @start="dragging = true"
+        @end="dragging = false">
+        <activity
+          v-bind:class="{ 'sub-activity': name }"
+          v-for="it in activities"
+          :name="it.name"
+          :activities="it.activities">
+        </activity>
+      </draggable>
+    </transition>
   </div>
 </template>
 
@@ -20,6 +27,11 @@ import Draggable from 'vuedraggable';
 export default {
   name: 'activity',
   props: ['name', 'activities'],
+  data: function () {
+    return {
+      collapsed: false
+    };
+  },
   components: {
     Draggable
   }
@@ -27,18 +39,26 @@ export default {
 </script>
 
 <style lang="scss">
-  // TODO: Do proper styling
-  .activity {
-    margin: 10px 200px;
-    padding: 5px;
-    font-size: 22px;
-    color: red;
-    text-align: left;
-    background-color: #ccc;
-    cursor: pointer;
-  }
+// TODO: Do proper styling
+.activity {
+  margin: 10px 200px;
+  padding: 5px;
+  font-size: 22px;
+  color: red;
+  text-align: left;
+  background-color: #ccc;
+  cursor: pointer;
+}
 
-  .sub-activity {
-    margin-left: 50px;
-  }
+.sub-activity {
+  margin-left: 50px;
+}
+
+.fade-enter-active, .fade-leave-active {
+  transition: opacity .2s
+}
+
+.fade-enter, .fade-leave-active {
+  opacity: 0
+}
 </style>
