6d56afee7127d71c3c5f20784c551796acab7803
underscope <underscope@gmail.com>
2016-12-01 10:50:19 +0100

    Added outline component

--- client/components/course/Container.vue
+++ client/components/course/Container.vue
@@ -1,13 +1,21 @@
 <template>
-  <div>{{ name }}</div>
+  <div>
+    {{ name }}
+    <outline></outline>
+  </div>
 </template>
 
 <script>
+import Outline from './Outline.vue';
+
 export default {
   data: function () {
     return {
       name: 'Computer Science'
     }
+  },
+  components: {
+    Outline
   }
 };
 </script>
---
+++ client/components/course/Outline.vue
@@ -0,0 +1,17 @@
+<template>
+  <div>Total: {{ activities.length }}</div>
+</template>
+
+<script>
+export default {
+  data: function () {
+    return {
+      activities: []
+    };
+  }
+};
+</script>
+
+<style lang="scss">
+
+</style>
