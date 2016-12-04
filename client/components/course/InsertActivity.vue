fafbbd5f9c309d1719001074d2e9c47d50675eaf
underscope <underscope@gmail.com>
2016-12-04 11:02:50 +0100

    Added activity input

--- client/components/course/InsertActivity.vue
+++ client/components/course/InsertActivity.vue
@@ -1,7 +1,26 @@
 <template>
-  <div class="divider-wrapper">
-    <div class="divider">
-      <div class="action"><span class="fa fa-plus"></span></div>
+  <div>
+    <div class="divider-wrapper" v-if="!input" @click="show">
+      <div class="divider">
+        <div class="action"><span class="fa fa-plus"></span></div>
+      </div>
+    </div>
+    <div class="activity-input" v-if="input">
+      <div class="row">
+        <div class="col-lg-8">
+          <input type="text" class="form-control" placeholder="Activity name">
+        </div>
+        <div class="col-lg-2">
+          <select class="form-control">
+            <option>Section</option>
+            <option>Subsection</option>
+          </select>
+        </div>
+        <div class="col-lg-2">
+          <button class="btn btn-default">Add</button>
+          <button class="btn btn-default" @click.stop="hide">X</button>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -9,11 +28,30 @@
 <script>
 export default {
   data: function () {
-
+    return {
+      input: false
+    };
+  },
+  methods: {
+    show: function () {
+      this.input = true;
+    },
+    hide: function () {
+      this.input = false;
+    },
+    add: function () {
+      // TODO: Dispatch action
+    }
   }
 };
 </script>
 
 <style lang="scss">
+.activity-input {
+  padding: 20px 5px;
 
+  input {
+    background-color: #e0e0e0;
+  }
+}
 </style>
