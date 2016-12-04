b4f8c0e9b43dc491d65aa458dbd0ca78bb4694dc
Marko Vrbanec <marko.vrbanec@protonmail.com>
2016-12-04 16:19:21 +0100

    Fetch course store entries

--- client/store/modules/courses/getters.js
+++ client/store/modules/courses/getters.js
@@ -1,8 +1,10 @@
+const getCourse = state => state.course;
 const getCourses = state => state.courses;
 const getCreateStatus = state => state.createCourse;
 const getFetchStatus = state => state.fetchCourses;
 
 export default {
+  getCourse,
   getCourses,
   getCreateStatus,
   getFetchStatus
