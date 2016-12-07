8f1fb63e505b8afc22193ef78a298087c5d36066
Matej Salkovic <matej@extensionengine.com>
2016-12-07 17:11:25 +0100

    Implement transactional activity insert

--- server/activity/activity.model.js
+++ server/activity/activity.model.js
@@ -21,13 +21,45 @@ class ActivityModel extends BaseModel {
   }
 
   create(activity) {
+    const action = function({ doc }) {
+      const arango = require('@arangodb');
+      const db = arango.db;
+      const aql = arango.aql;
+
+      if (doc.parentKey && !db.activity.exists(doc.parentKey)) throw new Error(
+        `Parent activity (${doc.parentKey}) does not exist`);
+
+      const countSiblings = aql`RETURN LENGTH(FOR act IN activity
+        FILTER act.courseKey == ${doc.courseKey} AND
+          act.parentKey == ${doc.parentKey}
+        RETURN act)`;
+      const numSiblings = db._query(countSiblings).next();
+
+      if (doc.position === undefined ||
+          doc.position === null ||
+          doc.position >= numSiblings) {
+        doc.position = numSiblings;
+        // This does not return the entire object!
+        return db.activity.save(doc);
+      }
+
+      if (doc.position < 0) doc.position = 0;
+
+      const updateExisting = aql`FOR act IN activity
+        FILTER act.courseKey == ${doc.courseKey} AND
+          act.parentKey == ${doc.parentKey} AND
+          act.position >= ${doc.position}
+        UPDATE act WITH { position: act.position + 1 } IN activity`;
+      db._query(updateExisting);
+      return db.activity.save(doc);
+    };
+
     return this
       .validate(activity)
-      .then(validActivity => this.db.query(query.INSERT_ACTIVITY, {
-        '@collection': this.collectionName,
-        activity: validActivity
-      }))
-      .then(cursor => cursor.next());
+      .then(validActivity => this.db.transaction(
+        { read: 'activity', write: 'activity' },
+        String(action),
+        { doc: activity }));
   }
 }
 
