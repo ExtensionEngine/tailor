775aecee5a15b5f59fcba15e160b7a1517f730c0
underscope <underscope@gmail.com>
2016-12-12 12:07:52 +0100

    Improved navbar
    - Fixed on top
    - Added CIMA logo
    - Added tool name
    - Cleanup

--- client/components/common/Navbar.vue
+++ client/components/common/Navbar.vue
@@ -1,78 +1,65 @@
 <template>
-  <nav class="navbar navbar-default">
+  <nav class="navbar navbar-default navbar-fixed-top">
     <div class="container-fluid">
       <div class="navbar-header">
-        <button type="button" class="navbar-toggle collapsed" data-toggle="collapse" data-target="#bs-example-navbar-collapse-1" aria-expanded="false">
-          <span class="sr-only">Toggle navigation</span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-          <span class="icon-bar"></span>
-        </button>
         <router-link :to="{ name: 'catalog' }" class="navbar-brand">
           <img src="../../assets/img/logo.png" alt="Logo" />
+          <span>CGMA Author</span>
         </router-link>
       </div>
-
-      <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
-        <div v-if="course" class="course-title">
-          {{course.title}}
-        </div>
-
-        <ul class="nav navbar-nav navbar-right">
-          <li v-if="!loggedIn"><router-link :to="{ name: 'login' }">Login</router-link></li>
-          <li v-if="loggedIn"><a href="#">Logout</a></li>
-        </ul>
-      </div>
+      <div v-if="course" class="course-title">{{course.title}}</div>
+      <ul class="nav navbar-nav navbar-right">
+        <li v-if="loggedIn"><a href="#">Logout</a></li>
+      </ul>
     </div>
   </nav>
 </template>
 
 <script>
-  import { mapGetters } from 'vuex';
+import { mapGetters } from 'vuex';
 
-  export default {
-    name: 'navbar',
-
-    computed: {
-      ...mapGetters({
-        request: 'loginUserRequest',
-        success: 'loginUserSuccess',
-        course: 'getCourse'
-      }),
-      loggedIn() {
-        return this.success && !this.request;
-      }
+export default {
+  name: 'navbar',
+  computed: {
+    ...mapGetters({
+      request: 'loginUserRequest',
+      success: 'loginUserSuccess',
+      course: 'getCourse'
+    }),
+    loggedIn() {
+      return this.success && !this.request;
     }
-  };
+  }
+};
 </script>
 
 <style lang="scss">
-  .navbar {
-    background-color: #f5f5f5;
-    border-radius: 0;
-    color: rgba(0, 0, 0, .87);
-    font-weight: 500;
-    margin: 0;
-    z-index: 1000;
+.navbar {
+  color: #555;
+  font-weight: 500;
+  background-color: white;
 
-    .navbar-brand {
-      color: inherit;
-      padding: 10px 20px;
-      width: 80px;
-    }
+  .navbar-brand {
+    width: 250px;
+    padding: 9px 15px;
 
-    .navbar-right {
-      > li > a {
-        color: inherit;
-      }
-    }
-
-    .course-title {
-      display: inline-block;
+    img {
       float: left;
-      font-size: 22px;
-      font-weight: 600;
-      padding-top: 12px;
+      width: 72px;
+    }
+
+    span {
+      display: inline-block;
+      line-height: 32px;
+      font-size: 18px;
     }
   }
+
+  .course-title {
+    float: left;
+    padding-left: 10px;
+    font-size: 15px;
+    line-height: 50px;
+  }
+}
 </style>
