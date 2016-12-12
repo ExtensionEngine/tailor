36dd18b5082f7a3ddc85e9cd6cf746d5194f3f28
underscope <underscope@gmail.com>
2016-12-12 08:14:14 +0100

    Remove unused container

--- client/router.js
+++ client/router.js
@@ -2,7 +2,6 @@ import Vue from 'vue';
 import Router from 'vue-router';
 
 import Auth from './containers/Auth';
-import Home from './containers/Home';
 import Course from './components/course/Container';
 import Courses from './containers/Courses';
 import CourseEditor from './containers/CourseEditor';
