a307f9d3ac560c15d344b3c710e4ac542da97be6
Matej Salkovic <matej@extensionengine.com>
2016-12-19 17:26:41 +0100

    Add JSDoc comments to session store

--- server/session/store.js
+++ server/session/store.js
@@ -8,15 +8,29 @@ const query = require('./query');
 const db = database.db;
 const SESSION_COLLECTION = database.collection.SESSION;
 
-// https://ewiggin.gitbooks.io/expressjs-middleware/content/express-session.html
-
+/**
+ * ExpressJS session store backed by ArangoDB. Implements the minimum required
+ * interface described in the docs:
+ * {@link https://ewiggin.gitbooks.io/expressjs-middleware/content/express-session.html#session-store-implementation}
+ */
 class ArangoSessionStore extends Store {
+  /**
+   * Create the store instance.
+   * @param {object} db - JS interface/connection to ArangoDB, as received
+   * from {@link DatabaseConnector}
+   * @param {string} collectionName - Name of the collection which will store
+   * the session data.
+   */
   constructor(db, collectionName = SESSION_COLLECTION) {
     super();
     this.db = db;
     this.collectionName = collectionName;
   }
 
+  /**
+   * Get the session data from database.
+   * @param {string} sid - Session ID.
+   */
   get(sid, callback) {
     this.db
       .query(query.GET_SESSION_BY_KEY, {
@@ -28,6 +42,12 @@ class ArangoSessionStore extends Store {
       .catch(callback);
   }
 
+  /**
+   * Save the session data to database.
+   * @param {string} sid - Session ID.
+   * @param {object} session - Arbitrary session data in JSON format.
+   * @param {function} [callback=noop] - Callback called when session is saved.
+   */
   set(sid, session, callback = noop) {
     this.db
       .query(query.UPSERT_SESSION_BY_KEY, {
@@ -40,6 +60,11 @@ class ArangoSessionStore extends Store {
       .catch(callback);
   }
 
+  /**
+   * Remove the session from database.
+   * @param {string} sid - Session ID.
+   * @param {function} [callback=noop] - Called when session is destroyed.
+   */
   destroy(sid, callback = noop) {
     this.db
       .query(query.REMOVE_SESSION_BY_KEY, {
