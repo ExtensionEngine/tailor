29587eb309e7053b19082c64737023dcfd5fe4ec
Matej Salkovic <matej@extensionengine.com>
2016-12-19 11:48:58 +0100

    Test user-related middleware

--- server/user/middleware/_test_/loadUser.spec.js
+++ server/user/middleware/_test_/loadUser.spec.js
@@ -2,48 +2,48 @@
 
 const chai = require('chai');
 const sinon = require('sinon');
-const loadCourse = require('../loadCourse');
+const loadUser = require('../loadUser');
 const { locals } = require('../../../shared/io');
 
 sinon.assert.expose(chai.assert, { prefix: '' });
 const assert = chai.assert;
 
-describe('Course middleware', () => {
+describe('User middleware', () => {
   function getMocks() {
     const req = {
       params: {
-        courseKey: 'some-course-key'
+        userKey: 'some-user-key'
       }
     };
     const res = {};
     return { req, res };
   }
 
-  describe('loadCourse', () => {
-    it('loads course by key, saves it to req, calls next', done => {
+  describe('loadUser', () => {
+    it('loads user by key, saves it to req, calls next', done => {
       const { req, res } = getMocks();
-      const course = {
-        name: 'important course'
+      const user = {
+        email: 'user@email.com'
       };
       const model = {
-        getByKey: sinon.stub().returns(Promise.resolve(course))
+        getByKey: sinon.stub().returns(Promise.resolve(user))
       };
 
-      loadCourse(model)(req, res, err => {
+      loadUser(model)(req, res, err => {
         assert.isUndefined(err);
-        assert.deepEqual(locals.load(req, 'course'), course);
+        assert.deepEqual(locals.load(req, 'user'), user);
         done();
       });
     });
 
-    it('calls next with error from course model', done => {
+    it('calls next with error from user model', done => {
       const { req, res } = getMocks();
-      const error = new Error('course model error');
+      const error = new Error('user model error');
       const model = {
         getByKey: sinon.stub().returns(Promise.reject(error))
       };
 
-      loadCourse(model)(req, res, err => {
+      loadUser(model)(req, res, err => {
         assert.deepEqual(err, error);
         done();
       });
