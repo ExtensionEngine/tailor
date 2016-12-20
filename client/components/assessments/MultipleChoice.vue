b5ca918d911773c14788f92e93c5cb300e2ffcdb
Nediljko Krstic <nediljko@extensionengine.com>
2016-12-20 14:16:39 +0100

    Multiple choice component - add changes to html - relocated common css classes to parent files - changed data received with props - changed save function

--- client/components/assessments/MultipleChoice.vue
+++ client/components/assessments/MultipleChoice.vue
@@ -1,104 +1,84 @@
 <template>
   <div class="assessment multiple-choice">
-    <div class="row">
-      <div class="col-lg-12 intro">
-        <div class="label label-primary title">Multiple choice</div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12 ">
-        <div class="form-group">
-          <span class="title-sub">Question</span>
-          <span :class="{'has-error': errors.includes('question')}">
-            <input
-              class="form-control"
-              type="text"
-              :disabled="isDisabled"
-              v-model="question"
-              placeholder="Question..">
-          </span>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="form-group">
-          <span class="title-sub">Answers</span>
-          <button
-            class="btn btn-default answers-add"
-            type="button"
-            :disabled="isDisabled"
-            @click="addAnswer">
-            <span class="fa fa-plus"></span>
-          </button>
-          <ul>
-            <li v-for="(answer, index) in answers">
-              <span
-                  class="answers-checkbox"
-                  :class="{'error': errors.includes('correctAnswer')}">
-                <input
-                  type="checkbox"
-                  :disabled="isDisabled"
-                  v-model="correctAnswer"
-                  :value="index + 1">
-              </span>
-              <span
-                class="answers-input"
-                :class="{'has-error': errors.includes(`answers[${index}]`)}">
-                <input
-                  type="text"
-                  :disabled="isDisabled"
-                  v-model="answers[index]"
-                  placeholder="Answer..">
-              </span>
-              <button
-                class="destroy"
-                type="button"
-                :disabled="isDisabled"
-                @click="removeAnswer(index)">
-                <span class="fa fa-times"></span>
-              </button>
-            </li>
-          </ul>
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12">
-        <div class="form-group">
-          <span class="title-sub">Hint</span>
-          <input
-            class="form-control"
-            type="text"
-            :disabled="isDisabled"
-            v-model="hint"
-            placeholder="Optional hint..">
-        </div>
-      </div>
-    </div>
-    <div class="row">
-      <div class="col-lg-12 final">
-        <div class="alert-container">
-          <div
-            class="alert alert-dismissible alert-danger"
-            v-show="answers.length < 3">
-            <strong>Please make at least three answers available !</strong>
-          </div>
-          <div
-            class="alert alert-dismissible alert-success"
-            v-show="isSuccess">
-            <strong>Question successfully saved !</strong>
-          </div>
-        </div>
-        <button
-          class="btn btn-default"
-          type="button"
+    <div class="label label-primary assessment-type">Multiple choice</div>
+    <div class="form-group">
+      <span class="form-label">Question</span>
+      <span :class="{'has-error': errors.includes('question')}">
+        <input
+          v-model="question"
           :disabled="isDisabled"
-          @click="save">
-          Save Question
-        </button>
+          class="form-control"
+          type="text"
+          placeholder="Question..">
+      </span>
+    </div>
+    <div class="form-group">
+      <span class="form-label">Answers</span>
+      <button
+        :disabled="isDisabled"
+        @click="addAnswer"
+        class="btn btn-default answers-add"
+        type="button">
+        <span class="fa fa-plus"></span>
+      </button>
+      <ul>
+        <li v-for="(answer, index) in answers">
+          <span
+              class="answers-checkbox"
+              :class="{'error': errors.includes('correct')}">
+            <input
+              v-model="correctAnswers"
+              :value="index"
+              :disabled="isDisabled"
+              type="checkbox">
+          </span>
+          <span
+            class="answers-input"
+            :class="{'has-error': errors.includes(`answers[${index}]`)}">
+            <input
+              v-model="answers[index]"
+              :disabled="isDisabled"
+              type="text"
+              placeholder="Answer..">
+          </span>
+          <button
+            :disabled="isDisabled"
+            @click="removeAnswer(index)"
+            class="destroy"
+            type="button">
+            <span class="fa fa-times"></span>
+          </button>
+        </li>
+      </ul>
+    </div>
+    <div class="form-group">
+      <span class="form-label">Hint</span>
+      <input
+        v-model="hint"
+        :disabled="isDisabled"
+        class="form-control"
+        type="text"
+        placeholder="Optional hint..">
+    </div>
+    <div class="alert-container">
+      <div
+        v-show="answers.length < 3"
+        class="alert alert-dismissible alert-danger">
+        <strong>Please make at least three answers available !</strong>
+      </div>
+      <div
+        v-show="isSuccess"
+        class="alert alert-dismissible alert-success">
+        <strong>Question successfully saved !</strong>
       </div>
     </div>
+    <button
+      :disabled="isDisabled"
+      @click="save"
+      class="btn btn-default"
+      type="button">
+      Save Question
+    </button>
   </div>
 </template>
 
@@ -108,7 +88,7 @@ import yup from 'yup';
 const schema = yup.object().shape({
   question: yup.string().trim().min(1).required(),
   answers: yup.array().min(3).of(yup.string().trim().min(1)).required(),
-  correctAnswer: yup.array().min(2).of(yup.number().min(1)).required()
+  correct: yup.array().min(2).of(yup.number()).required()
 });
 
 export default {
@@ -119,11 +99,15 @@ export default {
     },
     'propAnswers': {
       type: Array,
-      default: ['', '', '']
+      default: function () {
+        return ['', '', ''];
+      }
     },
     'propCorrect': {
       type: Array,
-      default: []
+      default: function () {
+        return [];
+      }
     },
     'propHint': {
       type: String,
@@ -133,8 +117,9 @@ export default {
   data() {
     return {
       question: this.propQuestion,
-      answers: this.propAnswers,
-      correctAnswer: this.propCorrect,
+      // slice array to avoid changing data in parent component
+      answers: this.propAnswers.slice(0),
+      correctAnswers: this.propCorrect,
       hint: this.propHint,
       isSuccess: false,
       isDisabled: false,
@@ -147,45 +132,39 @@ export default {
     },
     removeAnswer(index) {
       this.answers.splice(index, 1);
-      if (this.correctAnswer.indexOf(index + 1) !== -1) {
-        this.correctAnswer.splice(this.correctAnswer.indexOf(index + 1), 1);
+
+      if (this.correctAnswers.indexOf(index) !== -1) {
+        this.correctAnswers.splice(this.correctAnswers.indexOf(index), 1);
       }
     },
     save() {
-      this.validate()
+      let question = {
+        question: this.question,
+        correct: this.correctAnswers,
+        answers: this.answers,
+        hint: this.hint
+      };
+
+      this.validate(question)
         .then(() => {
           this.errors = [];
           this.isSuccess = true;
-
-          let question = {
-            question: this.question,
-            correct: this.correctAnswer,
-            answers: this.answers,
-            hint: this.hint
-          };
-
           this.isDisabled = true;
+
           this.$emit('addQuestion', question);
-          return;
         })
         .catch((err) => {
           this.errors = [];
-          for (let i = 0; i < err.inner.length; i++) {
-            this.errors.push(err.inner[i].path);
-          }
+
+          err.inner.forEach((item) => {
+            this.errors.push(item.path);
+          });
         });
     },
-    validate() {
+    validate(question) {
       return schema.validate(
-        {
-          question: this.question,
-          answers: this.answers,
-          correctAnswer: this.correctAnswer
-        },
-        {
-          recursive: true,
-          abortEarly: false
-        }
+        question,
+        { recursive: true, abortEarly: false }
       );
     }
   }
@@ -199,26 +178,10 @@ export default {
   margin: 10px auto;
   padding: 10px 30px 30px 30px;
   box-shadow: 0 2px 9px rgba(0, 0, 0, 0.74);
-
-  .row {
-    margin: 0;
-  }
-
-  [class*="col-"] {
-    padding: 25px 10px 15px 10px;
-  }
-
-  .error {
-    border-bottom: none;
-    box-shadow: inset 0 -2px 0 #e51c23;
-  }
-
-  .btn[disabled] {
-    box-shadow: 1px 1px 4px rgba(0, 0, 0, 0.4);;
-  }
+  overflow: hidden;
 
   .alert-container {
-    padding: 0 10px;
+    padding: 0 20px;
   }
 
   .alert {
@@ -228,14 +191,9 @@ export default {
     text-align: center;
   }
 
-  .final {
-    padding: 10px 10px 15px 10px;
-    overflow: hidden;
-
-    button {
-      margin: 15px 10px 0 0;
-      float: right;
-    }
+  button {
+    margin: 15px 10px 0 0;
+    float: right;
   }
 
   .intro {
@@ -243,13 +201,14 @@ export default {
     padding: 15px 10px 45px 10px;
   }
 
-  .title {
+  .assessment-type {
     font-size: 13px;
     float: right;
     background-color: grey;
+    margin: 15px 15px 50px 0;
   }
 
-  .title-sub {
+  .form-label {
     font-size: 20px;
   }
 
@@ -327,12 +286,9 @@ export default {
   .form-group {
     text-align: left;
     margin: 0 auto;
-    padding: 0 10px;
+    padding: 25px 20px 15px 20px;
     width: 100%;
-  }
-
-  .form-control {
-    width: 100%;
+    overflow: hidden;
   }
 
   .form-control,
