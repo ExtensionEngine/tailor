a3495e0ee3e581ac6249e4cb5ccc5200a3c3e50c
Matej Salkovic <matej@extensionengine.com>
2016-12-22 13:40:58 +0100

    Import activity fixtures when 'db:seed' is run

--- server/script/seedDevDatabase.js
+++ server/script/seedDevDatabase.js
@@ -2,9 +2,11 @@
 
 const connector = require('../shared/database').databaseConnector;
 const coursePorter = require('../course/fixture/coursePorter');
+const activityPorter = require('../activity/fixture/activityPorter');
 const userPorter = require('../user/fixture/userPorter');
 
 let db;
+let insertedCourses;
 
 connector
   .initialize()
@@ -15,8 +17,13 @@ connector
     return coursePorter.insertFixtures(db);
   })
   .then(courses => {
+    console.log('Adding activities...');
+    insertedCourses = courses;
+    return activityPorter.insertFixtures(db, insertedCourses);
+  })
+  .then(() => {
     console.log('Adding users...');
-    return userPorter.insertFixtures(db, courses);
+    return userPorter.insertFixtures(db, insertedCourses);
   })
   .then(users => {
     console.log('Seeding complete');
