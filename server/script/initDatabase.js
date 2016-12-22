bd7b03cc4d4f2bda150dbab64db565f4414a7981
Matej Salkovic <matej@extensionengine.com>
2016-12-22 10:38:49 +0100

    Add 'db:init' script to create initial admin user

---
+++ server/script/initDatabase.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const connector = require('../shared/database').databaseConnector;
+const userModel = require('../user').model;
+const ADMIN = require('../user/role').ADMIN;
+
+// Last two arguments should be admin email and password - strip them out and
+// create the admin with those credentials.
+const args = process.argv.slice(2);
+if (args.length !== 2) {
+  console.error('You must supply exactly two arguments - admin email and password');
+  process.exit(1);
+}
+
+const email = args[0];
+const password = args[1];
+
+connector
+  .initialize()
+  .then(() => console.log('Database initialized'))
+  .then(() => userModel.create({
+    email,
+    password,
+    role: ADMIN
+  }))
+  .then(user => {
+    console.log(`Administrator created (user key: ${user._key})`);
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err.message || err);
+    process.exit(1);
+  });
