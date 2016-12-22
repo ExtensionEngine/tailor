dfe0e00482ed68c0ab4786bdb51067f27e67a62a
Matej Salkovic <matej@extensionengine.com>
2016-12-22 13:07:11 +0100

    Import user fixtures when 'db:seed' is run

--- server/script/seedDevDatabase.js
+++ server/script/seedDevDatabase.js
@@ -2,12 +2,24 @@
 
 const connector = require('../shared/database').databaseConnector;
 const coursePorter = require('../course/fixture/coursePorter');
+const userPorter = require('../user/fixture/userPorter');
+
+let db;
 
 connector
   .initialize()
-  .then(db => coursePorter.insertFixtures(db))
+  .then(conn => {
+    console.log('Database initialized');
+    console.log('Adding courses...');
+    db = conn;
+    return coursePorter.insertFixtures(db);
+  })
   .then(courses => {
-    console.log(JSON.stringify(courses, null, 2));
+    console.log('Adding users...');
+    return userPorter.insertFixtures(db, courses);
+  })
+  .then(users => {
+    console.log('Seeding complete');
     process.exit(0);
   })
   .catch(err => {
