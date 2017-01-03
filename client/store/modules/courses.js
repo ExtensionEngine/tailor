4ee72fcef0dfaa2671931a0e5f0bc14d6bb401a9
Marko Vrbanec <marko.vrbanec@protonmail.com>
2017-01-03 15:17:47 +0100

    Fixed course store extend

--- client/store/modules/courses.js
+++ client/store/modules/courses.js
@@ -1,8 +1,9 @@
 import VuexModel from '../helpers/model.js';
 const { state, getter, action, mutation, build } = new VuexModel('courses', '/courses');
 
+// TODO(marko): extend with default state
 state({
-  ...state,
+  items: {},
   search: ''
 });
 
