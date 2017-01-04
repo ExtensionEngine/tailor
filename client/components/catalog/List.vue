197f2626590f86240dbd0173fc19db00db218673
Marko Vrbanec <marko.vrbanec@protonmail.com>
2017-01-04 21:56:26 +0100

    Added and customized vue-infinite-loading component and dummy pagination data

--- client/components/catalog/List.vue
+++ client/components/catalog/List.vue
@@ -1,18 +1,31 @@
 <template>
   <div class="row course-list">
     <cube-spinner v-if="loader"></cube-spinner>
-    <div v-else v-for="course in courses" class="col-lg-4">
+    <div v-else  v-for="course in courses" class="col-lg-4">
       <card
         :id="course._cid"
         :name="course.name"
         :description="course.description">
       </card>
     </div>
+
+    <div class="col-lg-12 loader-wrapper">
+      <infinite-loading
+        :on-infinite="onInfinite"
+        :distance="60"
+        ref="infiniteLoading"
+      >
+        <div slot="spinner">
+          <cube-spinner></cube-spinner>
+        </div>
+      </infinite-loading>
+    </div>
   </div>
 </template>
 
 <script>
 import { mapActions, mapGetters } from 'vuex-module';
+import InfiniteLoading from 'vue-infinite-loading';
 import Card from './Card';
 import CubeSpinner from '../loaders/CubeSpinner';
 
@@ -20,11 +33,25 @@ export default {
   name: 'course-list',
   data() {
     return {
-      loader: true
+      loader: true,
+      loaderPaginate: false
     };
   },
   computed: mapGetters(['courses']),
-  methods: mapActions(['fetch'], 'courses'),
+  methods: {
+    ...mapActions(['fetch', 'fetchNextPage'], 'courses'),
+    onInfinite() {
+      this.loaderPaginate = true;
+
+      // TODO(marko): trigger is very sensitive without setTimeout?
+      setTimeout(() => {
+        this.fetchNextPage().then(() => {
+          this.loaderPaginate = false;
+          this.$refs.infiniteLoading.$emit('$InfiniteLoading:loaded');
+        });
+      }, 1000);
+    }
+  },
   created() {
     this.loader = true;
     this.fetch().then(() => {
@@ -33,7 +60,18 @@ export default {
   },
   components: {
     Card,
-    CubeSpinner
+    CubeSpinner,
+    InfiniteLoading
   }
 };
 </script>
+
+<style lang="scss">
+.loader-wrapper {
+  height: 80px;
+
+  .spinner {
+    margin: 60px auto;
+  }
+}
+</style>
--- client/store/modules/courses.js
+++ client/store/modules/courses.js
@@ -1,8 +1,47 @@
 import VuexModel from '../helpers/model.js';
-const { getter, build } = new VuexModel('courses', '/courses');
+import cuid from 'cuid';
+const { getter, action, mutation, build } = new VuexModel('courses', '/courses');
 
 getter(function courses() {
   return this.state.items;
 }, { global: true });
+
+action(function fetchNextPage() {
+  this.api.fetch()
+    .then(courses => {
+      const stateItemCount = Object.keys(this.state.items).length;
+      let newItems = [];
+
+      // Generate dummy values
+      for (let i = stateItemCount + 1; i < stateItemCount + 10; i++) {
+        newItems.push({
+          _cid: cuid(),
+          name: `Test name ${i}`,
+          description: 'Test description'
+        });
+      }
+
+      this.commit('fetchNextPage', newItems);
+    });
+});
+
+mutation(function fetchNextPage(courses) {
+  let stateItems = [];
+  let items = {};
+
+  // Convert state items to array
+  for (let key in this.state.items) {
+    stateItems.push(this.state.items[key]);
+  }
+
+  // Concat and convert back to object
+  stateItems.concat(courses).forEach(
+    item => {
+      items[item._cid] = item;
+    }
+  );
+
+  this.state.items = items;
+});
 
 export default build();
