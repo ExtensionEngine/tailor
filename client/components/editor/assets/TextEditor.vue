a7b83cfa4c717ef3d9f4006cc2cab2bf2680ca95
underscope <underscope@gmail.com>
2017-01-04 18:41:43 +0100

    Created basic text editor component

---
+++ client/components/editor/assets/TextEditor.vue
@@ -0,0 +1,35 @@
+<template>
+  <div>
+    <quill-editor
+      v-if="input"
+      v-model="content"
+      :config="config"
+      @blur="input = false">
+    </quill-editor>
+    <div
+      v-else
+      v-html="content"
+      @click="input = true"
+      class="ql-editor">
+    </div>
+  </div>
+</template>
+
+<script>
+import { quillEditor } from 'vue-quill-editor';
+
+export default {
+  name: 'text-editor',
+  data() {
+    return {
+      input: true,
+      content: '',
+      config: { modules: { toolbar: '#quillToolbar' } }
+    };
+  },
+  components: { quillEditor }
+};
+</script>
+
+<style lang="scss">
+</style>
