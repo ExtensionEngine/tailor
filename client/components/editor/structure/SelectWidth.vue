fc107bf3bc8ddcabd8f8eaa0ff0d74b693b80664
underscope <underscope@gmail.com>
2017-01-06 23:16:46 +0100

    Component for block width selection

---
+++ client/components/editor/structure/SelectWidth.vue
@@ -0,0 +1,24 @@
+<template>
+  <div>
+    <div
+      class="content-width"
+      v-for="width in widths"
+      @click="$emit('selected', width.value)">
+      <span class="fa" :class="width.icon"></span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'select-width',
+  data() {
+    return {
+      widths: [
+        { value: 6, icon: 'fa-pause' },
+        { value: 12, icon: 'fa-stop' }
+      ]
+    };
+  }
+};
+</script>
