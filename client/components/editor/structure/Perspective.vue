b7669cc902cc6b9f4f15405bdcd2b1062b32cdf2
underscope <underscope@gmail.com>
2017-01-06 15:16:39 +0100

    Created components for perspectives

---
+++ client/components/editor/structure/Perspective.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="perspective">
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'perspective'
+};
+</script>
+
+<style lang="scss">
+</style>
---
+++ client/components/editor/structure/Perspectives.vue
@@ -0,0 +1,27 @@
+<template>
+  <div class="perspectives">
+    <div v-if="!perspectives.length">
+      Click here to create new perspective.
+    </div>
+    <perspective v-for="it in perspectives"></perspective>
+  </div>
+</template>
+
+<script>
+import Perspective from './Perspective';
+
+export default {
+  name: 'perspectives',
+  data() {
+    return {
+      perspectives: []
+    };
+  },
+  components: {
+    Perspective
+  }
+};
+</script>
+
+<style lang="scss">
+</style>
