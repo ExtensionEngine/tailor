7c8f1e6ea1681ed5843526df55962be6be7cc964
underscope <underscope@gmail.com>
2017-01-06 22:49:07 +0100

    Created component for asset selection

---
+++ client/components/editor/structure/SelectAsset.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <div
+      v-for="asset in assets"
+      @click="$emit('selected', asset.type)"
+      class="content-type">
+      <span class="fa" :class="asset.icon"></span>
+      <span>{{ asset.label }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'select-asset',
+  data() {
+    return {
+      assets: [
+        { type: 'video', label: 'Video', icon: 'fa-video-camera' },
+        { type: 'text', label: 'Text', icon: 'fa-file-text-o' },
+        { type: 'image', label: 'Image', icon: 'fa-image' }
+      ]
+    };
+  }
+};
+</script>
