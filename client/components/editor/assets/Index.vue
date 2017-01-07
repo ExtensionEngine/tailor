3ead61814975c478e0a3392ccfc0e6c57ded7f4c
underscope <underscope@gmail.com>
2017-01-07 11:15:13 +0100

    Added TextEditor to asset container

--- client/components/editor/assets/Index.vue
+++ client/components/editor/assets/Index.vue
@@ -1,12 +1,35 @@
 <template>
-  <div class="asset"></div>
+  <div class="asset-container" :class="columnWidth">
+    <div class="asset">
+      <text-editor v-if="asset.type === 'text'"></text-editor>
+    </div>
+  </div>
 </template>
 
 <script>
+import TextEditor from './Text';
+
 export default {
-  name: 'asset'
+  name: 'asset',
+  props: { asset: Object },
+  computed: {
+    columnWidth() {
+      return `col-xs-${this.asset.width}`;
+    }
+  },
+  components: {
+    TextEditor
+  }
 };
 </script>
 
 <style lang="scss">
+.asset-container {
+  padding: 7px 0;
+}
+
+.asset {
+  padding: 5px;
+  border: 1px dashed #ccc;
+}
 </style>
