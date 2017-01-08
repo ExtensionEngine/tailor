5b8a0f0d47a47bd378569ee1b3e431e6f83bf339
underscope <underscope@gmail.com>
2017-01-08 15:48:31 +0100

    Created AssessmentItem
    - Used for assessment list

---
+++ client/components/editor/structure/AssessmentItem.vue
@@ -0,0 +1,20 @@
+<template>
+  <li class="list-group-item">
+    <div>
+      <span class="label label-success">{{ assessment.type }}</span>
+      <span>{{ assessment.title }}</span>
+    </div>
+  </li>
+</template>
+
+<script>
+import Assessment from '../assessments';
+
+export default {
+  name: 'assessment-item',
+  props: ['assessment'],
+  components: {
+    Assessment
+  }
+};
+</script>
