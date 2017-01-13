30164c679f4ca7cf64ba2519269a744c229a2032
Matej Salkovic <matej@extensionengine.com>
2017-01-13 13:58:30 +0100

    Show pencil icon on hover over activity name

--- client/components/course/Sidebar.vue
+++ client/components/course/Sidebar.vue
@@ -1,8 +1,14 @@
 <template>
   <div class="course-sidebar">
     <div class="title-bar" v-show="isActivitySelected">
-      <h3 class="title">{{ activity.name }}</h3>
-      <button class="btn btn-default" @click.stop="removeSelectedActivity">X</button>
+      <span class="title-editor">
+        <h3 class="title">{{ activity.name }}</h3>
+        <span class="fa fa-pencil pencil" aria-hidden="true"></span>
+      </span>
+      <button class="btn btn-default"
+              @click.stop="removeSelectedActivity">
+        X
+      </button>
     </div>
   </div>
 </template>
@@ -28,19 +34,37 @@ export default {
 
 <style lang="scss">
 .course-sidebar {
-  width: 400px;
-  height: 100%;
   position: fixed;
   right: 0;
+  width: 400px;
+  height: 100%;
   padding: 30px 20px;
+  text-align: left;
   border-top: 1px solid #e8e8e8;
   background-color: #fcfcfc;
 
   .title {
     display: inline-block;
-    width: 90%;
+    width: 85%;
     text-align: left;
     font-size: 18px;
+  }
+
+  .title-editor {
+    .pencil {
+      display: none;
+    }
+  }
+
+  .title-editor:hover {
+    .pencil {
+      display: inline;
+    }
+  }
+
+  button {
+    float: right;
+    margin-top: 12px;
   }
 }
 </style>
