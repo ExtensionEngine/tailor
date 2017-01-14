f0344c08a2c7d17e319859087d567c9a916695ec
underscope <underscope@gmail.com>
2017-01-14 15:34:35 +0100

    Updated atom module

--- client/store/modules/atom.js
+++ client/store/modules/atom.js
@@ -1,40 +1,34 @@
-import cuid from 'cuid';
-import Vue from 'vue';
+import filter from 'lodash/filter';
+import find from 'lodash/find';
 import { VuexModule } from 'vuex-module';
 
 const { state, getter, action, mutation, build } = new VuexModule('atom');
 
-// TODO: WIP version; location and structure
-// will change upon BE integration and should be much flatter
 state({
-  perspectives: {
-    items: {}
-  },
-  assets: {
-    focused: null,
-    items: {}
-  },
-  assessments: {
-    items: {}
-  }
+  focusedAsset: null
 });
 
-// TODO: WIP version; Temporary getters
+getter(function activity() {
+  const { route } = this.rootState;
+  const { activities } = this.rootGetters;
+  return find(activities, { _key: route.params.activityKey });
+});
+
 getter(function perspectives() {
-  return this.state.perspectives.items;
-});
-
-getter(function assets() {
-  return this.state.assets.items;
+  const { route } = this.rootState;
+  const { activities } = this.rootGetters;
+  return filter(activities, { parentKey: route.params.activityKey });
 });
 
 getter(function assessments() {
-  return this.state.assessments.items;
+  const { route } = this.rootState;
+  const { assessments: collection } = this.rootGetters;
+  return filter(collection, { activityKey: route.params.activityKey });
 });
 
 getter(function focusedAsset() {
-  const { items, focused } = this.state.assets;
-  return items[focused];
+  const { assets } = this.rootGetters;
+  return assets[this.state.focusedAsset];
 });
 
 // TODO: Implement persistance upon focusout
@@ -42,35 +36,8 @@ action(function focusoutAsset() {
   this.commit('focusAsset', null);
 });
 
-action(function createPerspective() {
-  const model = { _cid: cuid() };
-  this.commit('createPerspective', model);
-});
-
-action(function createAsset() {
-  const model = { _cid: cuid() };
-  this.commit('createAsset', model);
-});
-
-action(function createAssessment() {
-  const model = { _cid: cuid() };
-  this.commit('createAssessment', model);
-});
-
-mutation(function createPerspective(model) {
-  Vue.set(this.state.perspectives.items, model._cid, model);
-});
-
-mutation(function createAsset(model) {
-  Vue.set(this.state.assets.items, model._cid, model);
-});
-
-mutation(function createAssessment(model) {
-  Vue.set(this.state.assessment.items, model._cid, model);
-});
-
-mutation(function focusAsset(assset) {
-  this.state.assets.focused = assset._cid;
+mutation(function focusAsset(asset) {
+  this.state.focusedAsset = asset._cid;
 });
 
 export default build();
