1d48becbd072fb7924ee7f6dece81ceb6594297b
Marko Vrbanec <marko.vrbanec@protonmail.com>
2017-01-15 18:53:33 +0100

    Moved user role definitions in global config

--- client/settings.js
+++ client/settings.js
@@ -1,25 +1,5 @@
 export default {
   debug: {
     state: false
-  },
-
-  // TODO: should match user roles stored in db
-  role: {
-    STUDENT: {
-      value: 'student',
-      render: 'Student'
-    },
-    EDITOR: {
-      value: 'editor',
-      render: 'Editor'
-    },
-    COURSE_ADMIN: {
-      value: 'course_admin',
-      render: 'Course admin'
-    },
-    GLOBAL_ADMIN: {
-      value: 'global_admin',
-      render: 'Global admin'
-    }
   }
 };
