d1c7dabd80666c170ee3535b10df8f691e178270
underscope <underscope@gmail.com>
2017-01-16 14:55:05 +0100

    Video component improvements
    - Added placeholder for uninitialised component
    - Added overlay when not focused
    - Essential styling

--- client/components/editor/assets/Video.vue
+++ client/components/editor/assets/Video.vue
@@ -1,6 +1,26 @@
 <template>
-  <div>
-    <video-player :options="options"></video-player>
+  <div class="video-asset">
+    <div v-if="showPlaceholder">
+      <div class="well video-placeholder">
+        <div class="message">
+          <span class="heading">Video placeholder</span>
+          <span v-if="!isFocused">Select to edit</span>
+          <span v-else>Please use toolbar to enter url</span>
+        </div>
+      </div>
+    </div>
+    <div v-else>
+      <div class="overlay" v-if="!isFocused">
+        <div class="message">
+          Click to preview
+          <span class="fa fa-play"></span>
+        </div>
+      </div>
+      <video-player
+        ref="video"
+        :options="options">
+      </video-player>
+    </div>
   </div>
 </template>
 
@@ -10,18 +30,67 @@ import { videoPlayer } from 'vue-video-player';
 export default {
   name: 'video-asset',
   props: ['asset', 'isFocused'],
-  data() {
-    return {
-      options: {
-        source: {
-          type: 'video/webm',
-          src: 'https://cdn.theguardian.tv/webM/2015/07/20/150716YesMen_synd_768k_vp8.webm'
-        }
-      }
-    };
+  computed: {
+    player() {
+      return this.$refs.video && this.$refs.video.player;
+    },
+    options() {
+      return {
+        autoplay: false,
+        source: { src: this.asset.src }
+      };
+    },
+    showPlaceholder() {
+      return !this.asset.src;
+    }
+  },
+  watch: {
+    isFocused(val, oldVal) {
+      if (oldVal && !val) if (this.player) this.player.pause();
+    }
   },
   components: {
     videoPlayer
   }
 };
 </script>
+
+<style lang="scss" scoped>
+.video-asset {
+  position: relative;
+}
+
+.video-placeholder {
+  .message {
+    padding: 100px;
+
+    .heading {
+      font-size: 24px;
+    }
+
+    span {
+      display: block;
+      font-size: 18px;
+    }
+  }
+}
+
+.overlay {
+  position: absolute;
+  z-index: 99;
+  width: 100%;
+  height: 100%;
+  background-color: #333;
+  opacity: 0.9;
+
+  .message {
+    margin-top: 60px;
+    color: green;
+    font-size: 22px;
+  }
+}
+
+.video-asset .vjs-big-play-button {
+  display: none !important;
+}
+</style>
