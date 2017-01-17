a242abd5c6a238c3e8ba5ddb1abd71bdb49380a4
underscope <underscope@gmail.com>
2017-01-17 15:01:15 +0100

    Sidebar cleanup :lipstick:

--- client/components/course/Sidebar.vue
+++ client/components/course/Sidebar.vue
@@ -1,25 +1,37 @@
 <template>
   <div class="course-sidebar">
-    <div class="title-bar" v-if="isActivitySelected">
-      <template v-if="showNameInput">
+    <div v-if="isActivitySelected" class="row">
+      <div class="col-xs-10">
         <textarea
           class="form-control"
+          ref="activityName"
+          v-show="showNameInput"
           v-model="newActivityName"
           @blur="onInputBlur"
           @keyup.enter="onInputEnter"
           @keyup.esc="deactivateInput">
         </textarea>
-      </template>
-      <template v-else>
-        <span class="title-editor">
-          <h3 class="title" @click.stop="activateInput">{{ activity.name }}</h3>
-          <span class="fa fa-pencil pencil" aria-hidden="true"></span>
-        </span>
-        <button class="btn btn-default"
-                @click.stop="removeSelectedActivity">
-          X
-        </button>
-      </template>
+        <div v-show="!showNameInput" class="title-container">
+          <h3 class="title" @click.stop="activateInput">
+            {{ activity.name }}
+          </h3>
+          <span class="fa fa-pencil"></span>
+        </div>
+      </div>
+      <div class="col-xs-2">
+        <div class="dropdown">
+          <button
+            class="btn btn-default btn-options dropdown-toggle"
+            type="button"
+            data-toggle="dropdown">
+            <span class="fa fa-ellipsis-v"></span>
+          </button>
+          <ul class="dropdown-menu pull-right">
+            <li><a @click.stop="removeSelectedActivity">Delete</a></li>
+            <li><a href="#">Action 2</a></li>
+          </ul>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -35,15 +47,17 @@ export default {
     };
   },
   computed: {
+    ...mapGetters(['activity'], 'editor'),
     isActivitySelected() {
       return !!this.activity.name;
-    },
-    ...mapGetters(['activity'], 'editor')
+    }
   },
   methods: {
+    ...mapActions(['remove', 'update'], 'activity'),
     activateInput() {
       this.newActivityName = this.activity.name.slice(0);
       this.showNameInput = true;
+      setTimeout(() => this.$refs.activityName.focus(), 0);
     },
     deactivateInput() {
       this.newActivityName = '';
@@ -67,13 +81,12 @@ export default {
     },
     removeSelectedActivity() {
       this.remove(this.activity);
-    },
-    ...mapActions(['remove', 'update'], 'activity')
+    }
   }
 };
 </script>
 
-<style lang="scss">
+<style lang="scss" scoped>
 .course-sidebar {
   position: fixed;
   right: 0;
@@ -84,32 +97,47 @@ export default {
   border-top: 1px solid #e8e8e8;
   background-color: #fcfcfc;
 
+  .btn-options {
+    margin: 0px 10px;
+    padding: 6px 8px 4px;
+    color: #555;
+    border: 1px #ddd solid;
+    border-radius: 2px;
+    background-color: white;
+    box-shadow: none;
+
+    &:hover {
+      color: #444;
+      border: 1px #555 solid;
+    }
+  }
+
   .title {
     display: inline-block;
-    width: 85%;
-    text-align: left;
-    font-size: 18px;
+    float: left;
+    width: 94%;
+    margin: 5px 3px 0 0;
+    font-size: 17px;
+    font-weight: normal;
   }
 
-  .title-editor {
-    .pencil {
+  .title-container {
+    .fa {
       display: none;
     }
-  }
 
-  .title-editor:hover {
-    .pencil {
-      display: inline;
+    &:hover {
+      cursor: pointer;
+
+      .fa {
+        display: inline-block;;
+      }
     }
-  }
-
-  button {
-    float: right;
-    margin-top: 12px;
   }
 
   textarea {
-    margin: 10px 0;
+    margin: 5px 0;
+    min-height: 100px;
   }
 }
 </style>
