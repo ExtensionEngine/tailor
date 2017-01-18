bc420ac2b043b035465e9a5a0c993a7b25685ee0
Matej Salkovic <matej@extensionengine.com>
2017-01-18 11:40:47 +0100

    Toggle inputs for course name and description

--- client/components/course/CourseDetails.vue
+++ client/components/course/CourseDetails.vue
@@ -1,14 +1,30 @@
 <template>
   <div class="container">
     <div class="course-name">
-      <h2>
-        {{ course.name }}
-      </h2>
-      <span class="fa fa-pencil pencil" aria-hidden="true"></span>
+      <template v-if="showNameInput">
+        <input class="form-control" v-model="newCourseName"/>
+      </template>
+      <template v-else>
+        <h2>
+          {{ course.name }}
+        </h2>
+        <span class="fa fa-pencil pencil" aria-hidden="true"></span>
+      </template>
     </div>
     <div class="course-description">
-      {{ course.description }}
-      <span class="fa fa-pencil pencil" aria-hidden="true"></span>
+      <template v-if="showDescriptionInput">
+        <textarea
+          class="form-control"
+          v-model="newCourseDescription">
+          <!-- @blur="onInputBlur"
+          @keyup.enter="onInputEnter"
+          @keyup.esc="deactivateInput"> -->
+        </textarea>
+      </template>
+      <template v-else>
+        {{ course.description }}
+        <span class="fa fa-pencil pencil" aria-hidden="true"></span>
+      </template>
     </div>
     <div class="course-actions">
       <button type="button" class="btn btn-danger">
@@ -24,11 +40,23 @@ import { mapGetters } from 'vuex-module';
 import find from 'lodash/find';
 
 export default {
+  data() {
+    return {
+      showNameInput: true,
+      showDescriptionInput: true,
+      newCourseName: '',
+      newCourseDescription: '',
+    };
+  },
   computed: {
     course() {
       return find(this.courses, c => c._key === this.$route.params.courseKey) || {};
     },
     ...mapGetters(['courses'])
+  },
+  onCreated() {
+    this.newCourseName = this.course.name || '';
+    this.newCourseDescription = this.course.description || '';
   }
 };
 </script>
