030890576743f743b69f5ef5a3058bc7e7e0f6fd
Matej Salkovic <matej@extensionengine.com>
2017-01-20 12:52:02 +0100

    Update assets API to the latest spec

--- server/asset/index.js
+++ server/asset/index.js
@@ -3,61 +3,45 @@
 const express = require('express');
 
 const controller = require('./asset.controller').controller;
-const course = require('../course');
 const io = require('../shared/io');
 const model = require('./asset.model');
-
-const loadCourse = course.middleware.loadCourse(course.model);
-const requireCourseAccess = course.middleware.requireCourseAccess;
 
 const router = express.Router();
 const input = io.input();
 const output = io.output();
 
-router.get('/courses/:courseKey/assets',
+router.get('/assets',
   input,
-  requireCourseAccess,
-  loadCourse,
   controller.list,
   output
 );
 
-router.get('/courses/:courseKey/assets/:assetKey',
+router.get('/assets/:assetKey',
   input,
-  requireCourseAccess,
-  loadCourse,
   controller.show,
   output
 );
 
-router.post('/courses/:courseKey/assets/',
+router.post('/assets/',
   input,
-  requireCourseAccess,
-  loadCourse,
   controller.create,
   output
 );
 
-router.patch('/courses/:courseKey/assets/:assetKey',
+router.patch('/assets/:assetKey',
   input,
-  requireCourseAccess,
-  loadCourse,
   controller.patch,
   output
 );
 
-router.put('/courses/:courseKey/assets/:assetKey',
+router.put('/assets/:assetKey',
   input,
-  requireCourseAccess,
-  loadCourse,
   controller.replace,
   output
 );
 
-router.delete('/courses/:courseKey/assets/:assetKey',
+router.delete('/assets/:assetKey',
   input,
-  requireCourseAccess,
-  loadCourse,
   controller.remove,
   output
 );
