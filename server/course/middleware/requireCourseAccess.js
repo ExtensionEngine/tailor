0bfc1d4f67d6c8f42405ad7788c91fba495c7011
underscope <underscope@gmail.com>
2017-01-24 18:28:52 +0100

    Course access middleware

--- server/course/middleware/requireCourseAccess.js
+++ server/course/middleware/requireCourseAccess.js
@@ -1,15 +1,16 @@
 'use strict';
 
 const get = require('lodash/get');
-const role = require('../../../config/shared').role;
+const locals = require('../../shared/io').locals;
+const { user: role } = require('../../../config/shared').role;
 
 function requireCourseAccess(req, res, next) {
-  const courseKey = req.params.courseKey;
-  const userHasAccess = get(req, 'user.courses', []).includes(courseKey);
-  const userIsSystemAdmin = get(req, 'user.role') === role.SYSTEM_ADMIN;
-  const userIsAdmin = get(req, 'user.role') === role.ADMIN;
+  const user = req.user;
+  const course = locals.load(req, 'course');
+  const isAdmin = get(req, 'user.role') === role.ADMIN;
+  const hasAccess = course.users[user._key];
 
-  if (userIsSystemAdmin || userIsAdmin || userHasAccess) next();
+  if (isAdmin || hasAccess) next();
   else res.status(401).json();
 }
 
