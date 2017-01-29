358b1e7045443df3c724920aefea0969a44c53cf
underscope <underscope@gmail.com>
2017-01-29 23:05:16 +0100

    Applied global auth middleware & error handling

--- server/app.js
+++ server/app.js
@@ -3,38 +3,21 @@
 const bodyParser = require('body-parser');
 const cors = require('cors');
 const express = require('express');
-const expressSession = require('express-session');
+const includes = require('lodash/includes');
 const passport = require('passport');
 
 // Setup authentication before instantiating the main app router.
 // eslint-disable-next-line no-unused-vars
 const auth = require('./shared/auth');
 const config = require('../config/server');
-const errorHandler = require('./shared/error').errorHandler;
 const logger = require('./shared/logger');
 const router = require('./router');
-const sessionStore = require('./session').store;
 
 const app = express();
 app.disable('x-powered-by');
-
-app.use(cors({
-  origin: config.auth.corsAllowedOrigins,
-  credentials: true
-}));
+app.use(cors({ origin: config.auth.corsAllowedOrigins, credentials: true }));
 app.use(bodyParser.json());
-
-// Initialize Passport and restore authentication state, if any, from the
-// session.
-const session = expressSession({
-  secret: config.auth.sessionSecret,
-  resave: false,
-  saveUninitialized: false,
-  store: sessionStore
-});
-app.use(session);
 app.use(passport.initialize());
-app.use(passport.session());
 
 // Log all incoming requests.
 app.use('/api/v1', (req, res, next) => {
@@ -42,30 +25,44 @@ app.use('/api/v1', (req, res, next) => {
   next();
 });
 
-// Mount the main router.
+// Apply auth for all routes except whitelisted.
+app.use('*', (req, res, next) => {
+  const whitelisted = ['/api/v1/users/login'];
+  if (includes(whitelisted, req.baseUrl)) {
+    next();
+  } else {
+    passport.authenticate('jwt')(req, res, next);
+  }
+});
+
+// Mount main router.
 app.use('/api/v1', router);
 
 // Global error handler.
-app.use(errorHandler());
+app.use((err, req, res, next) => {
+  if (!err.status || err.status === 500) {
+    res.status(500).end();
+    return;
+  }
+  const { status, message } = err;
+  res.status(err.status).json({ error: { status, message } });
+});
 
 // Serve swagger API spec in development environment.
 if (config.swagger.serveDocs) {
   const path = require('path');
   const swaggerJsDoc = require('swagger-jsdoc');
-
   const spec = swaggerJsDoc({
     swaggerDefinition: config.swagger.swaggerDefinition,
     apis: ['./server/**/*.js']
   });
-  app.get('/api/v1/swagger.json', (req, res, next) => res.status(200).json(spec));
 
-  const dir = path.join(__dirname, '..', 'node_modules', 'swagger-ui', 'dist');
+  app.get('/api/v1/swagger.json', (req, res, next) => res.json(spec));
+  const dir = path.join(__dirname, '../node_modules/swagger-ui/dist');
   app.use('/swagger', express.static(dir));
 }
 
 // Handle non-existing routes.
-app.use((req, res, next) => {
-  res.status(404).json();
-});
+app.use((req, res, next) => res.status(404).end());
 
 module.exports = app;
