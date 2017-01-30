98a9307dd01f9609d2d6a9b7356f71898d9e6691
Marko Vrbanec <marko.vrbanec@protonmail.com>
2017-01-30 12:36:46 +0100

    Removed redundant POSTGRES_URI definition

--- config/server/database.js
+++ config/server/database.js
@@ -1,5 +1,4 @@
 module.exports = {
   uri: process.env.ARANGODB_URI || 'http://localhost:8529',
-  name: process.env.DB_NAME || 'tailor-dev',
-  postgresUri: process.env.POSTGRES_URI
+  name: process.env.DB_NAME || 'tailor-dev'
 };
