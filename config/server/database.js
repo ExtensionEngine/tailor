4c02a93722f5dcb70f1ab63bd975bc0481b90d88
Marko Vrbanec <marko.vrbanec@protonmail.com>
2017-01-30 02:28:22 +0100

    Temp sequelize config

--- config/server/database.js
+++ config/server/database.js
@@ -1,4 +1,5 @@
 module.exports = {
   uri: process.env.ARANGODB_URI || 'http://localhost:8529',
-  name: process.env.DB_NAME || 'tailor-dev'
+  name: process.env.DB_NAME || 'tailor-dev',
+  postgresUri: process.env.POSTGRES_URI
 };
--- server/shared/database/index.js
+++ server/shared/database/index.js
@@ -1,9 +1,15 @@
 'use strict';
 
+const Sequelize = require('sequelize');
 const connector = require('./connector');
 const collection = require('./collection');
+const config = require('../../../config/server');
+
+// TODO(marko): Temp solution.
+const sequelize = new Sequelize(config.database.postgresUri);
 
 module.exports = {
+  sequelize,
   collection,
   db: connector.db,
   databaseConnector: connector.databaseConnector
