5f70602971124939e0fc279e1d5817e08a2f9b85
Matej Salkovic <matej@extensionengine.com>
2017-01-31 12:06:34 +0100

    Course patch and remove API

--- server/course/course.controller.js
+++ server/course/course.controller.js
@@ -1,11 +1,31 @@
 'use strict';
 
+const omit = require('lodash/omit');
+const { Course } = require('../shared/database/sequelize');
+
 function index(req, res) {
   const user = req.user;
   return user.getCourses()
     .then(courses => res.json({ data: courses }));
 };
 
+function patch(req, res) {
+  const id = req.params.id;
+  const course = omit(req.body, ['id', 'created_at', 'updated_at']);
+  return Course.update(course, { where: { id }, returning: true })
+    .then(([n, courses]) => n
+      ? res.status(200).json({ data: courses[0] })
+      : res.status(404).send());
+};
+
+function remove(req, res) {
+  const id = req.params.id;
+  return Course.destroy({ where: { id } })
+    .then(() => res.status(204).send());
+};
+
 module.exports = {
-  index
+  index,
+  patch,
+  remove
 };
--- server/course/index.js
+++ server/course/index.js
@@ -5,6 +5,8 @@ const middleware = require('./middleware');
 const router = require('express-promise-router')();
 
 router.get('/courses', ctrl.index);
+router.patch('/courses/:id', ctrl.patch);
+router.delete('/courses/:id', ctrl.remove);
 
 module.exports = {
   controller: ctrl,
