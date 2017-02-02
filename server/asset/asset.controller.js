f720d87fabae5f61347dce68b7b6db517dfd2a4c
Marko Vrbanec <marko.vrbanec@protonmail.com>
2017-02-02 15:26:32 +0100

    Asset controller cleanup

--- server/asset/asset.controller.js
+++ server/asset/asset.controller.js
@@ -8,35 +8,35 @@ const pick = require('lodash/pick');
 function list({ query }, res) {
   return Asset
     .findAllByActivity(query.activityId)
-    .then(activities => res.json({ data: Asset.serializeMany(activities) }));
+    .then(assets => res.json({ data: assets }));
 }
 
 function show({ params }, res) {
   return Asset
     .findById(params.assetId)
     .then(asset => asset || createError(NOT_FOUND, 'Asset not found'))
-    .then(asset => res.json({ data: asset.serialize() }));
+    .then(asset => res.json({ data: asset }));
 }
 
 function create({ body }, res) {
-  const fields = ['layoutWidth', 'position', 'type', 'data', 'courseId', 'parentId'];
+  const fields = ['layoutWidth', 'position', 'type', 'data', 'courseId', 'activityId'];
   return Asset
     .create(pick(body, fields))
-    .then(asset => res.json({ data: asset.serialize() }));
+    .then(asset => res.json({ data: asset }));
 }
 
 function patch({ body, params }, res) {
   return Asset
     .updateById(params.assetId, body)
     .then(asset => asset || createError(NOT_FOUND, 'Asset not found'))
-    .then(asset => res.json({ data: asset.serialize() }));
+    .then(asset => res.json({ data: asset }));
 }
 
 function remove({ params }, res) {
   return Asset
     .deleteById(params.assetId)
-    .asset(asset => asset || createError(NOT_FOUND, 'Asset not found'))
-    .then(asset => res.json({ data: asset.serialize() }));
+    .then(asset => asset || createError(NOT_FOUND, 'Asset not found'))
+    .then(asset => res.json({ data: asset }));
 }
 
 module.exports = {
