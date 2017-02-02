98b9c9517018dc722dcaed44d8a1922a0780e21c
Marko Vrbanec <marko.vrbanec@protonmail.com>
2017-02-02 12:06:11 +0100

    Updated asset routes

--- server/asset/index.js
+++ server/asset/index.js
@@ -2,53 +2,22 @@
 
 const express = require('express');
 
-const controller = require('./asset.controller').controller;
-const io = require('../shared/io');
+const ctrl = require('./asset.controller');
 const model = require('./asset.model');
-const parsers = require('../shared/middleware').queryParamParsers;
-
 const router = express.Router();
-const input = io.input();
-const output = io.output();
-const queryParsers = [
-  parsers.parsePagination,
-  parsers.parseSearchTerms,
-  parsers.parseSort
-];
 
-router.get('/assets',
-  input,
-  queryParsers,
-  controller.listFiltered,
-  output
-);
+router
+  .route('/assets')
+  .get(ctrl.list)
+  .post(ctrl.create);
 
-router.get('/assets/:assetId',
-  input,
-  controller.show,
-  output
-);
-
-router.post('/assets/',
-  input,
-  controller.create,
-  output
-);
-
-router.patch('/assets/:assetId',
-  input,
-  controller.patch,
-  output
-);
-
-router.delete('/assets/:assetId',
-  input,
-  controller.remove,
-  output
-);
+router
+  .route('/assets/:assetId')
+  .get(ctrl.show)
+  .patch(ctrl.patch)
+  .delete(ctrl.remove);
 
 module.exports = {
-  controller,
   model,
   router
 };
