ea0d35494429ac6383050394c41a2521e28da17e
underscope <underscope@gmail.com>
2017-02-17 09:55:50 +0100

    Cleanup :lipstick:

--- server/revision/index.js
+++ server/revision/index.js
@@ -1,7 +1,6 @@
 'use strict';
 
 const router = require('express-promise-router')();
-const mw = require('../course/middleware');
 const ctrl = require('./revision.controller');
 
 router.get('/courses/:courseId/revisions', ctrl.index);
