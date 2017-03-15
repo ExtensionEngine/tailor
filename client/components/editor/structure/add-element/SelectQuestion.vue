2be0d6c31a71827c7db3ae3d5de093fa443ad791
underscope <underscope@gmail.com>
2017-03-15 20:02:49 +0100

    Updated SelectQuestion component

---
+++ client/components/editor/structure/add-element/SelectQuestion.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="select-question">
+    <div
+      v-for="question in questions"
+      @click="$emit('selected', question.type)"
+      class="btn-base question-type">
+      <span>{{ question.title }}</span>
+    </div>
+  </div>
+</template>
+
+<script>
+import { typeInfo } from 'utils/assessment';
+
+export default {
+  name: 'select-question',
+  data() {
+    return { questions: typeInfo };
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.select-question {
+  margin: 15px 0;
+  color: #444;
+
+  .btn-base {
+    display: inline-block;
+    font-size: 28px;
+    line-height: 28px;
+    vertical-align: middle;
+
+    &:hover {
+      color: #42b983;
+      cursor: pointer;
+    }
+  }
+
+  .question-type {
+    margin: 0 15px;
+    font-size: 16px;
+    line-height: 16px;
+  }
+}
+</style>
