5543ad846b1a1355aac71db80fad11afa60004a3
underscope <underscope@gmail.com>
2017-03-16 10:41:04 +0100

    Created Embed element

---
+++ client/components/editor/teaching-elements/Embed.vue
@@ -0,0 +1,91 @@
+<template>
+  <div class="te-embed">
+    <div v-if="showPlaceholder">
+      <div class="well placeholder">
+        <span class="heading">Embed placeholder</span>
+        <span class="message" v-show="!isFocused">Select to edit</span>
+        <span class="message" v-show="isFocused">Please use toolbar to enter url</span>
+      </div>
+    </div>
+    <div v-else>
+      <div class="content">
+        <div class="overlay" v-show="!isFocused">
+          <div class="message">Click to preview</div>
+        </div>
+        <iframe
+          :src="url"
+          ref="frame"
+          class="content"
+          frameborder="0">
+        </iframe>
+      </div>
+      <div class="pusher"></div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'te-embed',
+  props: ['element', 'isFocused'],
+  computed: {
+    url() {
+      return this.element.data.url;
+    },
+    showPlaceholder() {
+      return !this.element.data.url;
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.te-embed {
+  position: relative;
+  overflow: auto;
+}
+
+.placeholder {
+  margin: 0;
+  padding: 119px;
+
+  .heading {
+    font-size: 24px;
+  }
+
+  .message {
+    display: block;
+    font-size: 18px;
+  }
+}
+
+.overlay {
+  position: absolute;
+  z-index: 99;
+  width: 100%;
+  height: 100%;
+  background-color: #333;
+  opacity: 0.9;
+
+  .message {
+    position: relative;
+    top: 45%;
+    color: green;
+    font-size: 22px;
+  }
+}
+
+.content {
+  top: 0;
+  left: 0;
+  right: 0;
+  bottom: 0;
+  position: absolute;
+  width: 100%;
+  height: 100%;
+}
+
+.pusher {
+  margin-top: 56.25%;
+}
+</style>
