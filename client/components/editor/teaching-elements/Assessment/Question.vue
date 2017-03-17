ba20930b5fecbd837702177e0fe5e03a183d83d1
underscope <underscope@gmail.com>
2017-03-17 08:56:53 +0100

    Updated Question component :wrench:

--- client/components/editor/teaching-elements/Assessment/Question.vue
+++ client/components/editor/teaching-elements/Assessment/Question.vue
@@ -4,47 +4,45 @@
     <div
       :class="{ editing: isEditing, 'question-error': questionError }"
       class="question">
-      <asset
-        v-for="asset in assessment.question"
-        :asset="asset"
+      <primitive
+        v-for="element in assessment.data.question"
+        :initialElement="element"
         :disabled="!isEditing"
-        @input="assetChanged">
-      </asset>
-      <select-asset v-show="isEditing" @selected="addAsset"></select-asset>
+        @save="elementChanged">
+      </primitive>
+      <add-element
+        v-show="isEditing"
+        :include="['HTML', 'IMAGE']"
+        @add="addElement">
+      </add-element>
     </div>
-    <span class="help-block" v-if="isEditing && helperText">
+    <span v-if="isEditing && helperText" class="help-block">
       {{ helperText }}
     </span>
   </div>
 </template>
 
 <script>
-import Asset from '../assets';
+import AddElement from '../../structure/AddElement';
 import cloneDeep from 'lodash/cloneDeep';
-import cuid from 'cuid';
 import EventBus from 'EventBus';
 import findIndex from 'lodash/findIndex';
-import { helperText } from '../../../utils/assessment';
-import { mapGetters } from 'vuex-module';
+import { helperText } from 'utils/assessment';
 import pullAt from 'lodash/pullAt';
-import SelectAsset from './SelectAsset';
+import Primitive from '../Primitive';
 
 const appChannel = EventBus.channel('app');
 
 export default {
+  name: 'question',
   props: {
     assessment: Object,
     isEditing: Boolean,
     errors: Array
   },
   computed: {
-    ...mapGetters(['toolbar'], 'atom'),
-    isFocused() {
-      const ctx = this.toolbar.context;
-      return this.isEditing && (ctx && ctx._cid === this.assessment._cid);
-    },
     helperText() {
-      const helper = helperText[this.assessment.type] || {};
+      const helper = helperText[this.assessment.data.type] || {};
       return helper.question;
     },
     questionError() {
@@ -52,28 +50,30 @@ export default {
     }
   },
   methods: {
-    addAsset(type) {
-      const asset = { id: cuid(), _cid: cuid(), type, data: {}, embedded: true };
-      const question = this.assessment.question.concat(asset);
+    addElement(element) {
+      const question = this.assessment.data.question.concat(element);
       this.$emit('update', { question });
     },
-    assetChanged(value) {
-      this.$emit('update', { question: this.assessment.question });
+    elementChanged(element) {
+      const question = cloneDeep(this.assessment.data.question);
+      const index = findIndex(question, { id: element.id });
+      question[index] = element;
+      this.$emit('update', { question });
     }
   },
   created() {
-    appChannel.on('deleteAsset', asset => {
-      if (!asset.embedded) return;
-      const index = findIndex(this.assessment.question, { _cid: asset._cid });
+    appChannel.on('deleteElement', element => {
+      if (!element.embedded) return;
+      const index = findIndex(this.assessment.data.question, { id: element.id });
       if (index === -1) return;
-      let question = cloneDeep(this.assessment.question);
+      let question = cloneDeep(this.assessment.data.question);
       pullAt(question, index);
       this.$emit('update', { question });
     });
   },
   components: {
-    Asset,
-    SelectAsset
+    AddElement,
+    Primitive
   }
 };
 </script>
