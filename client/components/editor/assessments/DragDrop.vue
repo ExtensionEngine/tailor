4b38fd6b674432bdb35e0e1523678c20e5c212bb
Nediljko Krstic <nediljko@extensionengine.com>
2017-03-20 17:29:12 +0100

    Drag and drop refactor

--- client/components/editor/assessments/DragDrop.vue
+++ client/components/editor/assessments/DragDrop.vue
@@ -1,15 +1,10 @@
 <template>
   <div :class="{ 'disabled': disabled }">
     <div class="row">
-      <div
-        v-for="(dropSpot, col) in dropSpots"
-        :class="lgCol"
-        class="drop-spots col-md-6 col-sm-12">
-        <div :class="{'flip': allowFlip(col)}" class="heading-box flipper">
-          <div @click="toggleDisplay(col)" class="heading-view">
-            <span :class="errorClass(col)" class="heading-text">
-              {{ dropSpot.heading }}
-            </span>
+      <div v-for="(dropSpot, col) in dropSpots" :class="lgCol" class="col-md-6">
+        <div :class="{'flip': isSelected(col)}" class="heading-box">
+          <div @click="flip(col)" class="heading-view">
+            <span :class="errorClass(col)">{{ dropSpot.heading }}</span>
             <span
               v-show="allowRemove(col)"
               @click.stop="removeDropSpot(col)"
@@ -19,21 +14,18 @@
           <input
             v-model="dropSpot.heading"
             v-focus="{ col, row: null }"
-            @keyup.enter="allowFlip(col) && toggleDisplay(col)"
-            @keyup.esc="allowFlip(col) && toggleDisplay(col)"
-            @blur="allowFlip(col) && toggleDisplay(col)"
-            class="form-control heading-input"
-            type="text">
+            @keyup.enter="flip(col)"
+            @keyup.esc="flip(col)"
+            @blur="isSelected(col) && flip(col)"
+            class="form-control heading-input">
         </div>
         <ul>
           <li
             v-for="(answer, row) in answerGroup(col)"
-            :class="{'flip': allowFlip(col, row)}"
-            class="response-box flipper">
-            <div @click="toggleDisplay(col, row)" class="response-view">
-              <span :class="errorClass(col, row)" class="response-text">
-                {{ answer }}
-              </span>
+            :class="{'flip': isSelected(col, row)}"
+            class="response-box">
+            <div @click="flip(col, row)" class="response-view">
+              <span :class="errorClass(col, row)">{{ answer }}</span>
               <span
                 v-show="allowRemove(col, row)"
                 @click.stop="removeAnswer(col, row)"
@@ -43,15 +35,13 @@
             <input
               v-model="answerGroup(col)[row]"
               v-focus="{ col, row }"
-              @keyup.esc="allowFlip(col, row) && toggleDisplay(col, row)"
-              @keyup.enter="allowFlip(col, row) && toggleDisplay(col, row)"
-              @blur="allowFlip(col, row) && toggleDisplay(col, row)"
-              class="form-control response-input"
-              type="text">
+              @keyup.esc="flip(col, row)"
+              @keyup.enter="flip(col, row)"
+              @blur="isSelected(col, row) && flip(col, row)"
+              class="form-control response-input">
           </li>
           <span
-            @click="addAnswer(col)"
-            class="fa fa-plus btn btn-link pull-center add-answer">
+            @click="addAnswer(col)" class="fa fa-plus btn btn-link add-answer">
           </span>
         </ul>
       </div>
@@ -64,12 +54,26 @@
 
 <script>
 import cloneDeep from 'lodash/cloneDeep';
+import isEmpty from 'lodash/isEmpty';
 import times from 'lodash/times';
 
 const defaultAnswer = 'Response item..';
 const defaultHead = 'Drop spot..';
 
 export default {
+  directives: {
+    focus: {
+      update(el, binding, vnode) {
+        const flippedCol = vnode.context.flipped.col;
+        const flippedRow = vnode.context.flipped.row;
+        const selectedCol = binding.value.col;
+        const selectedRow = binding.value.row;
+        flippedCol === selectedCol && flippedRow === selectedRow
+          ? el.focus()
+          : el.blur();
+      }
+    }
+  },
   props: {
     assessment: Object,
     errors: Array,
@@ -90,95 +94,78 @@ export default {
     }
   },
   computed: {
-    dropSpots() {
-      return this.assessment.correct;
-    },
     disabled() {
       return !this.isEditing;
+    },
+    dropSpots() {
+      return this.assessment.correct;
     },
     lgCol() {
       return this.dropSpots.length === 2 ? 'col-lg-6' : 'col-lg-4';
     }
   },
   methods: {
-    update(message) {
-      this.$emit('update', { correct: this.dropSpots });
-    },
-    answerGroup(col) {
-      return this.dropSpots[col].answers;
-    },
     addAnswer(col) {
       this.answerGroup(col).push(defaultAnswer);
       this.update();
     },
     addDropSpot() {
-      this.dropSpots.push({
-        heading: defaultHead,
-        answers: [defaultAnswer] });
-      this.update();
-    },
-    removeAnswer(col, row) {
-      this.answerGroup(col).splice(row, 1);
-      this.$emit('syncErrors');
-      this.update();
-    },
-    removeDropSpot(col) {
-      this.dropSpots.splice(col, 1);
-      this.$emit('syncErrors');
+      this.dropSpots.push({ heading: defaultHead, answers: [defaultAnswer] });
       this.update();
     },
     allowRemove(col, row) {
-      return row !== undefined
-        ? this.dropSpots[col].answers.length > 1
-        : this.dropSpots.length > 2;
+      if (row !== undefined) return this.dropSpots[col].answers.length > 1;
+      return this.dropSpots.length > 2;
     },
-    allowFlip(col, row) {
-      const flipped = this.flipped;
-      return row === undefined
-        ? flipped.col === col && flipped.row === null
-        : flipped.col === col && flipped.row === row;
-    },
-    toggleDisplay(col, row) {
-      if (this.flipped.col === col &&
-         (this.flipped.row === row || this.flipped.row === null)) {
-        this.flipped = { col: null, row: null };
-        return;
-      }
-      this.flipped = row !== undefined ? { col, row } : { col, row: null };
+    answerGroup(col) {
+      return this.dropSpots[col].answers;
     },
     errorClass(col, row) {
       const answer = row === undefined
         ? `correct[${col}].heading`
         : `correct[${col}].answers[${row}]`;
       return { 'error': this.localErrors.includes(answer) };
+    },
+    flip(col, row = null) {
+      if (this.isSelected(col, row)) {
+        this.flipped = { col: null, row: null };
+      } else {
+        this.flipped = row !== null ? { col, row } : { col, row: null };
+      }
+      this.syncErrors();
+    },
+    isSelected(col, row = null) {
+      return this.flipped.col === col && this.flipped.row === row;
+    },
+    removeAnswer(col, row) {
+      this.answerGroup(col).splice(row, 1);
+      this.update();
+    },
+    removeDropSpot(col) {
+      this.dropSpots.splice(col, 1);
+      this.update();
+    },
+    syncErrors() {
+      if (!isEmpty(this.localErrors)) this.$emit('syncErrors');
+    },
+    update(message) {
+      this.$emit('update', { correct: this.dropSpots });
     }
   },
   watch: {
+    dropSpots() {
+      this.syncErrors();
+    },
     errors() {
       this.localErrors = this.errors;
-    }
-  },
-  directives: {
-    focus: {
-      update(el, binding, vnode) {
-        const flipCol = vnode.context.flipped.col;
-        const flipRow = vnode.context.flipped.row;
-        const col = binding.value.col;
-        const row = binding.value.row;
-        if (flipCol === col && flipRow === row) el.focus();
-      }
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.flipper{
-  transition: 0.5s;
-	transform-style: preserve-3d;
-}
-
-.flipper.flip{
+.heading-box.flip,
+.response-box.flip {
   transform: rotateX(180deg);
 }
 
@@ -203,7 +190,6 @@ ul {
 
   li {
     margin: 13px 0;
-    width: 100%;
   }
 }
 
@@ -219,6 +205,15 @@ ul {
   margin-top: 141px;
 }
 
+.destroy {
+  position: absolute;
+  right: 11px;
+  visibility: hidden;
+  opacity: 0.5;
+  border: 0;
+  background-color: transparent;
+}
+
 .heading-view,
 .response-view {
   white-space: nowrap;
@@ -230,18 +225,15 @@ ul {
   transform: rotateX(0deg);
 }
 
-.destroy {
-  position: absolute;
-  right: 11px;
-  visibility: hidden;
-  opacity: 0.5;
-  border: 0;
-  background-color: transparent;
+.heading-box,
+.response-box {
+  position: relative;
+  transition: 0.5s;
+  transform-style: preserve-3d;
 }
 
 .heading-box {
   min-height: 52px;
-  position: relative;
 
   .heading-view {
     height: 100%;
@@ -257,7 +249,6 @@ ul {
 
 .response-box {
   min-height: 46px;
-  position: relative;
 
   .response-view {
     background-color: rgb(217, 217, 217);
@@ -270,18 +261,18 @@ ul {
   }
 }
 
+.response-box:hover,
+.heading-box:hover {
+  .destroy {
+    visibility: visible;
+  }
+}
+
 .response-input,
 .heading-input {
   min-height: inherit;
   position: absolute;
   backface-visibility: hidden;
   transform: rotateX( 180deg );
-}
-
-.response-box:hover,
-.heading-box:hover {
-  .destroy {
-    visibility: visible;
-  }
 }
 </style>
