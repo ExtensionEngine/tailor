866e60526269c31e7a2e6cf03bc10a788cf27578
Ilija Veselica <ilija@extensionengine.com>
2017-03-24 16:31:02 +0100

    password reset validation (CA-89)

--- client/components/auth/ResetPassword.vue
+++ client/components/auth/ResetPassword.vue
@@ -1,20 +1,24 @@
 <template>
   <div>
-    <div class="message"><span v-if="message">{{ message }}</span></div>
     <form @submit.prevent="submit">
-      <div class="form-group">
+      <div class="form-group" :class="{'has-error': vErrors.has('password') }">
         <input
           v-model="password"
+          v-validate="'required|min:6'"
           class="form-control"
           type="password"
+          name="password"
           placeholder="Password"/>
+          <span v-show="vErrors.has('password')" class="help-block">{{ vErrors.first('password') }}</span>
       </div>
-      <div class="form-group">
+      <div class="form-group" :class="{'has-error': vErrors.has('password-match') }">
         <input
-          v-model="passwordConfirmation"
+          v-validate="'confirmed:password'"
           class="form-control"
           type="password"
+          name="password-match"
           placeholder="Please re enter your password"/>
+          <span v-show="vErrors.has('password-match')" class="help-block">{{ vErrors.first('password-match') }}</span>
       </div>
       <button type="submit" class="btn btn-default btn-block">
         Change password
@@ -29,24 +33,15 @@ import { mapActions } from 'vuex-module';
 export default {
   data() {
     return {
-      password: '',
-      passwordConfirmation: '',
-      message: ''
+      password: ''
     };
   },
   methods: {
     ...mapActions(['resetPassword']),
-    isValid() {
-      this.message = '';
-      let valid = this.password === this.passwordConfirmation;
-      if (!valid) this.message = 'Invalid password';
-      return valid;
-    },
     submit() {
-      if (!this.isValid()) return;
-      this.resetPassword({ password: this.password, token: this.$route.params.token })
-        .then(() => this.$router.push('/'))
-        .catch(() => (this.message = 'Error'));
+      this.$validator.validateAll()
+        .then(() => { return this.resetPassword({ password: this.password, token: this.$route.params.token }) })
+        .then(() => this.$router.push('/'));
     }
   }
 };
