819cf0a45c258204a735fe60578b858bde2b4218
Ilija Veselica <ilija@extensionengine.com>
2017-04-03 15:26:02 +0200

    minor cleanup, added default error message

--- client/components/auth/ResetPassword.vue
+++ client/components/auth/ResetPassword.vue
@@ -4,27 +4,30 @@
       <div class="form-group" :class="{ 'has-error': vErrors.has('password') }">
         <input
           v-model="password"
-          v-validate="'required|min:6'"
+          v-validate="'required|min:6|alpha_num'"
           class="form-control"
           type="password"
           name="password"
           placeholder="Password"/>
-        <span v-show="vErrors.has('password')" class="help-block">
+        <span class="help-block">
           {{ vErrors.first('password') }}
         </span>
       </div>
-      <div class="form-group" :class="{'has-error': vErrors.has('password-match') }">
+      <div class="form-group" :class=" {'has-error': vErrors.has('passwordMatch') }">
         <input
           v-validate="'confirmed:password'"
           class="form-control"
           type="password"
-          name="password-match"
+          name="passwordMatch"
           placeholder="Please re enter your password"/>
-          <span v-show="vErrors.has('password-match')" class="help-block">{{ vErrors.first('password-match') }}</span>
+          <span class="help-block">{{ vErrors.first('passwordMatch') }}</span>
       </div>
       <button type="submit" class="btn btn-default btn-block">
         Change password
       </button>
+      <div class="errorMessage">
+        <span v-if="errorMessage">{{ errorMessage }}</span>
+      </div>
     </form>
   </div>
 </template>
@@ -35,16 +38,31 @@ import { mapActions } from 'vuex-module';
 export default {
   data() {
     return {
-      password: ''
+      password: '',
+      errorMessage: ''
     };
   },
   methods: {
     ...mapActions(['resetPassword']),
     submit() {
+      this.errorMessage = '';
       this.$validator.validateAll()
-        .then(() => { return this.resetPassword({ password: this.password, token: this.$route.params.token }) })
-        .then(() => this.$router.push('/'));
+        .then(() => {
+          return this.resetPassword({
+            password: this.password,
+            token: this.$route.params.token
+          });
+        })
+        .then(() => this.$router.push('/'))
+        .catch(() => { this.errorMessage = 'An error has occurred!'; });
     }
   }
 };
 </script>
+
+<style>
+.errorMessage {
+  margin-top: 10px;
+  color: #a94442;
+}
+</style>
