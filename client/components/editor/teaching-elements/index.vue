e58c25e9c0db179a9c315de9d793e3a6c288bb6a
Marko Žarković <marko@kastela.com>
2017-04-21 17:16:02 +0200

    Replace the 'update' action in teaching element with the 'save'.

--- client/components/editor/teaching-elements/index.vue
+++ client/components/editor/teaching-elements/index.vue
@@ -66,7 +66,7 @@ export default {
     }
   },
   methods: {
-    ...mapActions({ updateElement: 'update' }, 'tes'),
+    ...mapActions({ saveElement: 'save' }, 'tes'),
     ...mapMutations(['focusElement'], 'editor'),
     resolveElement(type) {
       return TE_TYPES[type];
@@ -84,7 +84,7 @@ export default {
         this.$emit('save', { ...this.element, data });
       } else {
         Object.assign(this.element.data, elementData);
-        this.updateElement(this.element);
+        this.saveElement(this.element);
       }
     }
   },
