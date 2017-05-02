af2061f4e7d1f5aed0c5545621ae01e8603c6975
underscope <underscope@gmail.com>
2017-05-02 13:23:36 +0200

    Created Feedback component :wrench:

---
+++ client/components/editor/teaching-elements/Assessment/Feedback.vue
@@ -0,0 +1,96 @@
+<template>
+  <div class="feedback">
+    <span class="heading">
+      Feedback
+      <span
+        @click="isExpanded = !isExpanded"
+        class="btn btn-link btn-xs">
+        {{ isExpanded ? 'hide' : 'show' }}
+      </span>
+    </span>
+    <transition name="fade">
+      <ul v-if="isExpanded">
+        <li v-for="(answer, index) in processedAnswers">
+          <div>Answer {{ index + 1 }}: {{ answer }}</div>
+          <textarea
+            :ref="'option' + index"
+            :value="feedback ? feedback[index] : ''"
+            @change="update(index)"
+            class="form-control">
+          </textarea>
+        </li>
+      </ul>
+    </transition>
+  </div>
+</template>
+
+<script>
+import cloneDeep from 'lodash/cloneDeep';
+import isArray from 'lodash/isArray';
+
+export default {
+  name: 'feedback',
+  props: {
+    answers: [Array, Boolean],
+    feedback: Object
+  },
+  data() {
+    return {
+      isExpanded: true
+    };
+  },
+  computed: {
+    processedAnswers() {
+      return isArray(this.answers) ? this.answers : ['True', 'False'];
+    }
+  },
+  methods: {
+    update(index) {
+      let options = this.options ? cloneDeep(this.options) : {};
+      options[index] = this.$refs['option' + index][0].value;
+      this.$emit('save', options);
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.feedback {
+  width: 100%;
+  margin: 0 auto;
+  padding: 30px 20px 15px 20px;
+  text-align: left;
+
+  .heading {
+    font-size: 20px;
+  }
+
+  ul {
+    margin-top: 20px;
+    list-style: none;
+  }
+
+  li {
+    padding: 10px 0;
+    font-size: 15px;
+  }
+
+  textarea {
+    height: 60px;
+    margin: 10px 0;
+    resize: none;
+  }
+
+  .fade-enter-active {
+    transition: opacity .5s
+  }
+
+  .fade-enter {
+    opacity: 0
+  }
+
+  .fade-leave, .fade-leave-active, .fade-leave-to {
+    display: none;
+  }
+}
+</style>
