6a9eabc83f101b9b4f71a59bf7bae72404931d4f
underscope <underscope@gmail.com>
2017-05-15 06:24:47 +0200

    Added script for integration user creation

---
+++ server/script/addIntegration.js
@@ -0,0 +1,20 @@
+'use strict';
+
+const { User } = require('../shared/database');
+const { user: role } = require('../../config/shared').role;
+
+User.findOne({ where: { role: role.INTEGRATION } })
+  .then(user => {
+    if (!user) return true;
+    console.log('Integration already exists');
+    process.exit(0);
+  })
+  .then(() => User.create({ role: role.INTEGRATION }))
+  .then(user => {
+    console.log(`Integration user created: ${user.id}`);
+    process.exit(0);
+  })
+  .catch(err => {
+    console.error(err.message);
+    process.exit(1);
+  });
