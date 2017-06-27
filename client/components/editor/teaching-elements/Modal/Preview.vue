81084cae900adfc881ab1d40506a690cb77664a4
Nediljko Krstic <nediljko@extensionengine.com>
2017-06-27 10:43:01 +0200

    Modal image size fix

--- client/components/editor/teaching-elements/Modal/Preview.vue
+++ client/components/editor/teaching-elements/Modal/Preview.vue
@@ -1,11 +1,5 @@
 <template>
   <div>
-    <button
-      @click="showModal = true"
-      class="btn btn-primary btn-open"
-      type="button">
-      {{ title }}
-    </button>
     <modal :show.sync="showModal" :backdrop="false">
       <div slot="modal-header"></div>
       <div slot="modal-body" class="modal-body">
@@ -19,7 +13,7 @@
         </div>
       </div>
       <div slot="modal-footer" class="modal-footer">
-        <button @click="showModal = false" class="btn btn-default" type="button">
+        <button @click="close()" class="btn btn-default" type="button">
           Close
         </button>
       </div>
@@ -33,11 +27,20 @@ import Primitive from '../Primitive';
 
 export default {
   name: 'te-modal-preview',
-  props: ['title', 'elements'],
+  props: ['elements'],
   data() {
     return {
       showModal: false
     };
+  },
+  mounted() {
+    this.showModal = true;
+  },
+  methods: {
+    close() {
+      this.showModal = false;
+      this.$emit('close');
+    }
   },
   components: {
     modal,
@@ -45,14 +48,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.btn-open {
-  max-width: 90%;
-  padding: 9px 20px;
-  font-family: 'Helvetica Neue', Arial, sans-serif;
-  text-overflow: ellipsis;
-  white-space: nowrap;
-  overflow: hidden;
-}
-</style>
