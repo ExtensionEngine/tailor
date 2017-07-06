6425269093aa6be33f6b5fccd106bdc9239890ae
underscope <underscope@gmail.com>
2017-07-06 08:55:37 +0200

    Removed unnecessary root :wrench:

--- client/components/editor/teaching-elements/Modal/Preview.vue
+++ client/components/editor/teaching-elements/Modal/Preview.vue
@@ -1,24 +1,22 @@
 <template>
-  <div>
-    <modal :show.sync="visible" :backdrop="false">
-      <div slot="modal-header"></div>
-      <div slot="modal-body" class="modal-body">
-        <div class="row">
-          <primitive
-            v-for="it in elements"
-            :key="it.id"
-            :initialElement="it"
-            :disabled="true">
-          </primitive>
-        </div>
+  <modal :show.sync="visible" :backdrop="false">
+    <div slot="modal-header"></div>
+    <div slot="modal-body" class="modal-body">
+      <div class="row">
+        <primitive
+          v-for="it in elements"
+          :key="it.id"
+          :initialElement="it"
+          :disabled="true">
+        </primitive>
       </div>
-      <div slot="modal-footer" class="modal-footer">
-        <button @click="visible = false" class="btn btn-default" type="button">
-          Close
-        </button>
-      </div>
-    </modal>
-  </div>
+    </div>
+    <div slot="modal-footer" class="modal-footer">
+      <button @click="visible = false" class="btn btn-default" type="button">
+        Close
+      </button>
+    </div>
+  </modal>
 </template>
 
 <script>
