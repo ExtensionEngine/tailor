48c29eba3e90348fab57dba77ca83c32c85bdfb3
Dario Vladovic <d.vladimyr@gmail.com>
2017-07-07 19:38:12 +0200

    Refactoring migration script :wrench:

--- server/script/migrate.js
+++ server/script/migrate.js
@@ -1,33 +1,52 @@
 'use strict';
 
-const isEqual = require('lodash/isEqual');
-const exec = require('child_process').exec;
-const args = process.argv.slice(2);
+const { exec } = require('child_process');
+const map = require('lodash/map');
+const { slug } = require('to-case');
 
-let cmd = ['sequelize'];
-const cmdName = args[0];
-const migrationName = args[1];
-const configOption = '--config "./config/server/database.js"';
-const migrationsPathOption = '--migrations-path "./server/shared/database/migrations"';
+const argv = process.argv.slice(2);
 
-cmd = cmdName === 'create' ? [...cmd, 'migration:create'] : cmd;
-cmd = cmdName === 'create' && migrationName ? [...cmd, `--name="${migrationName}"`] : cmd;
-cmd = cmdName === 'up' ? [...cmd, 'db:migrate'] : cmd;
-cmd = cmdName === 'down' ? [...cmd, 'db:migrate:undo'] : cmd;
+const options = {
+  config: './config/server/database.js',
+  migrationsPath: './server/shared/database/migrations'
+};
 
-function printUsage() {
-  console.log('Invalid usage. Valid commands are:');
-  console.log('\tmigrate create [migration-name]');
-  console.log('\tmigrate up');
-  console.log('\tmigrate down');
-  process.exit();
+let command = getCommand(argv, options);
+if (!command) {
+  printUsage();
+  process.exit(0);
 }
 
-cmd = !isEqual(cmd, ['sequelize'])
-  ? [...cmd, configOption, migrationsPathOption]
-  : printUsage();
+command.unshift('sequelize');
+const str = command.join(' ');
 
-exec(cmd.join(' '), error => {
-  if (error) console.log(error);
-  else console.log('Command executed successfully!');
+exec(str, (err, stdout, stderr) => {
+  if (!err) {
+    process.stdout.write(stdout);
+    process.stderr.write(stderr);
+    return;
+  }
+  console.error(err.message);
+  console.error(err.stack);
+  process.exit(1);
 });
+
+function printUsage() {
+  console.log(`Invalid usage. Valid commands are:\n
+  migrate create [migration-name]
+  migrate up
+  migrate down`);
+}
+
+function getCommand([command, arg], options) {
+  if (command === 'up') return ['db:migrate', ...getFlags(options)];
+  if (command === 'down') return ['db:migrate:undo', ...getFlags(options)];
+  if (command === 'create') {
+    if (arg) options.name = arg;
+    return ['migration:create', ...getFlags(options)];
+  }
+}
+
+function getFlags(options) {
+  return map(options, (value, name) => `--${slug(name)}="${value}"`);
+}
