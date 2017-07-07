69d7e2a275bd6f3e656ac7e359d7e9b90031a756
Marko Žarković <marko@kastela.com>
2017-07-07 15:26:52 +0200

    :koala: Make the migration script prettier.

--- server/script/migrate.js
+++ server/script/migrate.js
@@ -1,29 +1,33 @@
 'use strict';
 
+const isEqual = require('lodash/isEqual');
 const exec = require('child_process').exec;
 const args = process.argv.slice(2);
 
-let cmd = 'sequelize ';
-const configOption = ' --config "./config/server/database.js"';
-const migrationsPathOption = ' --migrations-path "./server/shared/database/migrations"';
+let cmd = ['sequelize'];
+const cmdName = args[0];
+const migrationName = args[1];
+const configOption = '--config "./config/server/database.js"';
+const migrationsPathOption = '--migrations-path "./server/shared/database/migrations"';
 
-if (args[0] === 'create') {
-  const migrationName = args[1];
-  const nameOption = migrationName ? ` --name="${migrationName}"` : '';
-  cmd = cmd + 'migration:create' + configOption + migrationsPathOption + nameOption;
-} else if (args[0] === 'up') {
-  cmd = cmd + 'db:migrate' + configOption + migrationsPathOption;
-} else if (args[0] === 'down') {
-  cmd = cmd + 'db:migrate:undo' + configOption + migrationsPathOption;
-} else {
+cmd = cmdName === 'create' ? [...cmd, 'migration:create'] : cmd;
+cmd = cmdName === 'create' && migrationName ? [...cmd, `--name="${migrationName}"`] : cmd;
+cmd = cmdName === 'up' ? [...cmd, 'db:migrate'] : cmd;
+cmd = cmdName === 'down' ? [...cmd, 'db:migrate:undo'] : cmd;
+
+function printUsage() {
   console.log('Invalid usage. Valid commands are:');
-  console.log('migrate create [migration-name]');
-  console.log('migrate up');
-  console.log('migrate down');
+  console.log('\tmigrate create [migration-name]');
+  console.log('\tmigrate up');
+  console.log('\tmigrate down');
   process.exit();
 }
 
-exec(cmd, error => {
+cmd = !isEqual(cmd, ['sequelize'])
+  ? [...cmd, configOption, migrationsPathOption]
+  : printUsage();
+
+exec(cmd.join(' '), error => {
   if (error) console.log(error);
-  else console.log('Command successfully executed!');
+  else console.log('Command executed successfully!');
 });
