4a0a091dc96ed5dd25f0a542667fd72331616b36
Dario Vladovic <d.vladimyr@gmail.com>
2017-08-16 18:56:00 +0200

    New features :nada:
    - adding multi answer support
    - adding support for prefixes and suffixes

--- client/components/editor/teaching-elements/Assessment/NumericalResponse.vue
+++ client/components/editor/teaching-elements/Assessment/NumericalResponse.vue
@@ -1,41 +1,121 @@
 <template>
   <div class="form-group">
-    <span class="form-label">Answer</span>
-    <span :class="{ 'has-error': correctError }" class="answer">
-      <input
-        v-model="correct"
-        :disabled="!isEditing"
-        @blur="update"
-        class="form-control">
-        <span class="help-block">
-          Only numerical input allowed, if decimal number is needed please
-          use '.' to separate numbers (e.g. '3.14').
-        </span>
+    <span class="form-label">Answers</span>
+    <span
+      v-if="isEditing"
+      @click="addAnswer"
+      class="btn-add-answer btn btn-link mdi mdi-plus pull-right">
     </span>
+    <ul >
+      <li
+        v-for="(answer, index) in answers"
+        :class="{ 'has-error': isValidAnswer(index) }"
+        class="answer row">
+        <div class="col-xs-3">
+          <input
+            :disabled="!isEditing"
+            :value="prefixes[index]"
+            @input="updateAnswer('prefixes', $event.target.value, index)"
+            type="text"
+            placeholder="Prefix..."
+            class="form-control">
+        </div>
+        <div class="col-xs-5">
+          <input
+            :disabled="!isEditing"
+            :value="correct[index]"
+            @input="updateAnswer('correct', $event.target.value, index)"
+            type="text"
+            placeholder="Correct value..."
+            class="form-control">
+        </div>
+        <div class="col-xs-3">
+          <input
+            :disabled="!isEditing"
+            :value="suffixes[index]"
+            @input="updateAnswer('suffixes', $event.target.value, index)"
+            type="text"
+            placeholder="Suffix..."
+            class="form-control">
+        </div>
+        <div class="col-xs-1">
+          <span
+            v-if="isEditing && answers.length > 1"
+            @click="removeAnswer(index)"
+            class="btn-remove mdi mdi-close">
+          </span>
+        </div>
+      </li>
+    </ul>
+    <div :class="{ 'has-error': !isValid }">
+      <span class="help-block">
+        Only numerical input allowed, if decimal number is needed please
+        use '.' to separate numbers (e.g. '3.14').
+      </span>
+    </div>
   </div>
 </template>
 
 <script>
+import cloneDeep from 'lodash/cloneDeep';
+import find from 'lodash/find';
+import get from 'lodash/get';
+import includes from 'lodash/includes';
+import pullAt from 'lodash/pullAt';
+import startsWith from 'lodash/startsWith';
+
 export default {
   props: {
     assessment: Object,
     errors: Array,
     isEditing: Boolean
   },
-  data() {
-    return {
-      correct: this.assessment.correct
-    };
-  },
   computed: {
-    correctError() {
-      return this.errors.includes('correct');
+    isValid() {
+      return !find(this.errors, err => startsWith(err, 'correct'));
+    },
+    answers() {
+      return get(this.assessment, 'answers', []);
+    },
+    correct() {
+      return get(this.assessment, 'correct', []);
+    },
+    prefixes() {
+      return get(this.assessment, 'prefixes', []);
+    },
+    suffixes() {
+      return get(this.assessment, 'suffixes', []);
     }
   },
   methods: {
-    update() {
-      let data = { correct: this.correct };
-      this.$emit('update', data);
+    isValidAnswer(index) {
+      return includes(this.errors, `correct[${index}]`);
+    },
+    addAnswer() {
+      let { answers, correct, prefixes, suffixes } = cloneDeep(this.assessment);
+      answers.push('');
+      prefixes.push('');
+      suffixes.push('');
+      correct.push('');
+      this.update({ answers, prefixes, suffixes, correct });
+    },
+    updateAnswer(name, value, index) {
+      let values = cloneDeep(this[name]);
+      if (name === 'correct') value = Number(value) || value;
+      values[index] = value;
+      this.update({ [name]: values });
+    },
+    removeAnswer(index) {
+      if (this.answers.length <= 1) return;
+      let { answers, correct, prefixes, suffixes } = cloneDeep(this.assessment);
+      pullAt(answers, index);
+      pullAt(prefixes, index);
+      pullAt(suffixes, index);
+      pullAt(correct, index);
+      this.update({ answers, prefixes, suffixes, correct });
+    },
+    update(data) {
+      this.$emit('update', data, true /* validate */);
     }
   },
   watch: {
@@ -56,16 +136,44 @@ export default {
 }
 
 .form-label {
+  display: inline-block;
+  padding: 5px;
   font-size: 20px;
 }
 
+.btn-add-answer {
+  font-size: 20px;
+  padding: 4px 10px;
+  border-radius: 2px;
+}
+
+ul {
+  padding: 0;
+  list-style: none;
+}
+
 .answer {
-  padding: 10px 0 0 50px;
   font-size: 16px;
-  margin: 10px 0;
+  margin: 20px 0;
+
+  .btn-remove {
+    display: inline-block;
+    height: 34px;
+    line-height: 34px;
+    cursor: pointer;
+    color: #888;
+
+    &:hover {
+      color: darken(#888, 20%);
+    }
+  }
 }
 
 input.form-control {
   padding-left: 10px;
+}
+
+.help-block {
+  margin-top: 30px;
 }
 </style>
