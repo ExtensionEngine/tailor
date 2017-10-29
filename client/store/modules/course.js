26da3fec87b023500e73a76d98cfa1d1f0a63bc8
underscope <underscope@gmail.com>
2017-10-29 20:20:01 +0100

    Add structure getter to the course module

--- client/store/modules/course.js
+++ client/store/modules/course.js
@@ -1,5 +1,6 @@
 import filter from 'lodash/filter';
 import find from 'lodash/find';
+import { getOutlineLevels } from 'shared/activities';
 import values from 'lodash/values';
 import courseApi from '../../api/course';
 import Vue from 'vue';
@@ -20,6 +21,14 @@ getter(function course() {
   if (!route.fullPath.match(COURSE_ROUTE)) return;
   const id = Number(route.params.courseId);
   return find(courses, { id });
+});
+
+getter(function structure() {
+  const { route } = this.rootState;
+  const { courses } = this.rootGetters;
+  if (!route.fullPath.match(COURSE_ROUTE)) return [];
+  const course = find(courses, { id: Number(route.params.courseId) });
+  return getOutlineLevels(course.schema);
 });
 
 getter(function activities() {
