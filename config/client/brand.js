aeeb21fc8aa68e4ccf56670620bf737bee7ed5a0
Toma Zelić <zelic.toma@gmail.com>
2017-10-31 10:16:41 +0100

    Improve logo/favicon files configuring

--- config/client/brand.js
+++ config/client/brand.js
@@ -2,27 +2,29 @@ const brandConfig = require('rcfile')('brand-');
 const map = require('lodash/map');
 const merge = require('lodash/merge');
 
+const imagesPath = '/assets/img';
+
 const defaultGlobals = {
   title: {
-    full: 'Tailor Content Authoring',
+    full: 'Tailor Author',
     short: 'Tailor',
     sub: 'Content Authoring'
   },
   logo: {
-    compact: '/assets/img/tailor-logo-compact.svg',
-    full: '/assets/img/tailor.svg'
+    compact: 'default-logo-compact.svg',
+    full: 'default-logo-full.svg'
   },
-  favicon: '/assets/favicon.ico'
+  favicon: 'default-favicon.ico'
 };
 
 const defaultStyle = {
-  brandColor: 'blue',
-  altBrandColor: 'grey'
+  brandColor: '#3949AB',
+  altBrandColor: '#5C6BC0'
 };
 
-const config = merge(brandConfig, defaultGlobals);
-const styleConstants = merge(brandConfig.style, defaultStyle);
+const config = merge(defaultGlobals, brandConfig);
 
+const styleConstants = merge(defaultStyle, brandConfig.style);
 const style = map(styleConstants, (val, key) => `$${key}: ${val};`).join('\n');
 
 module.exports = {
@@ -30,9 +32,9 @@ module.exports = {
     TITLE_FULL: config.title.full,
     TITLE_SHORT: config.title.short,
     TITLE_SUB: config.title.sub,
-    LOGO_COMPACT: config.logo.compact,
-    LOGO_FULL: config.logo.compact,
-    FAVICON: config.favicon
+    LOGO_COMPACT: `${imagesPath}/${config.logo.compact}`,
+    LOGO_FULL: `${imagesPath}/${config.logo.full}`,
+    FAVICON: `${imagesPath}/${config.favicon}`
   },
   style
 };
