12770e5614aaf853c1e90d05681032b57d514343
underscope <underscope@gmail.com>
2017-10-31 11:57:18 +0100

    Prefixing with schema upon migration

--- server/shared/database/migrations/20171028170417-course-schema.js
+++ server/shared/database/migrations/20171028170417-course-schema.js
@@ -1,8 +1,29 @@
+const Promise = require('bluebird');
+
+const DEFAULT_SCHEMA = 'COURSE';
+
 module.exports = {
-  up(queryInterface, Sequelize) {
-    return queryInterface.addColumn('course', 'schema', {
-      type: Sequelize.STRING
-    });
+  up(queryInterface, Sequelize, { Course, Activity }) {
+    return Activity.findAll()
+      .then(activities => {
+        return Promise.map(activities, it => {
+          it.type = `${DEFAULT_SCHEMA}/${it.type}`;
+          return it.save();
+        });
+      })
+      .then(() => {
+        return queryInterface.addColumn('course', 'schema', {
+          type: Sequelize.STRING
+        });
+      })
+      .then(() => {
+        return Course.findAll().then(courses => {
+          return Promise.map(courses, it => {
+            it.schema = DEFAULT_SCHEMA;
+            return it.save();
+          });
+        });
+      });
   },
   down(queryInterface, Sequelize) {
     return queryInterface.removeColumn('course', 'schema');
