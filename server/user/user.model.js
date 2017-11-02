0795cc09ffda923c3d8a627377d78248b81bbd08
Toma Zelić <zelic.toma@gmail.com>
2017-11-02 15:11:31 +0100

    Refactor User model to ES6 class

--- server/user/user.model.js
+++ server/user/user.model.js
@@ -2,97 +2,57 @@ const { user: role } = require('../../config/shared').role;
 const config = require('../../config/server');
 const jwt = require('jsonwebtoken');
 const mail = require('../shared/mail');
+const { Model } = require('sequelize');
 const Promise = require('bluebird');
 
 const bcrypt = Promise.promisifyAll(require('bcryptjs'));
 const AUTH_SECRET = process.env.AUTH_JWT_SECRET;
 
-module.exports = function (sequelize, DataTypes) {
-  const User = sequelize.define('user', {
-    email: {
-      type: DataTypes.STRING,
-      validate: { isEmail: true },
-      unique: { msg: 'The specified email address is already in use.' }
-    },
-    password: {
-      type: DataTypes.STRING,
-      validate: { notEmpty: true, len: [5, 100] }
-    },
-    role: {
-      type: DataTypes.ENUM(role.ADMIN, role.USER, role.INTEGRATION),
-      defaultValue: role.USER
-    },
-    token: {
-      type: DataTypes.STRING,
-      unique: true
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      field: 'created_at'
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      field: 'updated_at'
-    },
-    deletedAt: {
-      type: DataTypes.DATE,
-      field: 'deleted_at'
-    }
-  }, {
-    getterMethods: {
-      profile() {
-        return {
-          id: this.id,
-          email: this.email,
-          role: this.role
-        };
+class User extends Model {
+  static fields(DataTypes) {
+    const { DATE, ENUM, STRING } = DataTypes;
+    return {
+      email: {
+        type: STRING,
+        validate: { isEmail: true },
+        unique: { msg: 'The specified email address is already in use.' }
+      },
+      password: {
+        type: STRING,
+        validate: { notEmpty: true, len: [5, 100] }
+      },
+      role: {
+        type: ENUM(role.ADMIN, role.USER, role.INTEGRATION),
+        defaultValue: role.USER
+      },
+      token: {
+        type: STRING,
+        unique: true
+      },
+      createdAt: {
+        type: DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        type: DATE,
+        field: 'updated_at'
+      },
+      deletedAt: {
+        type: DATE,
+        field: 'deleted_at'
       }
-    },
-    classMethods: {
-      associate(models) {
-        User.belongsToMany(models.Course, {
-          through: models.CourseUser,
-          foreignKey: { name: 'userId', field: 'user_id' }
-        });
-      },
-      invite(user) {
-        return User.create(user).then(user => {
-          user.token = user.createToken({ expiresIn: '5 days' });
-          mail.invite(user);
-          return user.save();
-        });
-      }
-    },
-    instanceMethods: {
-      isAdmin() {
-        return this.role === role.ADMIN || this.role === role.INTEGRATION;
-      },
-      authenticate(password) {
-        if (!this.password) return Promise.resolve(false);
-        return bcrypt
-          .compare(password, this.password)
-          .then(match => match ? this : false);
-      },
-      encrypt(val) {
-        return bcrypt.hash(val, config.auth.saltRounds);
-      },
-      encryptPassword() {
-        if (!this.password) return Promise.resolve(false);
-        return this
-          .encrypt(this.password)
-          .then(pw => (this.password = pw));
-      },
-      createToken(options = {}) {
-        const payload = { id: this.id, email: this.email };
-        return jwt.sign(payload, AUTH_SECRET, options);
-      },
-      sendResetToken() {
-        this.token = this.createToken({ expiresIn: '5 days' });
-        mail.resetPassword(this);
-        return this.save();
-      }
-    },
-    hooks: {
+    };
+  }
+
+  static associate({ Course, CourseUser }) {
+    User.belongsToMany(Course, {
+      through: CourseUser,
+      foreignKey: { name: 'userId', field: 'user_id' }
+    });
+  }
+
+  static hooks() {
+    return {
       beforeCreate(user) {
         return user.encryptPassword();
       },
@@ -106,12 +66,66 @@ module.exports = function (sequelize, DataTypes) {
         users.forEach(user => updates.push(user.encryptPassword()));
         return Promise.all(updates);
       }
-    },
-    underscored: true,
-    timestamps: true,
-    paranoid: true,
-    freezeTableName: true
-  });
+    };
+  }
 
-  return User;
-};
+  static options() {
+    return {
+      underscored: true,
+      timestamps: true,
+      paranoid: true,
+      freezeTableName: true
+    };
+  }
+
+  static invite(user) {
+    return User.create(user).then(user => {
+      user.token = user.createToken({ expiresIn: '5 days' });
+      mail.invite(user);
+      return user.save();
+    });
+  }
+
+  isAdmin() {
+    return this.role === role.ADMIN || this.role === role.INTEGRATION;
+  }
+
+  authenticate(password) {
+    if (!this.password) return Promise.resolve(false);
+    return bcrypt
+      .compare(password, this.password)
+      .then(match => match ? this : false);
+  }
+
+  encrypt(val) {
+    return bcrypt.hash(val, config.auth.saltRounds);
+  }
+
+  encryptPassword() {
+    if (!this.password) return Promise.resolve(false);
+    return this
+      .encrypt(this.password)
+      .then(pw => (this.password = pw));
+  }
+
+  createToken(options = {}) {
+    const payload = { id: this.id, email: this.email };
+    return jwt.sign(payload, AUTH_SECRET, options);
+  }
+
+  sendResetToken() {
+    this.token = this.createToken({ expiresIn: '5 days' });
+    mail.resetPassword(this);
+    return this.save();
+  }
+
+  profile() {
+    return {
+      id: this.id,
+      email: this.email,
+      role: this.role
+    };
+  }
+}
+
+module.exports = User;
