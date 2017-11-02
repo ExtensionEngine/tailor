8505496a97f6d6635fd12f98bb78a4e5686605c8
Toma Zelić <zelic.toma@gmail.com>
2017-11-02 14:08:58 +0100

    Refactor Course model to ES6 class

--- server/course/course.model.js
+++ server/course/course.model.js
@@ -1,84 +1,92 @@
 const hooks = require('./hooks');
+const { Model } = require('sequelize');
 
-module.exports = function (sequelize, DataTypes) {
-  const Course = sequelize.define('course', {
-    schema: {
-      type: DataTypes.STRING,
-      validate: { notEmpty: true, len: [2, 20] }
-    },
-    name: {
-      type: DataTypes.STRING,
-      validate: { notEmpty: true, len: [2, 250] }
-    },
-    description: {
-      type: DataTypes.TEXT,
-      validate: { notEmpty: true, len: [2, 2000] }
-    },
-    data: {
-      type: DataTypes.JSON,
-      defaultValue: {}
-    },
-    stats: {
-      type: DataTypes.JSON,
-      defaultValue: { objectives: 0, assessments: 0 }
-    },
-    createdAt: {
-      type: DataTypes.DATE,
-      field: 'created_at'
-    },
-    updatedAt: {
-      type: DataTypes.DATE,
-      field: 'updated_at'
-    },
-    deletedAt: {
-      type: DataTypes.DATE,
-      field: 'deleted_at'
-    }
-  }, {
-    classMethods: {
-      associate(models) {
-        Course.hasMany(models.Activity, {
-          foreignKey: { name: 'courseId', field: 'course_id' }
-        });
-        Course.hasMany(models.TeachingElement, {
-          foreignKey: { name: 'courseId', field: 'course_id' }
-        });
-        Course.belongsToMany(models.User, {
-          through: models.CourseUser,
-          foreignKey: { name: 'courseId', field: 'course_id' }
-        });
+class Course extends Model {
+  static fields(DataTypes) {
+    const { DATE, JSON, STRING, TEXT } = DataTypes;
+    return {
+      schema: {
+        type: STRING,
+        validate: { notEmpty: true, len: [2, 20] }
       },
-      addHooks(models) {
-        hooks.add(Course, models);
+      name: {
+        type: STRING,
+        validate: { notEmpty: true, len: [2, 250] }
       },
-      updateStats(id, key, value) {
-        return Course.findById(id)
-          .then(course => {
-            const stats = course.stats || {};
-            stats[key] = value;
-            return course.update({ stats });
-          });
+      description: {
+        type: TEXT,
+        validate: { notEmpty: true, len: [2, 2000] }
+      },
+      data: {
+        type: JSON,
+        defaultValue: {}
+      },
+      stats: {
+        type: JSON,
+        defaultValue: { objectives: 0, assessments: 0 }
+      },
+      createdAt: {
+        type: DATE,
+        field: 'created_at'
+      },
+      updatedAt: {
+        type: DATE,
+        field: 'updated_at'
+      },
+      deletedAt: {
+        type: DATE,
+        field: 'deleted_at'
       }
-    },
-    instanceMethods: {
-      getUser(user) {
-        return this.getUsers({ where: { id: user.id } })
-          .then(users => users[0]);
-      },
-      getInventoryItems() {
-        const where = { detached: false };
-        return Promise.all([
-          this.getActivities({ where }),
-          this.getTeachingElements({ where, order: [['activityId', 'ASC']] })
-        ])
-        .then(([activities, tes]) => ({ activities, tes }));
-      }
-    },
-    underscored: true,
-    timestamps: true,
-    paranoid: true,
-    freezeTableName: true
-  });
+    };
+  }
 
-  return Course;
-};
+  static associate({ Activity, CourseUser, TeachingElement, User }) {
+    Course.hasMany(Activity, {
+      foreignKey: { name: 'courseId', field: 'course_id' }
+    });
+    Course.hasMany(TeachingElement, {
+      foreignKey: { name: 'courseId', field: 'course_id' }
+    });
+    Course.belongsToMany(User, {
+      through: CourseUser,
+      foreignKey: { name: 'courseId', field: 'course_id' }
+    });
+  }
+
+  static hooks(models) {
+    hooks.add(Course, models);
+  }
+
+  static options() {
+    return {
+      underscored: true,
+      timestamps: true,
+      paranoid: true,
+      freezeTableName: true
+    };
+  }
+
+  static updateStats(id, key, value) {
+    return Course.findById(id).then(course => {
+      const stats = course.stats || {};
+      stats[key] = value;
+      return course.update({ stats });
+    });
+  }
+
+  getUser(user) {
+    return this.getUsers({ where: { id: user.id } })
+      .then(users => users[0]);
+  }
+
+  getInventoryItems() {
+    const where = { detached: false };
+    return Promise.all([
+      this.getActivities({ where }),
+      this.getTeachingElements({ where, order: [['activityId', 'ASC']] })
+    ])
+    .then(([activities, tes]) => ({ activities, tes }));
+  }
+}
+
+module.exports = Course;
