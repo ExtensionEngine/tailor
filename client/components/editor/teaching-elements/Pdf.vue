1b4d8179fe865db00cbb7b091e91a5d93010b5ef
Ilija Veselica <ilija@extensionengine.com>
2017-11-02 16:12:39 +0100

    Fixed ie modal display, cleanup

--- client/components/editor/teaching-elements/Pdf.vue
+++ client/components/editor/teaching-elements/Pdf.vue
@@ -18,7 +18,7 @@
           <circular-progress v-show="!showError"></circular-progress>
         </div>
       </div>
-      <div v-show="!this.ie || this.isFocused" class="pdf-container">
+      <div class="pdf-container">
         <div 
           v-show="showViewer"
           class="pdf"
@@ -51,6 +51,8 @@ import isIexplorer from 'is-iexplorer';
 import isSafari from 'is-safari';
 import CircularProgress from 'components/common/CircularProgress';
 
+if (isIexplorer) document.body.classList.add('ie');
+
 export default {
   name: 'te-pdf',
   props: ['element', 'isFocused'],
@@ -66,12 +68,12 @@ export default {
   methods: {
     embedPdf() {
       if (!this.source) return;
-      const el = this.$el.querySelector('object');
-      if (el) el.parentNode.removeChild(el);
+      if (this.pdfObject) this.pdfObject.remove();
       const pdfObject = document.createElement('object');
       pdfObject.data = this.source.src;
       pdfObject.type = this.source.type;
       this.$refs.pdf.appendChild(pdfObject);
+      this.pdfObject = pdfObject;
       setTimeout(() => (this.showError = true), 2500);
     }
   },
@@ -90,7 +92,13 @@ export default {
     },
     ie() {
       return isIexplorer;
+    },
+    showElement() {
+      return !this.ie || this.isFocused;
     }
+  },
+  beforeDestroy() {
+    this.pdfObject = null;
   },
   watch: {
     source() {
@@ -99,127 +107,132 @@ export default {
       this.embedPdf();
     }
   },
-  components: {
-    CircularProgress
-  }
+  components: { CircularProgress }
 };
 </script>
 
 <style lang="scss" scoped>
-/deep/ {
-  .te-pdf {
+.te-pdf {
+  position: relative;
+}
+
+.pdf-placeholder {
+  .message {
+    padding: 100px;
+
+    .heading {
+      font-size: 24px;
+    }
+
+    span {
+      display: block;
+      font-size: 18px;
+    }
+  }
+}
+
+.overlay {
+  position: absolute;
+  z-index: 99;
+  width: 100%;
+  height: 100%;
+  background-color: #333;
+  opacity: 0.9;
+  z-index: 10;
+
+  .message {
     position: relative;
+    top: 45%;
+    color: green;
+    font-size: 22px;
   }
+}
 
-  .pdf-placeholder {
-    .message {
-      padding: 100px;
+.error {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  background: rgba(0,0,0,.9);
+  z-index: 1;
+}
 
-      .heading {
-        font-size: 24px;
-      }
+.error .message {
+  position: absolute;
+  top: 50%;
+  left: 50%;
+  transform: translate(-50%, -50%);
+  color: #fff;
+  font-size: 18px;
+  font-weight: 500;
 
-      span {
-        display: block;
-        font-size: 18px;
-      }
-    }
-  }
+  .icon { font-size: 42px; }
+}
 
-  .overlay {
-    position: absolute;
-    z-index: 99;
+.well {
+  margin: 0;
+}
+
+.pdf-container {
+  position: relative;
+  width: 100%;
+  height: 360px;
+
+  .wrapper {
     width: 100%;
     height: 100%;
-    background-color: #333;
-    opacity: 0.9;
-    z-index: 10;
+  }
+}
 
-    .message {
-      position: relative;
-      top: 45%;
-      color: green;
-      font-size: 22px;
-    }
+.pdf {
+  position: absolute;
+  display: block;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  padding-bottom: 30px;
+  z-index: 2;
+
+  .ie & {
+    background: #585858;
   }
 
-  .error {
-    position: absolute;
-    top: 0;
-    left: 0;
+  /deep/ object {
+    display: block;
     width: 100%;
     height: 100%;
-    background: rgba(0,0,0,.9);
-    z-index: 1;
   }
+}
 
-  .error .message {
+.loader-outer {
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+
+  .loader-inner {
     position: absolute;
     top: 50%;
     left: 50%;
     transform: translate(-50%, -50%);
-    color: #fff;
-    font-size: 18px;
-    font-weight: 500;
-
-    .icon { font-size: 42px; }
   }
+}
 
-  .well {
-    margin: 0;
-  }
+.new-window {
+  position: absolute;
+  bottom: 0;
+  width: 100%;
+  background: #fff;
 
-  .pdf-container {
-    position: relative;
-    width: 100%;
-    height: 360px;
-  }
+  a {
+    color: #444;
+    font-size: 22px;
 
-  .pdf {
-    position: absolute;
-    display: block;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-    padding-bottom: 30px;
-    z-index: 2;
-
-    object {
-      display: block;
-      width: 100%;
-      height: 100%;
-    }
-  }
-
-  .loader-outer {
-    position: absolute;
-    top: 0;
-    left: 0;
-    width: 100%;
-    height: 100%;
-
-    .loader-inner {
-      position: absolute;
-      top: 50%;
-      left: 50%;
-      transform: translate(-50%, -50%);
-    }
-  }
-
-  .new-window {
-    position: absolute;
-    bottom: 0;
-    width: 100%;
-    background: #fff;
-
-    a {
-      color: #444;
-      font-size: 22px;
-
-      &:hover {
-        color: #42b983;
-      }
+    &:hover {
+      color: #42b983;
     }
   }
 }
