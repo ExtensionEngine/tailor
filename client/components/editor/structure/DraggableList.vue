ccd3a8609f3b33a785eb8e4b26753f6387451564
Toma Zelić <zelic.toma@gmail.com>
2017-11-08 14:14:56 +0100

    Add DraggableList component

---
+++ client/components/editor/structure/DraggableList.vue
@@ -0,0 +1,67 @@
+<template>
+  <div class="list-group">
+    <draggable
+      :list="list"
+      :options="dragOptions"
+      @update="update($event)"
+      @start="dragElementIndex = $event.oldIndex"
+      @end="dragElementIndex = -1"
+      class="row">
+      <div v-for="(it, index) in list"
+        @dragstart="dragElementIndex = index"
+        @dragend="dragElementIndex = -1"
+        :key="index">
+        <slot name="list-item"
+          :item="it"
+          :dragged="dragElementIndex === index">
+        </slot>
+      </div>
+    </draggable>
+    <add-element
+      :include="include"
+      :activity="activity"
+      :position="nextPosition"
+      :layout="layout"
+      @add="add">
+    </add-element>
+  </div>
+</template>
+
+<script>
+import AddElement from './AddElement';
+import Draggable from 'vuedraggable';
+import last from 'lodash/last';
+
+const dragOptions = { handle: '.drag-handle' };
+
+export default {
+  props: {
+    list: { type: Array, default() { return []; } },
+    dragOptions: { type: Object, default() { return dragOptions; } },
+    include: { type: Array, required: true },
+    activity: { type: Object, required: true },
+    layout: { type: Boolean, default: false }
+  },
+  data() {
+    return {
+      dragElementIndex: null
+    };
+  },
+  computed: {
+    nextPosition() {
+      const lastItem = last(this.list);
+      return lastItem ? lastItem.position + 1 : 1;
+    }
+  },
+  methods: {
+    add(element) {
+      this.$emit('add', element);
+    },
+    update(e) {
+      this.$emit('update', e);
+    }
+  },
+  components: { AddElement, Draggable }
+};
+</script>
+
