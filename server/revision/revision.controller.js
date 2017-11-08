6b3f4d067e6c6b3161554bfb2bcb746eae0cb0d1
Damir Perisic <underscope@gmail.com>
2017-11-08 10:18:28 +0100

    API cleanup :lipstick:

--- server/revision/revision.controller.js
+++ server/revision/revision.controller.js
@@ -1,36 +1,20 @@
 const { resolveStatics } = require('../shared/storage/helpers');
 const { Revision, User } = require('../shared/database');
 
-function index(req, res) {
-  const courseId = req.params.courseId;
-  const { limit, offset, entityId } = req.query;
-  const where = entityId ? { courseId, state: { id: entityId } } : { courseId };
-  return Revision
-    .findAll({
-      where,
-      include: [{ model: User, attributes: ['id', 'email'] }],
-      order: [['createdAt', 'DESC']],
-      limit,
-      offset
-    })
-    .then(data => res.json(data));
+function index({ course, query }, res) {
+  const { limit, offset, entityId } = query;
+  let where = { courseId: course.id };
+  if (entityId) where.state = { id: entityId };
+  const include = [{ model: User, attributes: ['id', 'email'] }];
+  const opts = { where, include, order: [['createdAt', 'DESC']], limit, offset };
+  return Revision.findAll(opts).then(data => res.json(data));
 }
 
-function resolve(req, res) {
-  const { courseId, revId } = req.params;
-  return Revision
-    .findOne({
-      where: { courseId, id: revId },
-      include: [{ model: User, attributes: ['id', 'email'] }]
-    })
-    .then(revision => {
-      if (revision.entity !== 'TEACHING_ELEMENT') return revision;
-      return resolveStatics(revision.state).then(state => {
-        revision.state = state;
-        return revision;
-      });
-    })
-    .then(data => res.json(data));
+function resolve({ revision }, res) {
+  return resolveStatics(revision.state).then(state => {
+    revision.state = state;
+    return res.json(revision);
+  });
 }
 
 module.exports = {
