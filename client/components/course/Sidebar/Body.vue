ca9ac9257de188ee97290f76b7592bba6e2027f3
Damir Perisic <underscope@gmail.com>
2017-11-24 09:07:05 +0100

    Added publish btn to the activity sidebar :tada:

--- client/components/course/Sidebar/Body.vue
+++ client/components/course/Sidebar/Body.vue
@@ -1,5 +1,17 @@
 <template>
   <div class="body">
+    <div class="publish-container">
+      <div class="publish-date">
+        <circular-progress v-if="publishing"></circular-progress>
+        <span v-else>{{ publishStatus }}</span>
+      </div>
+      <button
+        :disabled="publishing"
+        @click="publish"
+        class="btn btn-primary btn-material">
+        Publish
+      </button>
+    </div>
     <div class="meta-element">
       <meta-input
         v-for="it in metadata"
@@ -13,19 +25,33 @@
 </template>
 
 <script>
+import axios from 'client/api/request';
+import CircularProgress from 'components/common/CircularProgress';
 import cloneDeep from 'lodash/cloneDeep';
+import fecha from 'fecha';
 import get from 'lodash/get';
 import { getLevel } from 'shared/activities';
 import map from 'lodash/map';
-import { mapActions, mapGetters } from 'vuex-module';
+import { mapActions, mapGetters, mapMutations } from 'vuex-module';
 import Meta from 'components/common/Meta';
 import Prerequisites from './Prerequisites';
 
 export default {
+  data() {
+    return {
+      publishing: false
+    };
+  },
   computed: {
     ...mapGetters(['activity'], 'course'),
     config() {
       return getLevel(this.activity.type) || {};
+    },
+    publishStatus() {
+      const publishedAt = new Date(this.activity.publishedAt);
+      return publishedAt
+        ? `Published on ${fecha.format(publishedAt, 'M/D/YY HH:mm')}`
+        : 'Not published';
     },
     metadata() {
       if (!get(this.config, 'meta')) return [];
@@ -37,13 +63,24 @@ export default {
   },
   methods: {
     ...mapActions(['update'], 'activities'),
+    ...mapMutations({ localUpdate: 'save' }, 'activities'),
     updateActivity(key, value) {
       const data = cloneDeep(this.activity.data) || {};
       data[key] = value;
       this.update({ _cid: this.activity._cid, data });
+    },
+    publish() {
+      this.publishing = true;
+      const { id, courseId } = this.activity;
+      const url = `/courses/${courseId}/activities/${id}/publish`;
+      return axios.get(url).then(({ data: { data } }) => {
+        this.localUpdate({ ...this.activity, publishedAt: data.publishedAt });
+        this.publishing = false;
+      });
     }
   },
   components: {
+    CircularProgress,
     Prerequisites,
     MetaInput: Meta
   }
@@ -52,6 +89,29 @@ export default {
 
 <style lang="scss" scoped>
 .body {
+  position: relative;
   padding: 6px;
+}
+
+.publish-container {
+  min-height: 70px;
+  padding: 0 10px;
+
+  .publish-date {
+    width: 170px;
+    line-height: 44px;
+  }
+
+  .btn {
+    position: absolute;
+    top: 10px;
+    right: 24px;
+    padding: 6px 6px;
+  }
+
+  .circular-progress {
+    width: 24px;
+    margin: 0 25px;
+  }
 }
 </style>
