aac7fff658258008820a274c89dfc780979d02bc
Toma Zelic <zelic.toma@gmail.com>
2018-01-17 16:36:56 +0100

    Track courseId and activity ids for fetched comments

--- client/store/modules/comments.js
+++ client/store/modules/comments.js
@@ -1,7 +1,12 @@
 import filter from 'lodash/filter';
 import VuexCollection from '../helpers/collection';
 
-const { getter, build } = new VuexCollection('comments');
+const { action, getter, state, mutation, build } = new VuexCollection('comments');
+
+state({
+  courseId: '',
+  commentsFetched: {}
+});
 
 getter(function comments() {
   const activityId = this.rootGetters['course/activity'].id;
@@ -10,6 +15,34 @@ getter(function comments() {
 
 getter(function commentsCount() {
   return Object.keys(this.getters.comments).length;
+});
+
+getter(function commentsFetched() {
+  const activityId = this.rootGetters['course/activity'].id;
+  return this.state.commentsFetched[activityId];
+});
+
+action(function fetch() {
+  const { courseId } = this.rootState.route.params;
+  let action = 'fetch';
+  if (this.state.courseId !== courseId) {
+    this.commit('setCourseId', courseId);
+    action = 'reset';
+  };
+  const activityId = this.rootGetters['course/activity'].id;
+  const baseUrl = `/courses/${courseId}/activities/${activityId}/comments`;
+  this.commit('setBaseUrl', baseUrl);
+  this.api.fetch()
+    .then(result => this.commit(action, result))
+    .then(result => this.commit('setCommentsFetched', activityId));
+});
+
+mutation(function setCourseId(courseId) {
+  this.state.courseId = courseId;
+});
+
+mutation(function setCommentsFetched(activityId) {
+  this.state.commentsFetched[activityId] = true;
 });
 
 export default build();
