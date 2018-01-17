0a8c5775186673c64969c25a4b950ac22effeb43
Toma Zelic <zelic.toma@gmail.com>
2018-01-17 15:29:04 +0100

    Enable caching of fetched courses

--- client/store/modules/comments.js
+++ client/store/modules/comments.js
@@ -1,10 +1,15 @@
-import sortBy from 'lodash/sortBy';
+import filter from 'lodash/filter';
 import VuexCollection from '../helpers/collection';
 
 const { getter, build } = new VuexCollection('comments');
 
 getter(function comments() {
-  return sortBy(this.state.items, 'createdAt');
+  const activityId = this.rootGetters['course/activity'].id;
+  return filter(this.state.items, c => c.activityId === activityId);
 }, { global: true });
+
+getter(function commentsCount() {
+  return Object.keys(this.getters.comments).length;
+});
 
 export default build();
