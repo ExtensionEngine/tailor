f48a887ce53f113647947dc8c350f6d65ce164d2
Damir Perisic <underscope@gmail.com>
2018-01-20 19:35:23 +0100

    Minor cleanup :goat:

--- client/store/modules/comments.js
+++ client/store/modules/comments.js
@@ -1,6 +1,7 @@
 import cuid from 'cuid';
 import filter from 'lodash/filter';
 import find from 'lodash/find';
+import pick from 'lodash/pick';
 import SSEClient from '../../SSEClient';
 import Vue from 'vue';
 import VuexCollection from '../helpers/collection';
@@ -10,83 +11,74 @@ let SSE_CLIENT;
 
 state({
   courseId: '',
-  commentsFetched: {},
-  sseClient: undefined
+  activitiesFetched: {}
 });
 
 getter(function comments() {
   const activityId = this.rootGetters['course/activity'].id;
-  return filter(this.state.items, c => c.activityId === activityId);
+  return filter(this.state.items, { activityId });
 }, { global: true });
 
 getter(function commentsCount() {
-  return Object.keys(this.getters.comments).length;
+  return Object.keys(this.rootGetters.comments).length;
 });
 
 getter(function commentsFetched() {
   const activityId = this.rootGetters['course/activity'].id;
-  return !!this.state.commentsFetched[activityId];
-});
-
-mutation(function setCourseId(courseId) {
-  this.state.courseId = courseId;
-});
-
-mutation(function setCommentsFetched(activityId) {
-  Vue.set(this.state.commentsFetched, activityId, true);
-});
-
-mutation(function setSSEClient(subscription) {
-  this.state.subscription = subscription;
-});
-
-mutation(function addComment(comment) {
-  const userId = this.rootGetters['user'].id;
-  const {activityId} = comment;
-  if (userId === comment.authorId || !this.state.commentsFetched[activityId]) return;
-  comment._cid = cuid();
-  Vue.set(this.state.items, comment._cid, comment);
-});
-
-mutation(function updateComment(comment) {
-  const existing = find(this.state.items, { id: comment.id });
-  if (!existing) return;
-  const { content, createdAt, updatedAt, deletedAt } = comment;
-  const updated = Object.assign({}, existing, { content, createdAt, updatedAt, deletedAt });
-  Vue.set(this.state.items, updated._cid, updated);
-});
-
-action(function remove(comment) {
-  // Update locally and let real data update be pushed from server after soft delete
-  comment.deletedAt = new Date();
-  this.commit('update', comment);
-  return this.api.remove(comment);
+  return !!this.state.activitiesFetched[activityId];
 });
 
 action(function fetch({ activityId }) {
   const { courseId } = this.rootState.route.params;
-  let action = 'fetch';
-  if (this.state.courseId !== courseId) {
-    this.commit('setCourseId', courseId);
-    action = 'reset';
-  };
+  let action = this.state.courseId === courseId ? 'fetch' : 'reset';
+  if (action === 'reset') this.commit('setCourse', courseId);
   this.api.fetch({ activityId })
     .then(result => this.commit(action, result))
-    .then(result => this.commit('setCommentsFetched', activityId));
+    .then(result => this.commit('activitiesFetched', activityId));
 });
 
 action(function subscribe() {
   if (SSE_CLIENT) SSE_CLIENT.disconnect();
   SSE_CLIENT = new SSEClient(`/api/v1${this.state.$baseUrl}/subscribe`);
-  SSE_CLIENT.subscribe('comment_create', comment => this.commit('addComment', comment));
-  SSE_CLIENT.subscribe('comment_update', comment => this.commit('updateComment', comment));
-  SSE_CLIENT.subscribe('comment_delete', comment => this.commit('updateComment', comment));
+  SSE_CLIENT.subscribe('comment_create', item => this.commit('sseAdd', item));
+  SSE_CLIENT.subscribe('comment_update', item => this.commit('sseUpdate', item));
+  SSE_CLIENT.subscribe('comment_delete', item => this.commit('sseUpdate', item));
 });
 
 action(function unsubscribe() {
-  if (!this.state.sseClient) return;
-  this.state.sseClient.close();
-  this.commit('setSSEClient');
+  if (!SSE_CLIENT) return;
+  SSE_CLIENT.close();
+});
+
+action(function remove(comment) {
+  // Update locally and let real data update be pushed from server
+  // after soft delete
+  comment.deletedAt = new Date();
+  this.commit('update', comment);
+  return this.api.remove(comment);
+});
+
+mutation(function setCourse(courseId) {
+  this.state.courseId = courseId;
+});
+
+mutation(function commentsFetched(activityId) {
+  Vue.set(this.state.activitiesFetched, activityId, true);
+});
+
+mutation(function sseAdd(comment) {
+  const { id } = comment;
+  if (find(this.state.items, { id })) return;
+  comment._cid = cuid();
+  Vue.set(this.state.items, comment._cid, comment);
+});
+
+mutation(function sseUpdate(comment) {
+  const existing = find(this.state.items, { id: comment.id });
+  if (!existing) return;
+  const data = pick(comment, ['content', 'createdAt', 'updatedAt', 'deletedAt']);
+  const updated = Object.assign({}, existing, data);
+  Vue.set(this.state.items, updated._cid, updated);
 });
 
 export default build();
