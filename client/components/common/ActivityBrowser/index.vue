acf5c2feb0796d4ec67290ca73e7a5d1a5d36df9
Damir Perisic <underscope@gmail.com>
2018-03-12 07:08:30 +0100

    Created ActivityBrowser root component
    - Basic implementation, needs enhancements

---
+++ client/components/common/ActivityBrowser/index.vue
@@ -0,0 +1,34 @@
+<template>
+  <div>
+    <select-repository
+      v-if="!repository"
+      @selected="repository => (this.repository = repository)">
+    </select-repository>
+    <select-activity
+      v-else
+      :repository="repository"
+      :levels="levels"
+      @reset="repository = null"
+      @selected="activity => $emit('selected', activity)">
+    </select-activity>
+  </div>
+</template>
+
+<script>
+import SelectActivity from './SelectActivity';
+import SelectRepository from './SelectRepository';
+
+export default {
+  props: ['levels'],
+  data() {
+    return {
+      repository: null,
+      activity: null
+    };
+  },
+  components: {
+    SelectActivity,
+    SelectRepository
+  }
+};
+</script>
