71e10dee5cdad065b90aa58dde95bd615bd04b91
Damir Perisic <underscope@gmail.com>
2018-03-13 08:05:15 +0100

    Updated fetch on base collection :wrench:
    - Set _cid if does not exist

--- client/store/helpers/collection.js
+++ client/store/helpers/collection.js
@@ -1,5 +1,6 @@
 import cuid from 'cuid';
 import each from 'lodash/each';
+import find from 'lodash/find';
 import Vue from 'vue';
 import { VuexModule } from 'vuex-module';
 
@@ -73,7 +74,11 @@ export default function (collectionName, baseUrl = '') {
 
   // TODO: Do the proper syncing
   mutation(function fetch(result) {
-    each(result, it => Vue.set(this.state.items, it._cid, it));
+    each(result, it => {
+      const existing = find(this.state.items, { id: it.id });
+      it._cid = existing ? existing._cid : cuid();
+      Vue.set(this.state.items, it._cid, it);
+    });
   });
 
   mutation(function reset(result) {
