e446989eb58767e4d2c1386448b5aaaef5591f91
Damir Perisic <underscope@gmail.com>
2018-03-25 11:48:29 +0200

    Integrated extracted parser :bomb:

--- config/shared/activities.js
+++ config/shared/activities.js
@@ -5,49 +5,16 @@ const get = require('lodash/get');
 const isEmpty = require('lodash/isEmpty');
 const map = require('lodash/map');
 const mergeConfig = require('../utils/mergeConfig');
-const transform = require('lodash/transform');
+const parseSchemas = require('./schema-parser');
 
 const defaultConfiguration = require('./activities-rc');
 const customConfiguration = require('./activities-rc.load')();
-
 if (!process.env.ENABLE_DEFAULT_SCHEMA && !isEmpty(customConfiguration)) {
   defaultConfiguration.SCHEMAS = [];
 }
 
-const config = mergeConfig(defaultConfiguration, customConfiguration);
-const { SCHEMAS } = config;
-
-// Validate schemas
-// Prefix activity types with schema id; SCHEMA_ID/TYPE
-// Add name meta to each activity config
-SCHEMAS.forEach(schema => {
-  validateSchema(schema);
-  schema.meta = schema.meta || [];
-  const hasColorMeta = find(schema.meta, { key: 'color' });
-  if (!hasColorMeta) {
-    schema.meta.push({ type: 'COLOR', key: 'color', label: 'Label color' });
-  }
-  schema.defaultMeta = getMetaDefaults(schema.meta);
-  return schema.structure.forEach(it => {
-    it.type = `${schema.id}/${it.type}`;
-    it.subLevels = map(it.subLevels, type => `${schema.id}/${type}`);
-    it.meta = it.meta || [];
-    const hasNameMeta = find(it.meta, { key: 'name' });
-    if (!hasNameMeta) {
-      it.meta.unshift({
-        key: 'name',
-        type: 'TEXTAREA',
-        label: 'Name',
-        placeholder: 'Click to add...',
-        validate: { rules: { max: 250, required: true } }
-      });
-    }
-    it.defaultMeta = getMetaDefaults(it.meta);
-    if (it.hasExams && get(it, 'exams.objectives')) {
-      it.exams.objectives = map(it.exams.objectives, it => `${schema.id}/${it}`);
-    }
-  });
-});
+const { SCHEMAS } = mergeConfig(defaultConfiguration, customConfiguration);
+parseSchemas(SCHEMAS);
 
 module.exports = {
   SCHEMAS,
@@ -105,24 +72,4 @@ function getRepositoryMeta(repository) {
     let value = get(repository, `data.${it.key}`);
     return { ...it, value };
   });
-}
-
-function validateSchema(schema) {
-  const schemaMandatoryProps = ['id', 'name'];
-  schemaMandatoryProps.forEach(prop => {
-    if (!schema[prop]) throw new Error(`Schema must have ${prop}!`);
-  });
-  if (isEmpty(schema.structure)) throw new Error('Schema structure missing!');
-  const activityMandatoryProps = ['level', 'type', 'label', 'color'];
-  schema.structure.forEach(it => {
-    activityMandatoryProps.forEach(prop => {
-      if (!it[prop]) throw new Error(`Activity config must have ${prop}!`);
-    });
-  });
-}
-
-function getMetaDefaults(meta) {
-  return transform(meta, (acc, it) => {
-    if (it.defaultValue) acc[it.key] = it.defaultValue;
-  }, {});
 }
