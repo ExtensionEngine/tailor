7f69135b859e33c4f29dc056e0f3e55f81b04d1d
Toma Zelic <zelic.toma@gmail.com>
2018-04-27 17:40:24 +0200

    Extend eslint config with vue ruleset

--- .eslintrc.js
+++ .eslintrc.js
@@ -4,9 +4,12 @@ module.exports = {
   root: true,
   parserOptions: { sourceType: 'module' },
   // https://github.com/Flet/eslint-config-semistandard
-  extends: 'semistandard',
+  extends: [
+    'semistandard',
+    'plugin:vue/recommended'
+  ],
   // required to lint *.vue files
-  plugins: ['html'],
+  plugins: ['vue'],
   rules: {
     indent: ['error', 2, {
       SwitchCase: 1,
@@ -27,6 +30,46 @@ module.exports = {
     //   'ignoreCase': true,
     //   'ignoreMemberSort': false
     // }]
+    // Vue rules
+    'vue/html-self-closing': 0,
+    'vue/attribute-hyphenation': 0,
+    'vue/max-attributes-per-line': [2, { singleline: 5 }],
+    'vue/name-property-casing': [2, 'kebab-case'],
+    // TODO: Add order for custom directives once supported
+    'vue/attributes-order': [2, {
+      order: [
+        'DEFINITION',
+        'LIST_RENDERING',
+        'CONDITIONALS',
+        'RENDER_MODIFIERS',
+        'UNIQUE',
+        'BINDING',
+        'EVENTS',
+        'CONTENT',
+        'GLOBAL',
+        'OTHER_ATTR'
+      ]
+    }],
+    'vue/order-in-components': [2, {
+      order: [
+        'el',
+        'name',
+        ['template', 'render', 'renderError'],
+        ['parent','functional', 'delimiters', 'comments'],
+        'extends',
+        'mixins',
+        'inheritAttrs',
+        'model',
+        ['props', 'propsData'],
+        'data',
+        'computed',
+        'methods',
+        'LIFECYCLE_HOOKS',
+        'watch',
+        ['directives', 'filters'],
+        'components'
+      ]
+    }]
   },
   globals: {
     BRAND_CONFIG: true
