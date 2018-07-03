d9838695765f124d414e02e8fb676c319cc80b2b
abasic <antoniobasic@gmail.com>
2018-07-03 16:51:41 +0200

    Replaced video js with plyr

--- client/components/editor/teaching-elements/Video.vue
+++ client/components/editor/teaching-elements/Video.vue
@@ -20,13 +20,18 @@
         </div>
       </div>
       <div class="player">
-        <video-player
-          v-if="!isDragged"
-          :options="options"
-          @ready="onReady"
-          ref="video"
-          class="player">
-        </video-player>
+        <plyr
+          v-if="!switchingVideo"
+          :emit="['error']"
+          @ready="onError"
+          ref="video">
+          <video v-if="type.isNative">
+            <source :src="url" :type="type.name"/>
+          </video>
+          <div v-else class="plyr__video-embed">
+            <iframe :src="url" allowfullscreen></iframe>
+          </div>
+        </plyr>
       </div>
     </div>
   </div>
@@ -35,40 +40,43 @@
 <script>
 import { extname } from 'path';
 import get from 'lodash/get';
-import 'videojs-youtube';
-import 'videojs-vimeo';
-import { videoPlayer as VideoPlayer } from 'vue-video-player';
+import { Plyr } from 'vue-plyr';
+
 const MediaError = window.MediaError;
+
+const NOT_NATIVE = /youtu\.?be|vimeo/;
+const CUSTOM_TYPE_MAPPING = {
+  ogv: 'ogg'
+};
 
 export default {
   name: 'te-video',
-  props: ['element', 'isFocused', 'isDragged'],
+  props: {
+    element: { type: Object, required: true },
+    isFocused: { type: Boolean, default: false },
+    isDragged: { type: Boolean, default: false }
+  },
   data() {
-    return { error: null };
+    return {
+      error: null,
+      switchingVideo: false
+    };
   },
   computed: {
     player() {
-      return this.$refs.video && this.$refs.video.player;
+      return get(this.$refs, 'video.player');
     },
-    source() {
-      const src = get(this.element, 'data.url');
-      if (!src) return;
-      const type = getMimetype(src);
-      return { type, src };
+    url() {
+      return get(this.element, 'data.url', '');
     },
-    poster() {
-      return get(this.element, 'data.poster');
-    },
-    options() {
-      return {
-        autoplay: false,
-        techOrder: ['html5', 'youtube', 'vimeo'],
-        poster: this.poster,
-        sources: [this.source]
-      };
+    type() {
+      if (NOT_NATIVE.test(this.url)) return { isNative: false };
+      const ext = extname(this.url).substring(1);
+      const name = `video/${CUSTOM_TYPE_MAPPING[ext] || ext}`;
+      return { isNative: true, name };
     },
     showPlaceholder() {
-      return !this.source;
+      return !this.url;
     },
     showError() {
       if (!this.error) return false;
@@ -76,12 +84,6 @@ export default {
     }
   },
   methods: {
-    onReady(player) {
-      this.error = null;
-      player.on('error', () => this.onError(player.error()));
-      const err = player.error();
-      if (err) this.onError(err);
-    },
     onError(err) {
       this.error = err;
     }
@@ -89,20 +91,19 @@ export default {
   watch: {
     isFocused(val, oldVal) {
       if (oldVal && !val && this.player) this.player.pause();
+    },
+    url(val, oldVal) {
+      this.switchingVideo = true;
+      this.$nextTick(() => { this.switchingVideo = false; });
     }
   },
   beforeDestroy() {
     if (this.player) this.player.pause();
   },
-  components: { VideoPlayer }
+  components: {
+    Plyr
+  }
 };
-
-function getMimetype(url) {
-  if (url.match(/youtu\.?be/)) return 'video/youtube';
-  if (url.match(/vimeo/)) return 'video/vimeo';
-  const [, ext] = extname(url).split('.');
-  return `video/${ext}`;
-}
 </script>
 
 <style lang="scss" scoped>
@@ -146,7 +147,7 @@ function getMimetype(url) {
   z-index: 98;
   width: 100%;
   height: 100%;
-  background: rgba(0,0,0,.9);
+  background: rgba(0, 0, 0, .9);
 }
 
 .error .message {
@@ -158,7 +159,9 @@ function getMimetype(url) {
   font-size: 18px;
   font-weight: 500;
 
-  .icon { font-size: 42px; }
+  .icon {
+    font-size: 42px;
+  }
 }
 
 .well {
@@ -166,25 +169,22 @@ function getMimetype(url) {
 }
 
 .player {
-  height: 360px;
+  height: 410px;
   background: #000;
-}
 
-.player /deep/ .video-js .vjs-big-play-button {
-  display: none !important;
-}
+  /deep/ {
+    > div, .plyr--video, .plyr__video-wrapper, video {
+      height: 100%;
+    }
 
-// Addresses videojs-vimeo plugin issue:
-// https://github.com/videojs/videojs-vimeo/issues/70#issuecomment-196805920
-.player /deep/ .video-js .vimeoFrame {
-  // NOTE: Do not remove !important, overrides inline style!
-  position: absolute !important;
-  height: 100% !important;
-  padding: 0 !important;
-  top: 0;
-  bottom: 0;
-  left: 0;
-  right: 0;
+    .plyr__video-wrapper {
+      padding-bottom: 0 !important;
+    }
+
+    .plyr__control--overlaid {
+      display: none;
+    }
+  }
 }
 
 .disabled .overlay {
