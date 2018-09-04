e46a601a56c26dae49efebd9eb27c54e1d263760
Dario Vladovic <d.vladimyr@gmail.com>
2018-09-04 16:40:28 +0200

    Upgrade stylelint to v9 :tada:
    - updating stylelint config
    - introducing lint script

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,8 +1,17 @@
 module.exports = {
-  processors: ['stylelint-processor-html'],
   extends: 'stylelint-config-standard',
-  plugins: ['stylelint-order'],
+  plugins: ['stylelint-scss', 'stylelint-order'],
   rules: {
+    // NOTE: Required due to:
+    // https://github.com/mapbox/stylelint-processor-arbitrary-tags/tree/5e302529f447f4d18dc16d2a9e5a16ca98d3b378#caveats
+    'no-empty-source': null,
+    'at-rule-no-unknown': null,
+    'scss/at-rule-no-unknown': true,
+    'at-rule-empty-line-before': ['always', {
+      except: ['blockless-after-blockless'],
+      ignore: ['after-comment', 'inside-block']
+    }],
+    'function-name-case': 'lower',
     'selector-list-comma-newline-after': 'never-multi-line',
     'function-comma-space-after': null,
     'order/properties-order': [
@@ -34,6 +43,7 @@ module.exports = {
       'font-size',
       'font-weight',
       'line-height',
+      'text-transform',
       'background',
       'background-color',
       'border',
