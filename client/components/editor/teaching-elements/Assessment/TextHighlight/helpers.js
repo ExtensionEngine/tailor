ef2871c68e543bfed16a36898e3d9d2edce335fa
Tereza Karabatic <tkarabatic2@gmail.com>
2018-10-10 18:07:57 +0200

    Refactor wildcard indices search and fix text clean-up

--- client/components/editor/teaching-elements/Assessment/TextHighlight/helpers.js
+++ client/components/editor/teaching-elements/Assessment/TextHighlight/helpers.js
@@ -1,26 +1,38 @@
 import Highlight from './Highlight';
 import Highlights from './Highlights';
 
+function getOccurrenceIndices(text, wildcard) {
+  const regex = new RegExp(wildcard);
+  let indices = [];
+  const indexModifier = wildcard.length - 1;
+  let startIndex = 0;
+
+  let match = text.match(regex);
+  if (!match || !match.index) return indices;
+
+  let { index: currentIndex } = match;
+
+  while (currentIndex) {
+    if (!indices.includes(currentIndex)) indices.push(currentIndex);
+    startIndex = currentIndex + indexModifier;
+    const match = text.substring(startIndex).match(regex);
+    if (!match || !match.index) break;
+
+    currentIndex += match.index + indexModifier;
+  }
+
+  return indices;
+}
+
 export function getPlainContent(text) {
   const temp = document.createElement('div');
-  const preparedText = String(text).replace(/<p><br><\/p>/gi, '\n\n');
+  let preparedText = String(text).replace(/<p>/g, '');
+  preparedText = preparedText.replace(/<br>/g, '');
+  preparedText = preparedText.replace(/<\/p>/g, '\n');
+
   temp.innerHTML = preparedText;
 
   return temp.innerText;
-}
-
-// TODO: replace by a regex
-export function getOccurrenceIndices(str, substr, lastIndex = 0) {
-  if (!str.length) return [];
-
-  const currentIndex = str.indexOf(substr);
-  if (currentIndex === -1 || currentIndex + 1 > str.length) return [];
-
-  const newString = str.substring(currentIndex + 1);
-  const index = currentIndex + lastIndex;
-  const oldIndex = index + 1;
-
-  return [index].concat(getOccurrenceIndices(newString, substr, oldIndex));
 }
 
 export function isHighlightValid(highlight, text) {
