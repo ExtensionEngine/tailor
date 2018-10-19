ed7d518e34343a7164b0e2d90da3fd75c3e58228
Damir Perisic <underscope@gmail.com>
2018-10-19 16:23:30 +0200

    Toolbar naming helper

--- client/components/editor/teaching-elements/toolkit/utils.js
+++ client/components/editor/teaching-elements/toolkit/utils.js
@@ -4,6 +4,10 @@ export function getComponentName(type) {
   return `content-${toCase.slug(type)}`;
 }
 
+export function getToolbarName(type) {
+  return `${toCase.slug(type)}-toolbar`;
+}
+
 export function getElementId(element) {
   return element._cid || element.id;
 }
