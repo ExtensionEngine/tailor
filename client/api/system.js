cd7ece1d9400708071d25c5fdefb50875edf6d3d
abasic <antoniobasic@gmail.com>
2018-11-19 10:17:14 +0100

    Added extractData helper to system api

--- client/api/system.js
+++ client/api/system.js
@@ -1,13 +1,15 @@
 import request from './request';
 
+const extractData = res => res.data.data;
+
 function getUsers() {
   const options = { params: { roleType: 'user' } };
-  return request.get('/users', options).then(({ data: { data } }) => data);
+  return request.get('/users', options).then(extractData);
 }
 
 function upsertUser(email, data) {
   const body = { email, ...data };
-  return request.post('/users', body).then(({ data: { data } }) => data);
+  return request.post('/users', body).then(extractData);
 }
 
 function removeUser(id) {
