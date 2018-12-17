109bd83b1fd000bf7280ff7fcf8d6b396d7d97db
Damir Perisic <underscope@gmail.com>
2018-12-17 11:50:27 +0100

    Poll element improvements
    - Improved UX
    - Handling question deletion

--- client/components/editor/teaching-elements/Poll/index.vue
+++ client/components/editor/teaching-elements/Poll/index.vue
@@ -1,48 +1,55 @@
 <template>
   <div class="te-poll">
     <div class="poll-container">
-      <h2>Poll</h2>
-      <div :class="{ 'name-error': nameError }" class="name">
+      <span class="mdi mdi-poll pull-right"></span>
+      <div :class="{ 'has-error': nameError }" class="form-group name">
+        <label for="`${element._cid}pollName`">Poll name</label>
         <input
           v-model="name"
+          id="`${element._cid}pollName`"
           type="text"
-          placeholder="Name...">
+          class="form-control"
+          placeholder="Poll name...">
       </div>
-      <h4>Question</h4>
-      <div class="question">
-        <div class="row">
-          <primitive
-            :key="question.id"
-            :initialElement="question"
-            @save="saveQuestion"/>
+      <div class="question row">
+        <div class="col-xs-12">
+          <h4>Question</h4>
         </div>
+        <primitive
+          :key="question.id"
+          :initialElement="question"
+          @save="saveQuestion"/>
       </div>
       <div class="options-heading">
         <h4>Options</h4>
-        <button @click="addOption" class="btn">Add Option</button>
+        <button @click="addOption" class="btn btn-default btn-material">
+          Add Option
+        </button>
       </div>
-      <div class="options">
-        <draggable
-          :list="options"
-          :options="dragOptions"
-          @update="reorderOption"
-          class="row">
+      <draggable
+        :list="options"
+        :options="dragOptions"
+        @update="reorderOption"
+        class="options row">
+        <div
+          v-for="(option, index) in options"
+          :key="option.id"
+          class="option">
+          <span class="option-index">{{ index + 1 }}</span>
           <primitive
-            v-for="option in options"
-            :key="option.id"
             :initialElement="option"
-            :drag="true"
-            @save="saveOption"/>
-        </draggable>
-      </div>
+            @save="saveOption"
+            class="option-input"/>
+        </div>
+      </draggable>
     </div>
   </div>
 </template>
 
 <script>
 import calculatePosition from 'utils/calculatePosition.js';
-import cuid from 'cuid';
 import cloneDeep from 'lodash/cloneDeep';
+import cuid from 'cuid';
 import Draggable from 'vuedraggable';
 import EventBus from 'EventBus';
 import filter from 'lodash/filter';
@@ -67,19 +74,17 @@ export default {
   },
   data() {
     return {
-      dragOptions: { handle: '.drag-handle' }
+      dragOptions: { handle: '.option-index' }
     };
   },
   computed: {
     name: {
       get() {
-        const element = cloneDeep(this.element);
-        return element.data.name;
+        return this.element.data.name;
       },
       set(name) {
-        const element = cloneDeep(this.element);
-        element.data.name = name;
-        this.save(element);
+        const data = { ...this.element.data, name };
+        this.save({ ...this.element, data });
       }
     },
     embeds() {
@@ -112,9 +117,7 @@ export default {
       element.data.options = element.data.options || [];
       const existing = find(this.options, { id: option.id });
       set(element.data.embeds, option.id, option);
-      if (!existing) {
-        element.data.options.push(option.id);
-      }
+      if (!existing) element.data.options.push(option.id);
       this.save(element);
     },
     addOption() {
@@ -136,7 +139,12 @@ export default {
     appChannel.on('deleteElement', item => {
       const element = cloneDeep(this.element);
       if (!item.embedded) return;
-      if (item.id === this.question.id) return;
+      if (item.id === this.question.id) {
+        const question = cloneDeep(this.question);
+        question.data.content = '';
+        setTimeout(() => this.saveQuestion(question), 50);
+        return;
+      }
       if (!this.embedsTotal || this.embedsTotal < 4) return;
       if (!element.data.embeds[item.id]) return;
       delete element.data.embeds[item.id];
@@ -148,49 +156,93 @@ export default {
 </script>
 
 <style lang="scss" scoped>
+$label-color: #3f51b5;
+
 .te-poll {
-  min-height: 200px;
   margin: 10px auto;
-  padding: 10px 30px 30px;
+  padding: 10px 30px 20px;
 }
 
 .poll-container {
-  clear: both;
   width: 100%;
   text-align: left;
-
-  h2 {
-    padding-bottom: 20px;
-    text-align: center;
-  }
 }
 
-.name {
-  margin: 0 10px 30px;
-
-  input {
-    width: 100%;
-    height: 40px;
-  }
-
-  &.name-error input {
-    box-shadow: inset 0 -2px 0 #e51c23;
-  }
+.mdi-poll {
+  color: $label-color;
+  font-size: 20px;
 }
 
-.question, .options {
-  padding: 10px;
-  font-size: 22px;
-  text-align: center;
+h4 {
+  font-size: 16px;
+  text-align: left;
 }
 
 .question {
-  margin-bottom: 30px;
+  margin-top: 20px;
+  text-align: center;
+}
+
+.options {
+  margin: 10px 2px;
+  font-size: 22px;
 }
 
 .options-heading {
   display: flex;
   justify-content: space-between;
-  margin-right: 10px;
+  margin-top: 10px;
+}
+
+.option {
+  $min-height: 30px;
+
+  display: flex;
+
+  &-index {
+    position: relative;
+    width: 20px;
+    height: $min-height;
+    margin: 8px $min-height / 2 0 0;
+    padding: 0 0 0 4px;
+    color: #fff;
+    font-size: 16px;
+    font-weight: bold;
+    line-height: $min-height;
+    text-align: center;
+    background: $label-color;
+    cursor: pointer;
+
+    &::after {
+      $height: $min-height / 2;
+      content: '';
+      position: absolute;
+      top: 0;
+      right: -$height;
+      border-top: $height solid transparent;
+      border-left: $height solid $label-color;
+      border-bottom: $height solid transparent;
+    }
+  }
+
+  &-input {
+    flex: 1;
+
+    /deep/ .teaching-element {
+      $border-height: 2px;
+
+      min-height: $min-height + $border-height;
+      padding: 0;
+
+      .ql-editor {
+        min-height: $min-height;
+        padding: 6px 10px;
+      }
+
+      .text-placeholder {
+        display: none;
+      }
+    }
+  }
 }
 </style>
