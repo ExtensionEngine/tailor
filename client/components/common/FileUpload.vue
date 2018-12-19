f3b1cf41feb9f3bbbfea9f04616df08a1916532d
Ivan Saric <tcd-kakaroto@hotmail.com>
2018-12-19 15:15:29 +0100

    Add generic fileUpload component and meta file upload wrapper component

---
+++ client/components/common/FileUpload.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="file-upload">
+    <div class="upload-button">
+      <label for="upload" class="upload-label btn btn-link btn-sm">
+        <span class="mdi mdi-file"></span> Upload
+        <input @change="upload" id="upload" type="file" class="upload-input"/>
+      </label>
+    </div>
+  </div>
+</template>
+
+<script>
+import isEmpty from 'lodash/isEmpty';
+
+export default {
+  name: 'file-upload',
+  methods: {
+    upload({ target }) {
+      const file = !isEmpty(target.files) ? target.files[0] : null;
+      if (!file) return;
+      const reader = new window.FileReader();
+      reader.readAsDataURL(file);
+      reader.addEventListener('load', e => {
+        window.alert('uploaded');
+      });
+    }
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.upload-button {
+  padding: 0;
+}
+
+.upload-label {
+  height: 100%;
+  margin: 0;
+  padding: 0;
+  cursor: pointer;
+}
+
+.upload-input {
+  display: none;
+}
+</style>
---
+++ client/components/common/Meta/FileUpload.vue
@@ -0,0 +1,43 @@
+<template>
+  <div class="meta-file-upload">
+    <label :for="meta.key" class="meta-name">{{ meta.label }}</label>
+    <file-upload></file-upload>
+  </div>
+</template>
+
+<script>
+import FileUpload from '../FileUpload.vue';
+
+export default {
+  name: 'file',
+  props: {
+    meta: { type: Object, default: () => ({ value: null }) }
+  },
+  components: {
+    FileUpload
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.meta-file-upload {
+  position: relative;
+  min-height: 50px;
+  margin: 20px 0;
+  padding: 3px 8px;
+
+  &:hover {
+    background-color: #f5f5f5;
+  }
+
+  &.editing:hover {
+    background-color: inherit;
+  }
+
+  .meta-name {
+    width: 100%;
+    margin-bottom: 10px;
+    color: #808080;
+  }
+}
+</style>
