9ab5533d49aa266174a7eaf37c606b5266da4a31
Ivan Saric <tcd-kakaroto@hotmail.com>
2019-01-16 18:56:05 +0100

    Refactor general file upload component

--- client/components/common/FileUpload.vue
+++ client/components/common/FileUpload.vue
@@ -1,51 +1,47 @@
 <template>
-  <div class="file-upload-form">
-    <form action="http://sigv4examplebucket.s3.amazonaws.com/" method="post" enctype="multipart/form-data">
-      Key to upload:
-      <input type="input" name="key" value="user/user1/${filename}" /><br />
-      <input type="hidden" name="acl" value="public-read" />
-      <input type="hidden" name="success_action_redirect" value="http://sigv4examplebucket.s3.amazonaws.com/successful_upload.html" />
-      Content-Type:
-      <input type="input" name="Content-Type" value="image/jpeg" /><br />
-      <input type="hidden" name="x-amz-meta-uuid" value="14365123651274" />
-      <input type="hidden" name="x-amz-server-side-encryption" value="AES256" />
-      <input type="text" name="X-Amz-Credential" value="AKIAIOSFODNN7EXAMPLE/20151229/us-east-1/s3/aws4_request" />
-      <input type="text" name="X-Amz-Algorithm" value="AWS4-HMAC-SHA256" />
-      <input type="text" name="X-Amz-Date" value="20151229T000000Z" />
-      Tags for File:
-      <input type="input" name="x-amz-meta-tag" value="" /><br />
-      <input type="hidden" name="Policy" value="<Base64-encoded policy string>" />
-      <input type="hidden" name="X-Amz-Signature" value="<signature-value>" />
-      File:
-      <input type="file" name="file" /> <br />
-      <!-- The elements after this will be ignored -->
-      <input type="submit" name="submit" value="Upload to Amazon S3" />
+  <div>
+    <form @submit.prevent="upload">
+      <input
+        ref="fileInput"
+        @change="onFileSelected"
+        name="file"
+        type="file">
+      <input type="submit" value="Upload">
     </form>
   </div>
 </template>
 
 <script>
-// import request from '../../api/request';
+import request from '../../api/request';
 
 export default {
   name: 'file-upload',
-  methods: {}
+  data() {
+    return {
+      uploading: false
+    };
+  },
+  methods: {
+    onFileSelected(e) {
+      this.form = new FormData();
+      const [file] = e.target.files;
+      if (!file) {
+        this.filename = null;
+        return;
+      }
+      this.filename = file.name;
+      this.form.append('file', file, file.name);
+    },
+    upload(e) {
+      this.uploading = true;
+      request.post('/files', this.form).then(res => {
+        return res;
+      });
+    }
+  }
 };
 </script>
 
 <style lang="scss" scoped>
-.upload-button {
-  padding: 0;
-}
 
-.upload-label {
-  height: 100%;
-  margin: 0;
-  padding: 0;
-  cursor: pointer;
-}
-
-.upload-input {
-  display: none;
-}
 </style>
