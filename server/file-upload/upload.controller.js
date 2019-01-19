250eccefaf3b63afb84a6d4ecee48a3437e8b41f
Ivan Saric <tcd-kakaroto@hotmail.com>
2019-01-19 18:07:13 +0100

    Hash file to construct the upload key
    - return key and error report if any

--- server/file-upload/upload.controller.js
+++ server/file-upload/upload.controller.js
@@ -1,9 +1,14 @@
+const crypto = require('crypto');
+const path = require('path');
 const { saveFile } = require('../shared/storage');
 
 function uploadFile(req, res) {
-  const key = `repository/assets/someAsset`;
-  return saveFile(key, req.file.buffer).then(result => {
-    return res.json({ result });
+  const { file } = req;
+  const extension = path.extname(file.originalname);
+  const hash = crypto.createHash('md5').update(`${file}`).digest('hex');
+  const key = `repository/assets/${hash}${extension}`;
+  return saveFile(key, req.file.buffer).then(({ $response }) => {
+    return res.json({ key, error: $response.error });
   });
 }
 
