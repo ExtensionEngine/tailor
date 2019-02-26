adfa53b07bfe5f48d49550f2597eb4402587b6c1
Dario Vladovic <d.vladimyr@gmail.com>
2019-02-26 14:49:10 +0100

    Cleanup :lipstick:

--- server/shared/storage/storage.controller.js
+++ server/shared/storage/storage.controller.js
@@ -1,19 +1,30 @@
+'use strict';
+
 const { getFileUrl, saveFile } = require('./');
 const { ASSET_ROOT } = require('./helpers');
 const crypto = require('crypto');
+const fs = require('fs');
 const path = require('path');
+
+const sha256 = buf => crypto.createHash('sha256').update(buf).digest('hex');
 
 function getUrl(req, res) {
   const { query: { key } } = req;
   return getFileUrl(key).then(url => res.json({ url }));
 }
 
-function upload({ file }, res) {
+async function upload({ file }, res) {
+  const buffer = await readFile(file);
+  const hash = sha256(buffer);
   const extension = path.extname(file.originalname);
-  const hash = crypto.createHash('sha256').update(`${file.buffer}`).digest('hex');
-  const key = `${ASSET_ROOT}/${hash}${extension}`;
-  return saveFile(key, file.buffer, { ContentType: file.mimetype })
-    .then(() => res.json({ key }));
+  const key = path.join(ASSET_ROOT, `${hash}${extension}`);
+  await saveFile(key, buffer, { ContentType: file.mimetype });
+  return res.json({ key });
 }
 
 module.exports = { getUrl, upload };
+
+function readFile(file) {
+  if (file.buffer) return Promise.resolve(file.buffer);
+  return fs.readFile(file.path);
+}
