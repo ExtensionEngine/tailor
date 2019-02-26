5c06a1447b97b9ba91ba5854d56e4806d172b226
Dario Vladovic <d.vladimyr@gmail.com>
2019-02-27 00:23:34 +0100

    Refactor `html-version-spec` poi plugin
    - modifying poi config instead of webpack one

--- build/plugins/html-version-spec.js
+++ build/plugins/html-version-spec.js
@@ -3,23 +3,24 @@
 const { execFileSync } = require('child_process');
 
 const execSync = (cmd, args, opts) => execFileSync(cmd, args, opts).toString().trim();
+const forEach = (obj, cb) => Object.keys(obj).forEach(key => cb(obj[key], key));
+const isObject = arg => arg !== null && typeof arg === 'object';
 
 exports.name = 'html-version-spec';
 
-exports.apply = api => {
-  api.hook('createWebpackChain', config => {
-    if (api.config.output.html === false) return;
-    const meta = { version: getVersion(api.pkg) };
-    getHtmlPlugins(api.config).forEach(name => {
-      config.plugin(name).tap(([args]) => [{ ...args, meta }]);
-    });
-  });
+exports.apply = ({ config, pkg }) => {
+  const meta = {
+    get version() {
+      return getVersion(pkg);
+    }
+  };
+  if (isObject(config.html)) {
+    return Object.assign(config.html, { meta });
+  }
+  if (isObject(config.pages)) {
+    return forEach(config.pages, page => Object.assign(page, { meta }));
+  }
 };
-
-function getHtmlPlugins(config) {
-  if (!config.pages) return ['html'];
-  return Object.keys(config.pages).map(it => `html-page-${it}`);
-}
 
 function getVersion(pkg) {
   const semver = pkg.data.version;
