51bc8a282b3c072802d76e53619a4be11ddddab2
Dario Vladovic <d.vladimyr@gmail.com>
2019-03-27 23:46:23 +0100

    Refactor `<FileUpload>` component :wrench:

--- client/components/common/FileUpload.vue
+++ client/components/common/FileUpload.vue
@@ -1,12 +1,10 @@
 <template>
   <div class="file-upload">
     <circular-progress v-if="uploading"/>
-    <form v-else @submit.prevent class="upload-form">
+    <form v-else ref="form" @submit.prevent class="upload-form">
       <input
         v-validate="validate"
-        :id="id"
-        :ref="id"
-        :name="id"
+        v-bind="{ id, name: id }"
         @change="upload"
         type="file"
         class="upload-input">
@@ -16,14 +14,15 @@
         :class="[sm ? 'v-btn v-btn--small' : 'btn btn-material btn-sm upload-button']">
         {{ label }}
       </label>
-      <asset-link
-        v-else
-        :href="`storage://${fileKey}`"
-        :download="fileName"
-        class="file-name">
-        {{ fileName }}
-      </asset-link>
-      <span v-if="fileKey" @click="deleteFile" class="mdi mdi-delete delete"></span>
+      <template v-else>
+        <asset-link
+          :href="`storage://${fileKey}`"
+          :download="fileName"
+          class="file-name">
+          {{ fileName }}
+        </asset-link>
+        <span @click="deleteFile" class="mdi mdi-delete delete"></span>
+      </template>
     </form>
     <span class="help-block">{{ vErrors.first(id) }}</span>
   </div>
@@ -31,7 +30,6 @@
 
 <script>
 import CircularProgress from 'components/common/CircularProgress';
-import downloadMixin from 'utils/downloadMixin';
 import EventBus from 'EventBus';
 import uniqueId from 'lodash/uniqueId';
 import { withValidation } from 'utils/validation';
@@ -41,7 +39,7 @@ const appChannel = EventBus.channel('app');
 export default {
   name: 'file-upload',
   inject: ['$storageService'],
-  mixins: [downloadMixin, withValidation()],
+  mixins: [withValidation()],
   props: {
     id: { type: String, default: () => uniqueId('file_') },
     fileName: { type: String, default: '' },
@@ -50,29 +48,23 @@ export default {
     label: { type: String, default: 'Choose a file' },
     sm: { type: Boolean, default: false }
   },
-  data() {
-    return { uploading: false };
-  },
+  data: () => ({ uploading: false }),
   methods: {
-    createFileForm(e) {
-      this.form = new FormData();
-      const [file] = e.target.files;
-      if (!file) return;
-      this.form.append('file', file, file.name);
-    },
     upload(e) {
-      this.createFileForm(e);
-      this.$validator.validate(this.id).then(isValid => {
+      if (!e.target.files) return;
+      const [file] = e.target.files;
+      const data = new FormData();
+      data.append('file', file, file.name);
+      this.$validator.validateAll().then(isValid => {
         if (!isValid) return;
         this.uploading = true;
-        return this.$storageService.upload(this.form)
+        return this.$storageService.upload(data)
           .then(({ url, publicUrl, key }) => {
-            this.uploading = false;
-            const { name } = this.form.get('file');
+            const { name } = data.get('file');
             this.$emit('upload', { url, publicUrl, key, name });
-          }).catch(() => {
-            this.error = 'An error has occurred!';
-          });
+          })
+          .catch(() => (this.error = 'An error has occurred!'))
+          .finally(() => (this.uploading = false));
       });
     },
     deleteFile() {
