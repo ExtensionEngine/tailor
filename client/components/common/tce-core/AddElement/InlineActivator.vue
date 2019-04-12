ba44faa94cf3b2b46a7aedb77fde29d628385ad4
abasic <antoniobasic@gmail.com>
2019-04-12 11:26:20 +0200

    Improved styling of the inline activator

--- client/components/common/tce-core/AddElement/InlineActivator.vue
+++ client/components/common/tce-core/AddElement/InlineActivator.vue
@@ -1,7 +1,7 @@
 <template>
   <div class="inline-activator">
     <hr>
-    <v-icon color="blue-grey darken-2">mdi-plus</v-icon>
+    <v-icon class="activator" color="blue-grey darken-2">mdi-plus</v-icon>
     <hr>
   </div>
 </template>
@@ -13,19 +13,37 @@
   width: 100%;
   margin: 0;
   opacity: 0;
-  transition: opacity 0.5s ease-in-out;
+  transition: opacity 0.5s;
   cursor: pointer;
 
   hr {
     flex: 1;
     display: inline-flex;
     height: 2px;
-    margin: 0;
+    margin: 0 0 1px 0;
     background-color: #717171;
+    transition: height 0.2s;
+  }
+
+  .activator {
+    transition: font-size 0.2s;
   }
 
   &:hover {
+    $delay: 0.2s;
     opacity: 1;
+
+    &, hr, .activator {
+      transition-delay: $delay;
+    }
+
+    hr {
+      height: 3px;
+    }
+
+    .activator {
+      font-size: 40px;
+    }
   }
 }
 </style>
