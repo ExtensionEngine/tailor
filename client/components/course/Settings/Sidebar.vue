9eaf3b0a6e46201628f640ab7860736dd0ab5c4e
Damir Perisic <underscope@gmail.com>
2019-04-19 10:31:28 +0200

    Sidebar cleanup :lipstick:
    - Removed unnecessary methods
    - Removed repeated markup
    - UI tweaks

--- client/components/course/Settings/Sidebar.vue
+++ client/components/course/Settings/Sidebar.vue
@@ -1,73 +1,36 @@
 <template>
-  <v-navigation-drawer
-    stateless
-    value="true">
-    <v-list>
+  <v-navigation-drawer value="true" stateless>
+    <v-list class="grey--text text--darken-2">
       <v-list-tile
-        @click="routeTo('course-info')"
+        v-for="({ name, label, icon }) in routes"
+        :key="name"
+        :to="{ name }"
+        active-class="grey--text text--darken-3"
+        exact
         ripple>
         <v-list-tile-action>
-          <v-icon>mdi-wrench</v-icon>
+          <v-icon>{{ icon }}</v-icon>
         </v-list-tile-action>
-        <v-list-tile-title>General</v-list-tile-title>
-      </v-list-tile>
-      <v-list-tile
-        @click="routeTo('user-management')"
-        ripple>
-        <v-list-tile-action>
-          <v-icon>mdi-account</v-icon>
-        </v-list-tile-action>
-        <v-list-tile-title>User management</v-list-tile-title>
+        <v-list-tile-title>{{ label }}</v-list-tile-title>
       </v-list-tile>
       <v-list-group value="true">
         <template v-slot:activator>
           <v-list-tile>
-            <v-list-tile-title>Actions</v-list-tile-title>
+            <v-list-tile-title>Repository actions</v-list-tile-title>
           </v-list-tile>
         </template>
         <v-list-tile
-          @click="$emit('actionClick', 'knewton')"
-          color="blue-grey darken-3"
+          v-for="({ label, name, icon, color }) in actions"
+          :key="name"
+          :color="color ? color : 'blue-grey darken-3'"
+          @click="$emit('action', action)"
           ripple>
           <v-list-tile-action>
-            <v-icon>mdi-download</v-icon>
+            <v-icon :color="color ? color : 'blue-grey darken-2'">
+              mdi-{{ icon }}
+            </v-icon>
           </v-list-tile-action>
-          <v-list-tile-title>Knewton inventory</v-list-tile-title>
-        </v-list-tile>
-        <v-list-tile
-          @click="$emit('actionClick', 'clone')"
-          color="blue-grey darken-3"
-          ripple>
-          <v-list-tile-action>
-            <v-icon>mdi-content-copy</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Clone repository</v-list-tile-title>
-        </v-list-tile>
-        <v-list-tile
-          v-if="!isPublishing"
-          @click="$emit('actionClick', 'publish')"
-          color="blue-grey darken-3"
-          ripple>
-          <v-list-tile-action>
-            <v-icon>mdi-upload</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Publish content</v-list-tile-title>
-        </v-list-tile>
-        <v-list-tile
-          v-else
-          class="publishing-progress">
-          <v-progress-circular
-            color="blue-grey darken-3"
-            indeterminate/>
-        </v-list-tile>
-        <v-list-tile
-          @click.stop="$emit('actionClick', 'delete')"
-          color="error"
-          ripple>
-          <v-list-tile-action>
-            <v-icon color="error">mdi-delete</v-icon>
-          </v-list-tile-action>
-          <v-list-tile-title>Delete repository</v-list-tile-title>
+          <v-list-tile-title>{{ label }}</v-list-tile-title>
         </v-list-tile>
       </v-list-group>
     </v-list>
@@ -76,20 +39,27 @@
 
 <script>
 export default {
-  props: {
-    isPublishing: { type: Boolean, default: false }
-  },
-  methods: {
-    routeTo(name) {
-      this.$router.push({ name });
+  computed: {
+    routes() {
+      return [
+        { label: 'General', name: 'course-info', icon: 'mdi-wrench' },
+        { label: 'User management', name: 'user-management', icon: 'mdi-account' }
+      ];
+    },
+    actions() {
+      return [
+        { label: 'Knewton inventory', icon: 'download', name: 'knewton' },
+        { label: 'Clone', icon: 'content-copy', name: 'clone' },
+        { label: 'Publish', icon: 'upload', name: 'publish' },
+        { label: 'Delete', icon: 'delete', name: 'delete', color: 'error' }
+      ];
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.publishing-progress {
-  display: flex;
-  justify-content: center;
+.v-list__group::after {
+  background: none !important;
 }
 </style>
