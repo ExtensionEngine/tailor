ea3537a69ac997b27b0d2c99ca6f2a89c0f092cf
rkusan00 <renato.kusanovic@gmail.com>
2019-04-23 12:55:02 +0200

    Welcome and forgotpass templates match boutique ones

---
+++ server/shared/mail/index.js
@@ -0,0 +1,77 @@
+'use strict';
+
+const { renderHtml, renderText } = require('./render');
+const { origin } = require('../../../config/server');
+const email = require('emailjs');
+const logger = require('../logger');
+const path = require('path');
+const pick = require('lodash/pick');
+const Promise = require('bluebird');
+
+const EMAIL_ADDRESS = process.env.EMAIL_ADDRESS;
+const server = email.server.connect({
+  user: process.env.EMAIL_USER,
+  password: process.env.EMAIL_PASSWORD,
+  host: process.env.EMAIL_HOST,
+  ssl: true
+});
+logger.info(getConfig(server), '📧  SMTP client created');
+
+function send(message) {
+  return new Promise((resolve, reject) => {
+    server.send(message, (err, msg) => err ? reject(err) : resolve(msg));
+  });
+}
+
+const templatesDir = path.join(__dirname, './templates/');
+const resetUrl = user => `${origin}/#/reset-password/${user.token}`;
+
+function invite(user) {
+  const href = resetUrl(user);
+  const { hostname } = new URL(href);
+  const recipient = user.email;
+  const recipientName = user.firstName;
+  const data = { href, origin, hostname, recipientName };
+  const html = renderHtml(path.join(templatesDir, 'welcome.mjml'), data);
+  const text = renderText(path.join(templatesDir, 'welcome.txt'), data);
+  logger.info({ recipient, sender: EMAIL_ADDRESS }, '📧  Sending invite email to:', recipient);
+  return send({
+    from: EMAIL_ADDRESS,
+    to: recipient,
+    subject: 'Invite',
+    text,
+    attachment: [{ data: html, alternative: true }]
+  });
+}
+
+function resetPassword(user) {
+  const href = resetUrl(user);
+  const recipient = user.email;
+  const recipientName = user.firstName;
+  const data = { href, recipientName };
+  const html = renderHtml(path.join(templatesDir, 'reset.mjml'), data);
+  const text = renderText(path.join(templatesDir, 'reset.txt'), data);
+  logger.info({ recipient, sender: EMAIL_ADDRESS }, '📧  Sending reset password email to:', recipient);
+  return send({
+    from: EMAIL_ADDRESS,
+    to: recipient,
+    subject: 'Reset password',
+    text,
+    attachment: [{ data: html, alternative: true }]
+  });
+}
+
+function getConfig(server) {
+  // NOTE: List public keys: https://git.io/fxV4j
+  return pick(server.smtp, [
+    'host', 'port', 'domain',
+    'authentication', 'ssl', 'tls',
+    'timeout'
+  ]);
+}
+
+module.exports = {
+  send,
+  invite,
+  resetPassword
+};
---
+++ server/shared/mail/render.js
@@ -0,0 +1,58 @@
+'use strict';
+
+const { paramCase } = require('change-case');
+const cheerio = require('cheerio');
+const createDataUri = require('create-data-uri');
+const fileType = require('file-type');
+const fs = require('fs');
+const logger = require('../logger');
+const mapKeys = require('lodash/mapKeys');
+const map = require('lodash/map');
+const mjml2html = require('mjml');
+const path = require('path');
+const pupa = require('pupa');
+
+module.exports = {
+  renderHtml,
+  renderText
+};
+
+function renderHtml(templatePath, data, style) {
+  const template = fs.readFileSync(templatePath, 'utf8');
+  const $ = cheerio.load(template, { xmlMode: true });
+  const $style = $('mj-attributes');
+  $style.append(getAttributes($, style));
+  logger.debug({ style: dump($, $style) }, 'Style email using `mj-attributes`:');
+  $('img[src]').each((_, el) => {
+    const $image = $(el);
+    const imagePath = $image.attr('src');
+    $image.attr('src', getDataUri(imagePath));
+  });
+  const output = pupa($.html(), data);
+  return mjml2html(output, { minify: true }).html;
+}
+
+function renderText(templatePath, data) {
+  const template = fs.readFileSync(templatePath, 'utf8');
+  return pupa(template, data);
+}
+
+function getAttributes($, style = {}) {
+  return map(style, (declarations, name) => $('<mj-class>').attr({
+    name,
+    ...mapKeys(declarations, (_, key) => paramCase(key))
+  }));
+}
+
+function getDataUri(imagePath) {
+  const buf = fs.readFileSync(path.join(__dirname, './templates/', imagePath));
+  const { mime } = fileType(buf) || {};
+  return createDataUri(mime, buf.toString('base64'));
+}
+
+function dump($, $style) {
+  return $style.find('mj-class').get().reduce((acc, el) => {
+    const { name, ...attrs } = $(el).attr();
+    return Object.assign(acc, { [name]: attrs });
+  }, {});
+}
