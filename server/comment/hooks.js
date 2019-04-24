33504a6aafc8ced3c39f0cff2baf38274739b4ab
Damir Perisic <underscope@gmail.com>
2019-04-24 12:55:18 +0200

    Fixed comment hooks
    - Fixed issue with argument order

--- server/comment/hooks.js
+++ server/comment/hooks.js
@@ -4,18 +4,18 @@ const { broadcast, events } = require('./channel');
 const pick = require('lodash/pick');
 
 exports.add = (Comment, Hooks) => {
-  Comment.addHook(Hooks.afterCreate, (_, comment) => {
+  Comment.addHook(Hooks.afterCreate, (comment, _) => {
     comment.getAuthor().then(a => {
       const author = pick(a, ['id', 'email']);
       broadcast(events.CREATE, { ...comment.toJSON(), author });
     });
   });
 
-  Comment.addHook(Hooks.afterUpdate, (_, comment) => {
+  Comment.addHook(Hooks.afterUpdate, (comment, _) => {
     broadcast(events.UPDATE, comment);
   });
 
-  Comment.addHook(Hooks.afterDestroy, (_, comment) => {
+  Comment.addHook(Hooks.afterDestroy, (comment, _) => {
     Comment.findByPk(comment.id, { paranoid: false }).then(deleted => {
       broadcast(events.DELETE, deleted);
     });
