6581c2ef56d6ada897dbf5e527061e3f7ac621fe
Damir Perisic <underscope@gmail.com>
2019-04-24 13:12:55 +0200

    Cleanup :goat:

--- server/comment/hooks.js
+++ server/comment/hooks.js
@@ -4,18 +4,18 @@ const { broadcast, events } = require('./channel');
 const pick = require('lodash/pick');
 
 exports.add = (Comment, Hooks) => {
-  Comment.addHook(Hooks.afterCreate, (comment, _) => {
+  Comment.addHook(Hooks.afterCreate, comment => {
     comment.getAuthor().then(a => {
       const author = pick(a, ['id', 'email']);
       broadcast(events.CREATE, { ...comment.toJSON(), author });
     });
   });
 
-  Comment.addHook(Hooks.afterUpdate, (comment, _) => {
+  Comment.addHook(Hooks.afterUpdate, comment => {
     broadcast(events.UPDATE, comment);
   });
 
-  Comment.addHook(Hooks.afterDestroy, (comment, _) => {
+  Comment.addHook(Hooks.afterDestroy, comment => {
     Comment.findByPk(comment.id, { paranoid: false }).then(deleted => {
       broadcast(events.DELETE, deleted);
     });
