2f5f05c5f48447b6ddc8d7bb78a169a3354d6c49
Dario Vladovic <d.vladimyr@gmail.com>
2019-04-25 04:32:46 +0200

    Cleanup :lipstick:

--- client/plugins/asset/index.js
+++ client/plugins/asset/index.js
@@ -1,11 +1,11 @@
-import * as ResourceComponents from '../resource-components';
-import * as AssetLink from './link';
-import * as AssetUpload from './upload';
+import ResourceComponents from '../resource-components';
+import AssetLink from './link';
+import AssetUpload from './upload';
 
 export const install = (Vue, { auth, apiUrl, link, upload } = {}) => {
-  ResourceComponents.install(Vue, { auth });
-  AssetLink.install(Vue, { ...link, apiUrl });
-  AssetUpload.install(Vue, { ...upload, apiUrl });
+  Vue.use(ResourceComponents, { auth });
+  Vue.use(AssetLink, { ...link, apiUrl });
+  Vue.use(AssetUpload, { ...upload, apiUrl });
 };
 
 export default install;
