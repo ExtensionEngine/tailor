67eabc36aa3f36befedfa08d5c0a810084c135f1
zcuric <zcuric@gmail.com>
2019-06-04 10:50:05 +0200

    Add migration for source activity id in activity table

---
+++ server/shared/database/migrations/20190604084632-add-originId-to-acitivity.js
@@ -0,0 +1,17 @@
+'use strict';
+
+const TABLE_NAME = 'activity';
+const COLUMN_NAME = 'origin_id';
+
+module.exports = {
+  up: async (queryInterface, Sequelize) => {
+    const table = await queryInterface.describeTable(TABLE_NAME);
+    if (table.origin_id) return;
+    return queryInterface.addColumn(TABLE_NAME, COLUMN_NAME, {
+      type: Sequelize.INTEGER
+    });
+  },
+  down: queryInterface => {
+    return queryInterface.removeColumn(TABLE_NAME, COLUMN_NAME);
+  }
+};
