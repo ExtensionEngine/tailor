3e00391d7fb6f38364e26e018af07eae28e79f6e
Luka Saric <rpg.ari7@gmail.com>
2019-06-11 12:44:32 +0200

    Addressed issue
    - Fixed `rgba` missing spaces

--- client/components/content-elements/tce-modal/edit/Modal.vue
+++ client/components/content-elements/tce-modal/edit/Modal.vue
@@ -65,7 +65,7 @@ function toggleFocusTrap(focusTrap, condition) {
 }
 
 .modal.in[backdrop] {
-  background-color: rgba(0,0,0,0.5);
+  background-color: rgba(0, 0, 0, 0.5);
 }
 
 .modal-dialog {
@@ -103,9 +103,9 @@ function toggleFocusTrap(focusTrap, condition) {
   border: none;
   border-radius: 2px;
   box-shadow:
-    0 11px 15px -7px rgba(0,0,0,0.2),
-    0 24px 38px 3px rgba(0,0,0,0.14),
-    0 9px 46px 8px rgba(0,0,0,0.12);
+    0 11px 15px -7px rgba(0, 0, 0, 0.2),
+    0 24px 38px 3px rgba(0, 0, 0, 0.14),
+    0 9px 46px 8px rgba(0, 0, 0, 0.12);
   transition: all 0.3s ease;
 }
 
