4bd9a46d718d9ad3dcf9cecb8e0c70c0f96d5155
Dario Vladovic <d.vladimyr@gmail.com>
2019-06-26 23:27:12 +0200

    Split `module.exports` using `exports` :fox_face:

--- server/shared/database/migrations/20181115140943-add-meta-to-teaching-element.js
+++ server/shared/database/migrations/20181115140943-add-meta-to-teaching-element.js
@@ -3,16 +3,15 @@
 const TABLE_NAME = 'teaching_element';
 const COLUMN_NAME = 'meta';
 
-module.exports = {
-  up: async (queryInterface, Sequelize) => {
-    const table = await queryInterface.describeTable(TABLE_NAME);
-    if (table.meta) return;
-    return queryInterface.addColumn(TABLE_NAME, COLUMN_NAME, {
-      type: Sequelize.JSONB,
-      defaultValue: {}
-    });
-  },
-  down: queryInterface => {
-    return queryInterface.removeColumn(TABLE_NAME, COLUMN_NAME);
-  }
+exports.up = async (queryInterface, Sequelize) => {
+  const table = await queryInterface.describeTable(TABLE_NAME);
+  if (table.meta) return;
+  return queryInterface.addColumn(TABLE_NAME, COLUMN_NAME, {
+    type: Sequelize.JSONB,
+    defaultValue: {}
+  });
+};
+
+exports.down = queryInterface => {
+  return queryInterface.removeColumn(TABLE_NAME, COLUMN_NAME);
 };
