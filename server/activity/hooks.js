9afa72af926cb5cd2c5fb651b2041f78cfe8a731
Zdravko Ćurić <zcuric@gmail.com>
2019-06-27 10:22:08 +0200

    Pass transaction through afterDestroy hook

--- server/activity/hooks.js
+++ server/activity/hooks.js
@@ -3,24 +3,25 @@
 const Promise = require('bluebird');
 
 exports.add = (Activity, Hooks) => {
-  Activity.addHook(Hooks.afterDestroy, async activity => {
+  Activity.addHook(Hooks.afterDestroy, async (activity, options) => {
     const { isLink, origin } = activity;
     if (!isLink) return;
-    const links = await origin.getLinks();
+    const links = await origin.getLinks(options);
     if (links.length !== 1) return;
     const [ link ] = links;
-    const originTes = await origin.getTeachingElements();
+    const originTes = await origin.getTeachingElements(options);
+
     if (originTes.length) {
       await Promise.each(originTes, tes => tes.update({
         originId: null,
         activityId: link.id
-      }));
+      }, options));
     }
+
     await link.update({
       originId: null,
       data: origin.data
-    }, { revertLink: true });
-    await origin.destroy();
-    return link;
+    }, { ...options, revertLink: true });
+    return origin.destroy(options);
   });
 };
