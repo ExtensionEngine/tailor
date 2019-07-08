7e51901c35dba76df82577d50859cbb0a33cc9af
Damir Perisic <underscope@gmail.com>
2019-07-08 12:40:58 +0200

    Align store helpers with boutique ones

--- client/store/helpers/mutations.js
+++ client/store/helpers/mutations.js
@@ -2,31 +2,27 @@ import cuid from 'cuid';
 import each from 'lodash/each';
 import Vue from 'vue';
 
-export const fetch = (state, result) => {
-  each(result, it => {
-    // TODO: Extract api unrelated methods
-    state.api.setCid(it);
-    Vue.set(state.items, it._cid, it);
-  });
+export const fetch = (state, items) => {
+  each(items, it => Vue.set(state.items, it._cid, it));
 };
 
-export const reset = (state, result) => {
-  state.items = result;
+export const reset = (state, items = {}) => {
+  state.items = items;
 };
 
 export const add = (state, model) => {
-  if (!model._cid) model._cid = cuid();
+  const _cid = model._cid || cuid();
+  Vue.set(state.items, _cid, { ...model, _cid });
+};
+
+export const save = (state, model) => {
   Vue.set(state.items, model._cid, model);
 };
 
-export const save = (state, model) => Vue.set(state.items, model._cid, model);
-
-export const remove = (state, result) => {
-  result.forEach(it => Vue.delete(state.items, it._cid));
+export const remove = (state, models) => {
+  models.forEach(it => Vue.delete(state.items, it._cid));
 };
 
-export const update = (state, model) => Vue.set(state.items, model._cid, model);
-
-export const setBaseUrl = (state, url) => {
-  state.api.baseUrl = url;
+export const setEndpoint = (state, url) => {
+  state.$apiUrl = url;
 };
