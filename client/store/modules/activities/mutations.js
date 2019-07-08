9bb5d92f3ae80809d57788c4a6801304f33312f8
Damir Perisic <underscope@gmail.com>
2019-07-08 12:42:21 +0200

    Adapted activities module to store helpers

--- client/store/modules/activities/mutations.js
+++ client/store/modules/activities/mutations.js
@@ -1,7 +1,7 @@
-import { add, fetch, remove, reset, save, setBaseUrl, update } from '../../helpers/mutations';
+import { add, fetch, remove, reset, save, setEndpoint } from '../../helpers/mutations';
 
 const reorder = (state, { activity, position }) => {
-  activity.position = position;
+  state.items[activity._cid].position = position;
 };
 
-export { fetch, reset, add, save, remove, reorder, update, setBaseUrl };
+export { add, fetch, remove, reorder, reset, save, setEndpoint };
