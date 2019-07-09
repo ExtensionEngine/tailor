adaf7cc88059220366618fad4fd10df1cb65388d
Damir Perisic <underscope@gmail.com>
2019-07-09 07:48:52 +0200

    Minor tweaks :wrench:

--- client/components/system-settings/Sidebar.vue
+++ client/components/system-settings/Sidebar.vue
@@ -1,15 +1,15 @@
 <template>
   <v-navigation-drawer value="true" stateless>
-    <v-list class="grey--text text--darken-2">
+    <v-list class="grey--text text--darken-3">
       <v-list-tile
         v-for="({ name, label, icon }) in routes"
         :key="name"
         :to="{ name }"
-        active-class="grey--text text--darken-3"
+        active-class="blue-grey--text text--darken-4"
         exact
         ripple>
         <v-list-tile-action class="pl-1">
-          <v-icon>{{ icon }}</v-icon>
+          <v-icon>mdi-{{ icon }}</v-icon>
         </v-list-tile-action>
         <v-list-tile-title>{{ label }}</v-list-tile-title>
       </v-list-tile>
@@ -22,9 +22,9 @@ export default {
   computed: {
     routes() {
       return [
-        { label: 'System Users', name: 'system-user-management', icon: 'mdi-account' },
-        { label: 'Structure Types', name: 'installed-schemas', icon: 'mdi-file-tree' },
-        { label: 'Content Elements', name: 'installed-elements', icon: 'mdi-puzzle' }
+        { label: 'System Users', name: 'system-user-management', icon: 'account' },
+        { label: 'Structure Types', name: 'installed-schemas', icon: 'file-tree' },
+        { label: 'Content Elements', name: 'installed-elements', icon: 'puzzle' }
       ];
     }
   }
