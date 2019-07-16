83cdb3136070c0821150d723f4d2ae41858ec8e7
Igor Bebic <igor.bebic@gmail.com>
2019-07-16 13:26:39 +0200

    Replace custom modal with v-dialog

--- client/components/course/Settings/CloneModal.vue
+++ client/components/course/Settings/CloneModal.vue
@@ -1,56 +1,55 @@
 <template>
-  <modal :show="show">
-    <div slot="header">
-      <h3 class="modal-title">Clone repository</h3>
-    </div>
-    <div slot="body" class="modal-body">
-      <circular-progress v-if="showLoader" class="loader"></circular-progress>
-      <div v-else>
-        <div :class="{ 'has-error': vErrors.has('name') }" class="form-group">
-          <input
-            v-validate="{ required: true, min: 2, max: 250 }"
-            v-model="name"
-            class="form-control"
-            name="name"
-            type="text"
-            placeholder="Name"/>
-          <span class="help-block">{{ vErrors.first('name') }}</span>
+  <v-dialog :value="show" width="600px" persistent>
+    <v-card class="pa-3">
+      <v-card-title class="headline">Clone repository</v-card-title>
+      <v-card-text>
+        <div v-if="showLoader" class="search-spinner">
+          <v-progress-circular color="primary" indeterminate/>
         </div>
-        <div :class="{ 'has-error': vErrors.has('description') }" class="form-group">
-          <textarea
-            v-validate="{ required: true, min: 2, max: 2000 }"
-            v-model="description"
-            class="form-control"
-            name="description"
-            placeholder="Description">
-          </textarea>
-          <span class="help-block">{{ vErrors.first('description') }}</span>
+        <div v-else>
+          <div :class="{ 'has-error': vErrors.has('name') }" class="form-group">
+            <input
+              v-validate="{ required: true, min: 2, max: 250 }"
+              v-model="name"
+              class="form-control"
+              name="name"
+              type="text"
+              placeholder="Name"/>
+            <span class="help-block">{{ vErrors.first('name') }}</span>
+          </div>
+          <div
+            :class="{ 'has-error': vErrors.has('description') }"
+            class="form-group">
+            <textarea
+              v-validate="{ required: true, min: 2, max: 2000 }"
+              v-model="description"
+              class="form-control"
+              name="description"
+              placeholder="Description">
+            </textarea>
+            <span class="help-block">{{ vErrors.first('description') }}</span>
+          </div>
         </div>
-      </div>
-    </div>
-    <div slot="footer">
-      <button
-        :disabled="showLoader"
-        @click="close"
-        class="btn btn-material btn-default"
-        type="button">
-        Cancel
-      </button>
-      <button
-        :disabled="showLoader"
-        @click="cloneRepository"
-        class="btn btn-material btn-primary"
-        type="button">
-        Clone
-      </button>
-    </div>
-  </modal>
+      </v-card-text>
+      <v-card-actions>
+        <v-spacer/>
+        <v-btn :disabled="showLoader" @click="close">
+          Cancel
+        </v-btn>
+        <v-btn
+          :disabled="showLoader"
+          @click="cloneRepository"
+          color="primary"
+          outline>
+          Clone
+        </v-btn>
+      </v-card-actions>
+    </v-card>
+  </v-dialog>
 </template>
 
 <script>
-import CircularProgress from 'components/common/CircularProgress';
 import { mapActions } from 'vuex';
-import Modal from 'components/common/Modal';
 import pick from 'lodash/pick';
 import Promise from 'bluebird';
 
@@ -90,7 +89,6 @@ export default {
       this.vErrors.clear();
     }
   },
-  components: { CircularProgress, Modal },
   inject: ['$validator']
 };
 </script>
@@ -98,20 +96,5 @@ export default {
 <style lang="scss" scoped>
 .form-group {
   margin-bottom: 50px;
-}
-
-.modal-body {
-  min-height: 210px;
-  padding-right: 0;
-  padding-left: 0;
-}
-
-.loader {
-  position: absolute;
-  top: 0;
-  right: 0;
-  bottom: 0;
-  left: 0;
-  margin: auto;
 }
 </style>
