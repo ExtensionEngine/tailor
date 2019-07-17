7cd0986245c3b85a8ae0a67f7d0d3cfd49428c91
Damir Perisic <underscope@gmail.com>
2019-07-17 16:17:43 +0200

    Updated search component
    - Use vuetify

--- client/components/catalog/Search.vue
+++ client/components/catalog/Search.vue
@@ -1,30 +1,15 @@
 <template>
-  <div class="course-search">
-    <div
-      :style="{ width: expanded ? '100%' : '95%' }"
-      class="input-group">
-      <span class="input-group-btn">
-        <button
-          v-show="!query"
-          @mousedown.prevent="expand = true"
-          type="button"
-          class="btn">
-          <span class="mdi mdi-magnify"></span>
-        </button>
-        <button
-          v-show="query"
-          @mousedown.prevent="query = ''"
-          type="button"
-          class="btn">
-          <span class="mdi mdi-close"></span>
-        </button>
-      </span>
-      <input
+  <div class="repo-search">
+    <div :style="{ width: expanded ? '100%' : '95%' }">
+      <v-text-field
         v-focus="expanded"
         v-model.trim="query"
         @focus="expanded = true"
         @blur="expanded = false"
-        class="form-control"
+        @input="emitChange"
+        prepend-inner-icon="mdi-magnify"
+        solo
+        clearable
         placeholder="Search..."/>
     </div>
   </div>
@@ -46,49 +31,15 @@ export default {
       this.$emit('change', this.query);
     }, 500)
   },
-  watch: {
-    query() {
-      this.emitChange();
-    }
-  },
   directives: focus
 };
 </script>
 
 <style lang="scss" scoped>
-.course-search {
-  margin: 0 auto;
-
-  .input-group {
+.repo-search {
+  > div {
     margin: 0 auto;
-    overflow: hidden;
-    border-radius: 3px;
-    box-shadow:
-      0 2px 2px 0 rgba(0,0,0,0.14),
-      0 1px 5px 0 rgba(0,0,0,0.12),
-      0 3px 1px -2px rgba(0,0,0,0.2);
     transition: width 0.3s ease;
-  }
-
-  .form-control {
-    height: 44px;
-    padding: 0 7px;
-    font-size: 18px;
-    text-indent: 10px;
-    box-shadow: none;
-  }
-
-  .btn {
-    padding: 8px 12px 5px;
-    color: #656565;
-    font-size: 22px;
-    background-color: white;
-    box-shadow: none;
-    outline: none;
-
-    &:hover {
-      color: #444;
-    }
   }
 }
 </style>
