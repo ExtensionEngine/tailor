b03b3c9ae4fae4bc5802a30638005ac912138586
franosincic <fsincic@extensionengine.com>
2019-07-23 18:32:22 +0200

    Refactored privacy tab

--- client/components/user/Privacy.vue
+++ client/components/user/Privacy.vue
@@ -1,100 +1,103 @@
 <template>
   <v-card class="elevation-2">
-    <v-container class="user-privacy-container">
-      <v-card-title class="header elevation-2 primary" >
-        <v-icon>mdi-lock-open</v-icon>
-        <h4 class="title">Change Password</h4>
-      </v-card-title>
-      <v-form @submit.prevent="submit">
-        <v-layout class="fields-container">
-          <v-flex class="fields-box">
-            <v-text-field
-              v-validate="{ required: true, alphanumerical: true, min: 6 }"
-              v-model="currentPassword"
-              :error-messages="vErrors.first('currentPassword')"
-              data-vv-as="current password"
-              type="password"
-              name="currentPassword"
-              label="Current password"/>
-            <v-text-field
-              v-validate="{
-                required: true,
-                is_not: currentPassword,
-                alphanumerical: true,
-                min: 6
-              }"
-              ref="newPassword"
-              v-model="newPassword"
-              :error-messages="vErrors.first('newPassword')"
-              data-vv-as="new password"
-              type="password"
-              name="newPassword"
-              label="New password"/>
-            <v-text-field
-              v-validate="{ required: true, confirmed: 'newPassword' }"
-              v-model="confirmationPassword"
-              :error-messages="vErrors.first('confirmationPassword')"
-              data-vv-as="new password"
-              type="password"
-              name="confirmationPassword"
-              label="Please re-enter your new password"/>
-          </v-flex>
-          <v-card-actions>
-            <v-layout class="btn-actions">
-              <v-btn
-                :disabled="!isValidated"
-                type="submit"
-                color="light-blue darken-3"
-                flat>
-                Submit
-              </v-btn>
-            </v-layout>
-          </v-card-actions>
+    <v-card-title class="elevation-2 primary" >
+      <v-icon>mdi-lock-open</v-icon>
+      <h4>Change Password</h4>
+    </v-card-title>
+    <v-form @submit.prevent="submit">
+      <v-card-text>
+        <v-layout class="form-inputs">
+          <v-text-field
+            v-validate="{ required: true, alphanumerical: true, min: 6 }"
+            v-model="currentPassword"
+            :error-messages="vErrors.first('currentPassword')"
+            data-vv-as="current password"
+            type="password"
+            name="currentPassword"
+            label="Current password"/>
+          <v-text-field
+            v-validate="{
+              required: true,
+              is_not: currentPassword,
+              alphanumerical: true,
+              min: 6
+            }"
+            ref="newPassword"
+            v-model="newPassword"
+            :error-messages="vErrors.first('newPassword')"
+            data-vv-as="new password"
+            type="password"
+            name="newPassword"
+            label="New password"/>
+          <v-text-field
+            v-validate="{ required: true, confirmed: 'newPassword' }"
+            v-model="confirmationPassword"
+            :error-messages="vErrors.first('confirmationPassword')"
+            data-vv-as="new password"
+            type="password"
+            name="confirmationPassword"
+            label="Please re-enter your new password"/>
         </v-layout>
-      </v-form>
-    </v-container>
+      </v-card-text>
+      <v-card-actions>
+        <v-layout class="form-controls">
+          <v-spacer/>
+          <v-btn
+            :disabled="!hasChanges"
+            @click.prevent="reset"
+            color="pink"
+            flat>
+            Cancel
+          </v-btn>
+          <v-btn
+            :disabled="!isValid"
+            type="submit"
+            color="light-blue darken-3"
+            flat>
+            Submit
+          </v-btn>
+        </v-layout>
+      </v-card-actions>
+    </v-form>
   </v-card>
 </template>
 
 <script>
-import { mapActions, mapState } from 'vuex';
+import { mapActions } from 'vuex';
 import { withValidation } from 'utils/validation';
 
 const snackOpts = { right: true };
 
+const defaultData = () => ({
+  currentPassword: null,
+  newPassword: null,
+  confirmationPassword: null
+});
+
 export default {
   name: 'user-privacy',
   mixins: [withValidation()],
-  data() {
-    return {
-      currentPassword: '',
-      newPassword: '',
-      confirmationPassword: ''
-    };
-  },
+  data: () => defaultData(),
   computed: {
-    ...mapState({
-      user: state => state.auth.user
-    }),
-    isValidated: vm => Object.keys(vm.vFields).every(name => {
-      return vm.vFields[name] && vm.vFields[name].validated;
-    })
+    isValid: vm => Object.keys(vm.vFields).every(name => vm.vFields[name].valid),
+    hasChanges: vm => Object.keys(vm.vFields).some(name => vm.vFields[name].changed)
   },
   methods: {
     ...mapActions(['changePassword', 'logout']),
+    reset() {
+      Object.assign(this, defaultData());
+      return this.$validator.reset();
+    },
     submit() {
-      const { currentPassword, newPassword } = this;
-      this.$validator.validateAll()
+      const { currentPassword, newPassword, $snackbar } = this;
+      return this.$validator.validateAll()
         .then(isValid => {
-          if (!isValid) return this.$snackbar.error('Validation failed!');
+          if (!isValid) return $snackbar.error('Validation failed!', snackOpts);
           return this.changePassword({ currentPassword, newPassword })
-            .finally(() => {
-              this.currentPassword = this.newPassword = this.confirmationPassword = '';
-              requestAnimationFrame(() => this.$validator.reset());
-            })
-            .then(() => this.$snackbar.success('Password changed.', snackOpts))
+            .finally(() => this.reset())
+            .then(() => $snackbar.success('Password changed.', snackOpts))
             .then(() => this.logout())
-            .catch(() => this.$snackbar.error('An error has occurred!', snackOpts));
+            .catch(() => $snackbar.error('An error has occurred!', snackOpts));
         });
     }
   }
@@ -102,49 +105,32 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-$color: #fff;
-
-.user-privacy-container {
-  padding: 0;
-}
-
-.header {
+.v-card__title {
   height: 55px;
-  color: $color;
-
-  .title {
-    margin: 0 8px;
-    font-weight: 300;
-  }
+  color: #fff;
 
   .v-icon {
     margin-right: 8px;
     color: inherit;
   }
-}
 
-.fields-container {
-  flex-direction: column;
-  margin: 0 48px;
-  margin-top: 24px;
-}
-
-.fields-box {
-  margin: 0 38px;
-}
-
-.btn-actions {
-  justify-content: flex-end;
-  margin: 24px 0;
-
-  .v-btn {
-    width: 125px;
-    min-width: 100px;
-    margin-bottom: 10px;
+  h4 {
+    margin: 0 8px;
+    font-weight: 300;
   }
 }
 
-.v-card__actions {
-  padding: 0;
+.form-inputs {
+  flex-direction: column;
+  margin: 16px 64px;
+
+  .v-input {
+    height: 64px;
+    margin: 4px 0;
+  }
+}
+
+.form-controls {
+  margin: 0 64px 24px;
 }
 </style>
