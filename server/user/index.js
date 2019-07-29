511a2fa17796d7e3aa20661c0811d0971793481e
franosincic <fsincic@extensionengine.com>
2019-07-29 14:31:49 +0200

    Regular users can access their profile

--- server/user/index.js
+++ server/user/index.js
@@ -14,13 +14,13 @@ router
   .post('/users/forgot-password', ctrl.forgotPassword)
   .post('/users/reset-password', ctrl.resetPassword)
   // Protected routes:
-  .use('/users*', auth, authorize())
-  .get('/users', processPagination(User), ctrl.list)
-  .post('/users', ctrl.upsert)
+  .use('/users*', auth)
+  .get('/users', authorize(), processPagination(User), ctrl.list)
+  .post('/users', authorize(), ctrl.upsert)
   .post('/users/me/change-password', ctrl.changePassword)
   .patch('/users/me', ctrl.updateProfile)
-  .delete('/users/:id', ctrl.remove)
-  .post('/users/:id/reinvite', ctrl.reinvite);
+  .delete('/users/:id', authorize(), ctrl.remove)
+  .post('/users/:id/reinvite', authorize(), ctrl.reinvite);
 
 module.exports = {
   model,
