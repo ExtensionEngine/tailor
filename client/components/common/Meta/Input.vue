be5268ac1733283d2ba0c483553e9eea818f93a5
Igor Bebic <igor.bebic@gmail.com>
2019-07-29 09:16:13 +0200

    Remove readonly state for input, refactoring

--- client/components/common/Meta/Input.vue
+++ client/components/common/Meta/Input.vue
@@ -1,23 +1,13 @@
 <template>
-  <div
-    :class="{ editing }"
-    @focusout="focusoutInput"
-    @mousedown.stop="focusInput"
-    class="input">
-    <label :for="meta.key">{{ meta.label }}</label>
-    <v-text-field
-      v-validate="meta.validate"
-      v-show="editing"
-      v-model="value"
-      :ref="meta.key"
-      :name="meta.key"
-      :error-messages="vErrors.collect(meta.key)"
-      :placeholder="meta.placeholder"
-      @keyup.enter="focusoutInput"/>
-    <div v-show="!editing" @click.stop="focusInput">
-      <div class="content">{{ value || meta.placeholder }}</div>
-    </div>
-  </div>
+  <v-text-field
+    v-validate="meta.validate"
+    v-model="value"
+    :name="meta.key"
+    :error-messages="vErrors.collect(meta.key)"
+    :label="meta.label"
+    :placeholder="meta.placeholder"
+    @change="validateAndUpdate"
+    outline/>
 </template>
 
 <script>
@@ -31,19 +21,13 @@ export default {
   },
   data() {
     return {
-      value: this.meta.value,
-      editing: false
+      value: this.meta.value
     };
   },
   methods: {
-    focusInput() {
-      this.editing = true;
-      setTimeout(() => this.$refs[this.meta.key].focus(), 0);
-    },
-    focusoutInput() {
+    validateAndUpdate() {
       this.$validator.validate(this.meta.key).then(result => {
         if (!result) return;
-        this.editing = false;
         if (this.value === this.meta.value) return;
         this.$emit('update', this.meta.key, this.value);
       });
@@ -51,33 +35,3 @@ export default {
   }
 };
 </script>
-
-<style lang="scss" scoped>
-.input {
-  padding: 3px 8px;
-  cursor: pointer;
-
-  &:hover {
-    background-color: #f5f5f5;
-  }
-
-  &.editing:hover {
-    background-color: inherit;
-  }
-
-  label {
-    color: #808080;
-  }
-
-  .v-input {
-    min-height: 70px;
-  }
-
-  .content {
-    margin: 20px 3px 30px 0;
-    color: #333;
-    font-size: 16px;
-    word-wrap: break-word;
-  }
-}
-</style>
