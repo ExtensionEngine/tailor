0ff202921776b927b09088600c59162ce00e37ce
borna223 <bborna00@gmail.com>
2019-10-04 17:20:20 +0200

    Refactor tooltip node resolving

--- client/components/content-elements/tce-jodit-html/edit/controls/tooltip/index.js
+++ client/components/content-elements/tce-jodit-html/edit/controls/tooltip/index.js
@@ -1,119 +1,61 @@
-import cuid from 'cuid';
-import isFunction from 'lodash/isFunction';
 import Jodit from 'Jodit';
 
-const ENTER_KEY_CODE = 13;
 const TOOLTIP_CONTROL = 'tooltip';
+const TOOLTIP_TAG_NAME = 'tooltip-element';
 const TOOLTIP_ATTRIBUTE = 'data-tooltip';
-const JODIT_FORM_CLASS = 'jodit_form';
+
+class TooltipElement extends HTMLElement {}
+customElements.define(TOOLTIP_TAG_NAME, TooltipElement);
 
 export default {
   [TOOLTIP_CONTROL]: {
-    popup: (editor, _, __, close) => {
-      const node = resolveTooltipNode(editor);
-      if (node) return createTooltipWidget(editor, node, close);
+    popup: (editor, current, self, close) => {
+      const { sel } = editor.selection;
+      const { val } = Jodit.modules.Helpers;
+      const form = editor.create.fromHTML(
+        `<form class="jodit_form">
+          <textarea name="tooltip"></textarea>
+          <input name="text" type="text">
+          <div style="text-align: right">
+            <button name="delete" type="button">Delete</button>
+            <button name="submit" type="submit">Submit</button>
+          </div>
+        </form>`
+      );
+
+      const args = [current, TOOLTIP_TAG_NAME, editor.editor];
+      current = (current && Jodit.modules.Dom.closest(...args)) || false;
+      if (current) {
+        const tooltipValue = current.getAttribute(TOOLTIP_ATTRIBUTE) || '';
+        val(form, 'textarea[name=tooltip]', tooltipValue);
+        val(form, 'input[name=text]', current.innerText);
+      } else {
+        val(form, 'input[name=text]', sel ? sel.toString() : '');
+      }
+
+      const selInfo = editor.selection.save();
+
+      form.addEventListener('submit', event => {
+        event.preventDefault();
+        editor.selection.restore(selInfo);
+        const tooltipElement = current || new TooltipElement();
+        const tooltipValue = val(form, 'textarea[name=tooltip]');
+        tooltipElement.setAttribute(TOOLTIP_ATTRIBUTE, tooltipValue);
+        tooltipElement.innerText = val(form, 'input[name=text]');
+        tooltipElement.classList.add('tailor_jodit_tooltip');
+        if (!current) editor.selection.insertNode(tooltipElement);
+        close();
+      });
+
+      const deleteButton = form.querySelector('button[name=delete]');
+      deleteButton.addEventListener('click', event => {
+        event.preventDefault();
+        if (current) Jodit.modules.Dom.unwrap(current);
+        editor.selection.restore(selInfo);
+        close();
+      });
+
+      return form;
     }
   }
 };
-
-function resolveTooltipNode(editor) {
-  const { editor: root, selection: { sel, range } } = editor;
-  const { startContainer, endContainer, commonAncestorContainer: parent } = range;
-  if (isTooltipNode(startContainer)) return startContainer;
-  if (isTooltipNode(endContainer)) return endContainer;
-  return resolveWithinRangeParent(parent, sel) ||
-    resolveOutsideRangeParent(root, parent) ||
-    createTooltipNode(editor);
-}
-
-function resolveWithinRangeParent(parent, sel, options = {}) {
-  const { tagName = 'span', partialContainment = true } = options;
-  if (!isFunction(parent.getElementsByTagName)) return null;
-  const collection = parent.getElementsByTagName(tagName);
-  return Array.from(collection).find(node => {
-    return sel.containsNode(node, partialContainment) &&
-      isTooltipNode(node);
-  });
-}
-
-function resolveOutsideRangeParent(root, node = {}) {
-  if ([root, document].includes(node)) return null;
-  if (isTooltipNode(node)) return node;
-  const { parentNode: parent } = node;
-  return resolveOutsideRangeParent(root, parent);
-}
-
-function createTooltipNode(editor, { tagName = 'span' } = {}) {
-  const { selection: { sel } } = editor;
-  const id = cuid();
-  const html =
-    `<${tagName} id="${id}" ${TOOLTIP_ATTRIBUTE}="">` +
-      `${sel}` +
-    `</${tagName}>`;
-  editor.execCommand('insertHtml', false, html);
-  return document.getElementById(`${id}`);
-}
-
-function isTooltipNode(node = {}) {
-  if (!isFunction(node.hasAttribute)) return false;
-  return node.hasAttribute(TOOLTIP_ATTRIBUTE);
-}
-
-function createTooltipWidget(editor, node, close) {
-  const form = createTooltipForm(editor, node);
-  setupSubmissionHandler(editor, form, node, close);
-  setupDeletionHandler(editor, form, node, close);
-  setupEnterKeyDownHandler(form);
-  const tabs = { tooltip: form };
-  return new Jodit.modules.Widget.TabsWidget(editor, tabs);
-}
-
-function createTooltipForm(editor, node) {
-  const tooltip = node.getAttribute(TOOLTIP_ATTRIBUTE);
-  return editor.create.fromHTML(
-    `<form onsubmit="return false;" class="${JODIT_FORM_CLASS}">
-      <textarea name="tooltip">${tooltip}</textarea>
-      <div style="text-align: right">
-        <button name="delete" type="button">Delete</button>
-        <button name="submit" type="submit">Save</button>
-      </div>
-    </form>`
-  );
-}
-
-function setupSubmissionHandler(editor, form, node, close) {
-  form.addEventListener('submit', event => {
-    event.preventDefault();
-    event.stopPropagation();
-    const { value: tooltip } = form.querySelector(`textarea[name="tooltip"]`);
-    tooltip
-      ? node.setAttribute(TOOLTIP_ATTRIBUTE, tooltip)
-      : removeTooltipNode(editor, node);
-    close();
-  }, { once: true });
-}
-
-function setupDeletionHandler(editor, form, node, close) {
-  const deleteButton = form.querySelector(`button[name="delete"]`);
-  deleteButton.addEventListener('click', () => {
-    removeTooltipNode(editor, node);
-    close();
-  }, { once: true });
-}
-
-function setupEnterKeyDownHandler(form) {
-  const textArea = form.querySelector(`textarea[name="tooltip"]`);
-  textArea.addEventListener('keydown', event => {
-    if (event.keyCode !== ENTER_KEY_CODE) return;
-    event.stopPropagation();
-  });
-}
-
-function removeTooltipNode(editor, node = {}) {
-  const docFrag = document.createDocumentFragment();
-  while (node.firstChild) {
-    const child = node.removeChild(node.firstChild);
-    docFrag.appendChild(child);
-  }
-  if (node.parentNode) node.parentNode.replaceChild(docFrag, node);
-}
