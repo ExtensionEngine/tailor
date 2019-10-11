a5bcbedab263a58bb82ce020c141857e421d5d08
Dario Vladovic <d.vladimyr@gmail.com>
2019-10-11 05:23:35 +0200

    Migrate away from deprecated `@babel/polyfill`

--- client/polyfills.js
+++ client/polyfills.js
@@ -1,4 +1,5 @@
-import '@babel/polyfill';
+import 'core-js/stable';
+import 'regenerator-runtime/runtime';
 import 'dom-shims/shim/Element.classList';
 import 'dom-shims/shim/Element.mutation';
 import { EventSource } from 'event-source-polyfill';
