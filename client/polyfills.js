c02bae28df28270f370d22c796149ba5fd5276f6
Dario Vladovic <d.vladimyr@gmail.com>
2019-10-15 09:18:31 +0200

    Use fetch transport for SSE

--- client/polyfills.js
+++ client/polyfills.js
@@ -2,5 +2,5 @@ import 'core-js/stable';
 import 'regenerator-runtime/runtime';
 import 'dom-shims/shim/Element.classList';
 import 'dom-shims/shim/Element.mutation';
-import { EventSource } from 'event-source-polyfill';
-window.EventSource = EventSource;
+import { EventSourcePolyfill } from 'event-source-polyfill';
+window.EventSource = EventSourcePolyfill;
