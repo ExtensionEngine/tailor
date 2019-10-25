65f91f68c24b412b008a44c611d2462a29cf04a0
Tomislav Tokic <ttokic@extensionengine.com>
2019-10-25 20:55:20 +0200

    Added VUEX_PERSIST_STORAGE_KEY in .env

--- client/store/plugins/vuex-persist.js
+++ client/store/plugins/vuex-persist.js
@@ -1,12 +1,12 @@
 import VuexPersistence from 'vuex-persist';
 
-const STORAGE_KEY = 'TAILOR_USER';
+const STORAGE_KEY = process.env.VUEX_PERSIST_STORAGE_KEY;
+const MODULES_TO_PERSIST = ['auth'];
 
 export default new VuexPersistence({
   // The key to store the state on in the storage provider.
   key: STORAGE_KEY,
-  // Only save auth module.
-  modules: ['auth'],
+  modules: MODULES_TO_PERSIST,
   // Use localStorage
   storage: window.localStorage
 }).plugin;
