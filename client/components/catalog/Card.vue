20ef872617cff3983e9616d9cd0d180751444281
franosincic <fsincic@extensionengine.com>
2019-10-28 19:46:12 +0100

    Fix issues

--- client/components/catalog/Card.vue
+++ client/components/catalog/Card.vue
@@ -1,5 +1,5 @@
 <template>
-  <v-card @click="navigateTo" class="repository-card">
+  <v-card @click="navigateTo()" class="repository-card">
     <div class="card-heading blue-grey darken-4">
       <v-chip :color="repository.data.color" small label class="ml-3 mr-0" />
       <v-chip color="grey lighten-3" small label class="ml-0">
@@ -10,7 +10,7 @@
           flat
           icon
           color="grey"
-          class="btn-settings text--darken-1 ml-auto my-0">
+        class="btn-settings text--darken-1 pull-right my-0">
           <v-icon>mdi-settings</v-icon>
         </v-btn>
       <v-card-title class="headline grey--text text--lighten-4 pt-1">
