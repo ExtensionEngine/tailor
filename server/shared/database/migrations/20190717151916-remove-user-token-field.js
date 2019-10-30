31362c3f3e484c75c3cbe340269602c75a753ec7
Damir Perisic <underscope@gmail.com>
2019-10-30 22:27:00 +0100

    Force pw being set in order to have proper invite tokens

--- server/shared/database/migrations/20190717151916-remove-user-token-field.js
+++ server/shared/database/migrations/20190717151916-remove-user-token-field.js
@@ -3,10 +3,20 @@
 const { Sequelize } = require('sequelize');
 
 const TABLE = 'user';
-const COLUMN = 'token';
-const options = { type: Sequelize.STRING, unique: true };
 
 module.exports = {
-  up: queryInterface => queryInterface.removeColumn(TABLE, COLUMN, options),
-  down: queryInterface => queryInterface.addColumn(TABLE, COLUMN, options)
+  up: async qi => {
+    await qi.removeColumn(TABLE, 'token');
+    await qi.changeColumn(TABLE, 'password', {
+      type: Sequelize.STRING,
+      allowNull: false
+    });
+  },
+  down: async qi => {
+    await qi.addColumn(TABLE, 'token', { type: Sequelize.STRING, unique: true });
+    await qi.changeColumn(TABLE, 'password', {
+      type: Sequelize.STRING,
+      allowNull: true
+    });
+  }
 };
