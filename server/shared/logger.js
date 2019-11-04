02d881dc004e105aebfe74a4721449c14e353a21
Dario Vladovic <d.vladimyr@gmail.com>
2019-11-04 01:41:50 +0100

    Prettify queries using `sql-log-prettifier`

--- server/shared/logger.js
+++ server/shared/logger.js
@@ -2,15 +2,10 @@
 
 const bunyan = require('bunyan');
 
-const logLevel = (() => {
-  if (process.env.LOG_LEVEL) return process.env.LOG_LEVEL;
-  return process.env.NODE_ENV === 'production' ? 'info' : 'debug';
-})();
-
 const logger = bunyan.createLogger({
   name: 'tailor-server',
   serializers: bunyan.stdSerializers,
-  level: logLevel
+  level: bunyan.levelFromName.debug
 });
 
 module.exports = logger;
