0b3794d58c64fd7834e183d7b0eece0f8068e405
Damir Perisic <underscope@gmail.com>
2019-11-15 11:08:16 +0100

    Temp fix snackbar
    - Defer mounting

--- client/plugins/vuetify-snackbar/index.js
+++ client/plugins/vuetify-snackbar/index.js
@@ -5,18 +5,22 @@ const queue = new Queue(1, Infinity);
 
 export const install = (Vue, { parent } = {}) => {
   const SnackbarCtor = Vue.extend(Snackbar);
+  // Create unmounted snackbar instance; $mount called without selector
   const vm = new SnackbarCtor().$mount();
   document.addEventListener('DOMContentLoaded', () => {
-    const element = parent || document.getElementById('app');
-    element.appendChild(vm.$el);
+    // TODO: Temp fix, should be properly registrated
+    setTimeout(() => {
+      const element = parent || document.getElementById('app');
+      element.appendChild(vm.$el);
+    }, 1000);
   });
   const $snackbar = {
-    show: (msg, options) => queue.add(() => vm.show(msg, options)),
+    show: (msg, opts) => queue.add(() => vm.show(msg, opts)),
     close: () => vm.close,
-    success: (msg, options) => $snackbar.show(msg, { ...options, color: 'success' }),
-    info: (msg, options) => $snackbar.show(msg, { ...options, color: 'info' }),
-    warning: (msg, options) => $snackbar.show(msg, { ...options, color: 'warning' }),
-    error: (msg, options) => $snackbar.show(msg, { ...options, color: 'error' })
+    success: (msg, opts) => $snackbar.show(msg, { ...opts, color: 'success' }),
+    info: (msg, opts) => $snackbar.show(msg, { ...opts, color: 'info' }),
+    warning: (msg, opts) => $snackbar.show(msg, { ...opts, color: 'warning' }),
+    error: (msg, opts) => $snackbar.show(msg, { ...opts, color: 'error' })
   };
   Object.assign(Vue.prototype, { $snackbar });
 };
