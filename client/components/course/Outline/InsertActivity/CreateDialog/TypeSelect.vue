9c0c80f1835b8440694ae9839f83959e69c71004
Damir Perisic <underscope@gmail.com>
2019-12-04 17:07:53 +0100

    Basic create modal
    - WIP

---
+++ client/components/course/Outline/InsertActivity/CreateDialog/TypeSelect.vue
@@ -0,0 +1,59 @@
+<template>
+  <v-select
+    v-validate="{ required: true }"
+    @change="$emit('input', $event)"
+    :value="value"
+    :items="groupedOptions"
+    :error-messages="vErrors.collect('type')"
+    item-text="label"
+    item-value="type"
+    name="type"
+    label="Type"
+    outlined
+    class="required">
+    <template slot="item" slot-scope="{ item }">
+      <div v-if="item.group" class="pr-5">
+        <v-icon color="grey" size="16" class="pr-1">mdi-folder-open-outline</v-icon>
+        <span class="pt-2">{{ item.group }}</span>
+      </div>
+      <div
+        v-else
+        :class="{ 'pl-6': item.level > rootLevel }"
+        class="black--text">
+        <v-icon size="16" color="grey" class="pr-1">
+          mdi-{{ hasSubtypes(item) ? 'folder' : 'file-document-box-outline' }}
+        </v-icon>
+        {{ item.label }}
+      </div>
+    </template>
+  </v-select>
+</template>
+
+<script>
+import partition from 'lodash/partition';
+import size from 'lodash/size';
+import sortedUniq from 'lodash/sortedUniq';
+import { withValidation } from 'utils/validation';
+
+export default {
+  name: 'activity-type-select',
+  mixins: [withValidation({ inherit: true })],
+  props: {
+    value: { type: String, default: null },
+    options: { type: Array, required: true }
+  },
+  computed: {
+    levels: vm => sortedUniq(vm.options.map(it => it.level)),
+    rootLevel: vm => vm.levels[0],
+    groupedOptions() {
+      const { options, rootLevel } = this;
+      const [sameLevel, subLevel] = partition(options, { level: rootLevel });
+      if (!subLevel.length) return sameLevel;
+      return [...sameLevel, { group: 'Sublevels', disabled: true }, ...subLevel];
+    }
+  },
+  methods: {
+    hasSubtypes: item => !!size(item.subLevels)
+  }
+};
+</script>
---
+++ client/components/course/Outline/InsertActivity/CreateDialog/index.vue
@@ -0,0 +1,82 @@
+<template>
+  <v-dialog v-model="dialog" width="500">
+    <form @submit.prevent="create">
+      <v-card>
+        <v-card-title
+          primary-title
+          class="py-4 heading primary darken-1 white--text">
+          <v-avatar size="38" color="secondary" class="mr-3">
+            <v-icon dark>mdi-folder-plus-outline</v-icon>
+          </v-avatar>
+          Create
+        </v-card-title>
+        <v-card-text class="pt-8">
+          <type-select v-model="activity.type" :options="levels" />
+          <template v-if="activity.type">
+            <meta-input
+              v-for="input in metadata"
+              :key="input.key"
+              @update="setMetaValue"
+              :meta="input" />
+          </template>
+        </v-card-text>
+        <v-card-actions class="px-4 py-3">
+          <v-spacer />
+          <v-btn @click="dialog = false" text>Cancel</v-btn>
+          <v-btn color="primary" type="submit" text>Create</v-btn>
+        </v-card-actions>
+      </v-card>
+    </form>
+  </v-dialog>
+</template>
+
+<script>
+import { mapActions, mapGetters } from 'vuex';
+import { isSameLevel } from 'utils/activity';
+import MetaInput from 'components/common/Meta';
+import TypeSelect from './TypeSelect';
+import { withValidation } from 'utils/validation';
+
+const initActivityState = repositoryId => ({ repositoryId, type: null, data: {} });
+
+export default {
+  name: 'create-activity-dialog',
+  mixins: [withValidation()],
+  props: {
+    repositoryId: { type: Number, required: true },
+    levels: { type: Array, required: true },
+    anchor: { type: Object, default: null }
+  },
+  data() {
+    return {
+      dialog: true,
+      activity: initActivityState(this.repositoryId)
+    };
+  },
+  computed: {
+    ...mapGetters('course', ['getMetadata']),
+    ...mapGetters('activities', ['calculateInsertPosition']),
+    metadata() {
+      if (!this.activity.type) return null;
+      return this.getMetadata({ type: this.activity.type });
+    }
+  },
+  methods: {
+    ...mapActions('activities', ['save']),
+    setMetaValue(key, val) {
+      this.activity.data[key] = val;
+    },
+    async create() {
+      const isValid = await this.$validator.validateAll();
+      if (!isValid) return;
+      const { activity, anchor } = this;
+      activity.parentId = isSameLevel(activity, anchor) ? anchor.parentId : anchor.id;
+      activity.position = this.calculateInsertPosition(activity, anchor);
+      this.save({ ...activity });
+      if (anchor.id === activity.parentId) this.$emit('expand');
+      this.dialog = false;
+    }
+  },
+  components: { MetaInput, TypeSelect }
+};
+</script>
