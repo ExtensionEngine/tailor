f3608f5a7d68d1c6d44d1d3088596d7cd78ff481
Igor Bebic <igor.bebic@gmail.com>
2019-12-19 16:12:40 +0100

    DatePicker component cleanup

--- client/components/common/Meta/DatePicker/DatePicker.vue
+++ client/components/common/Meta/DatePicker/DatePicker.vue
@@ -8,53 +8,33 @@
       <v-text-field
         v-on="on"
         @click:append="show = true"
-        :value="displayedValue"
+        :value="value | formatDate('MMM D, YYYY')"
         :label="label"
         :placeholder="placeholder"
         outlined
         readonly />
     </template>
     <v-date-picker
-      ref="picker"
       @input="save"
-      v-bind="$attrs"
-      :value="normalizedValue"
+      :value="value | formatDate('YYYY-MM-DD')"
       color="primary"
       no-title />
   </v-menu>
 </template>
 
 <script>
-import fecha from 'fecha';
-
-const NORMALIZED_FORMAT = 'YYYY-MM-DD';
-const DISPLAY_FORMAT = 'MMM D, YYYY';
-
 export default {
   name: 'date-picker',
   props: {
     label: { type: String, default: null },
     value: { type: String, default: null },
-    inputFormat: { type: String, default: NORMALIZED_FORMAT },
     placeholder: { type: String, default: 'Set Date' }
   },
   data: () => ({ show: false }),
-  computed: {
-    normalizedValue: vm => vm.normalize(vm.value, vm.inputFormat, NORMALIZED_FORMAT),
-    displayedValue() {
-      return this.value && fecha.format(new Date(this.value), DISPLAY_FORMAT);
-    }
-  },
   methods: {
     save(value) {
-      const { inputFormat } = this;
-      this.$emit('change', this.normalize(value, NORMALIZED_FORMAT, inputFormat));
+      this.$emit('change', value);
       this.show = false;
-    },
-    normalize(value, inputFormat, outputFormat) {
-      if (!value) return;
-      const date = fecha.parse(value, inputFormat);
-      return fecha.format(date, outputFormat);
     }
   }
 };
