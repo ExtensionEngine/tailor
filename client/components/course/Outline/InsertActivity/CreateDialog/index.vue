e357f08c872675858faca1442a1fc195449c2ef9
Damir Perisic <underscope@gmail.com>
2019-12-20 08:02:00 +0100

    Use TailorDialog for activity creation

--- client/components/course/Outline/InsertActivity/CreateDialog/index.vue
+++ client/components/course/Outline/InsertActivity/CreateDialog/index.vue
@@ -1,48 +1,37 @@
 <template>
-  <v-dialog v-model="visible" width="500">
+  <tailor-dialog v-model="visible" header-icon="mdi-folder-plus-outline">
     <template v-if="showActivator" v-slot:activator="{ on }">
       <v-btn v-on="on" color="grey darken-3" text class="px-1">
         <v-icon class="pr-1">mdi-plus</v-icon>
         Create {{ hasSingleOption ? levels[0].label : '' }}
       </v-btn>
     </template>
-    <form @submit.prevent="create">
-      <v-card>
-        <v-card-title
-          primary-title
-          class="py-4 heading primary darken-1 white--text">
-          <v-avatar size="38" color="secondary" class="mr-3">
-            <v-icon dark>mdi-folder-plus-outline</v-icon>
-          </v-avatar>
-          Create
-        </v-card-title>
-        <v-card-text :key="visible" class="pt-8">
-          <type-select
-            v-model="activity.type"
-            :options="levels"
-            :disabled="hasSingleOption" />
-          <template v-if="activity.type">
-            <meta-input
-              v-for="input in metadata"
-              :key="input.key"
-              @update="setMetaValue"
-              :meta="input" />
-          </template>
-        </v-card-text>
-        <v-card-actions class="px-4 py-3">
-          <v-spacer />
-          <v-btn @click="visible = false" text>Cancel</v-btn>
-          <v-btn color="primary" type="submit" text>Create</v-btn>
-        </v-card-actions>
-      </v-card>
-    </form>
-  </v-dialog>
+    <template v-slot:header>Create</template>
+    <template v-slot:body>
+      <type-select
+        v-model="activity.type"
+        :options="levels"
+        :disabled="hasSingleOption" />
+      <template v-if="activity.type">
+        <meta-input
+          v-for="input in metadata"
+          :key="input.key"
+          @update="setMetaValue"
+          :meta="input" />
+      </template>
+    </template>
+    <template v-slot:actions>
+      <v-btn @click="visible = false" text>Cancel</v-btn>
+      <v-btn @click="create" color="primary" text>Create</v-btn>
+    </template>
+  </tailor-dialog>
 </template>
 
 <script>
 import { mapActions, mapGetters, mapMutations } from 'vuex';
 import { isSameLevel } from 'utils/activity';
 import MetaInput from 'components/common/Meta';
+import TailorDialog from '@/components/common/TailorDialog';
 import TypeSelect from './TypeSelect';
 import { withValidation } from 'utils/validation';
 
@@ -106,6 +95,6 @@ export default {
       }
     }
   },
-  components: { MetaInput, TypeSelect }
+  components: { MetaInput, TailorDialog, TypeSelect }
 };
 </script>
