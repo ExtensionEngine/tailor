c19b6827cf01a75e4a7db78815514aedafe718e3
abasic <antoniobasic@gmail.com>
2020-02-17 22:07:23 +0100

    Added assessment block insertion script

---
+++ server/script/insertAssessmentBlocks.js
@@ -0,0 +1,62 @@
+'use strict';
+
+const { Activity, ContentElement, sequelize } = require('../shared/database');
+const get = require('lodash/get');
+const Promise = require('bluebird');
+const { SCHEMAS } = require('../../config/shared/activities');
+
+const DEFAULTS = { data: {}, type: 'ASSESSMENT_BLOCK' };
+
+const types = extractAssessedTypes(SCHEMAS);
+
+insertAssessmentBlocks(types)
+  .then(() => {
+    console.info('Inserted assessment blocks.');
+    process.exit(0);
+  })
+  .catch(error => {
+    console.error(error.message);
+    process.exit(1);
+  });
+
+function extractAssessedTypes(schemas) {
+  return schemas.reduce((types, { structure }) => {
+    structure.forEach(it => it.hasAssessments && types.push(it.type));
+    return types;
+  }, []);
+}
+
+async function insertAssessmentBlocks(types) {
+  const transaction = await sequelize.transaction();
+  const assessed = await getAssessedActivitites(types, transaction);
+  await Promise.each(assessed, it => insertAssessmentBlock(it, transaction));
+  return transaction.commit();
+}
+
+function getAssessedActivitites(types, transaction) {
+  const options = { where: { type: types }, transaction };
+  return Activity.findAll(options);
+}
+
+async function insertAssessmentBlock(activity, transaction) {
+  const parentId = activity.id;
+  const assessmentBlock = await createAssessmentBlock(activity, transaction);
+  const updates = { activityId: assessmentBlock.id };
+  const options = { where: { activityId: parentId }, transaction };
+  return ContentElement.update(updates, options);
+}
+
+async function createAssessmentBlock(activity, transaction) {
+  const { id: parentId, repositoryId } = activity;
+  const position = await getPosition(parentId, transaction);
+  const data = { ...DEFAULTS, parentId, repositoryId, position };
+  return Activity.create(data, { transaction });
+}
+
+function getPosition(parentId, transaction) {
+  const where = { parentId };
+  const order = [['position', 'DESC']];
+  return Activity
+    .findOne({ where, order, transaction })
+    .then(it => get(it, 'position', 0) + 1);
+}
