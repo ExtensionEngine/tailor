7f1da3b656db6a402b2afb46455544f303fe91ba
abasic <antoniobasic@gmail.com>
2020-02-18 12:47:52 +0100

    Update assessment block util to support publishing

--- client/components/content-containers/tcc-assessment-block/util.js
+++ client/components/content-containers/tcc-assessment-block/util.js
@@ -1,11 +1,29 @@
 const info = require('./info');
+const pick = require('lodash/pick');
+const Promise = require('bluebird');
+
+const ATTRS = [
+  'id', 'uid', 'type', 'position', 'parentId', 'createdAt', 'updatedAt'
+];
+
+async function fetchBlock(block) {
+  const elements = await block.getContentElements({ raw: true });
+  return { ...pick(block, ATTRS), elements };
+}
 
 function fetch(parent) {
   const opts = { where: { type: info.type } };
-  return parent.getChildren(opts);
+  return parent.getChildren(opts).map(fetchBlock);
+}
+
+async function resolve(block, resolveStatics) {
+  block.elements = await Promise.map(block, resolveStatics);
+  return block;
 }
 
 module.exports = {
   ...info,
-  fetch
+  fetch,
+  resolve,
+  publishedAs: 'assessments'
 };
