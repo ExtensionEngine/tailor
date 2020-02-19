af0c5e149b6a9dad0a9d24656d0ee9b288ee2958
Luciano Peranni <lucianoperanni95@gmail.com>
2020-02-19 10:33:22 +0100

    Mailing service connected

--- server/shared/digest/index.js
+++ server/shared/digest/index.js
@@ -2,13 +2,23 @@
 
 const cron = require('node-cron');
 const { User, Revision } = require('../database');
+const { Op } = require('sequelize');
+const mail = require('../mail');
 
-async function initiateDigest() {
-  cron.schedule('*/2 * * * * *', () => {
+function initiateDigest() {
+  cron.schedule('*/2 * * * *', () => {
     const users = User.findAll({ raw: true });
-    users.map(user => {
-      const revisions = Revision.findAll({ where: { user_id: user.id }, raw: true });
-      console.log(revisions);
+    users.map(async user => {
+      const revisions = await Revision.findAll({
+        where: {
+          user_id: user.id,
+          created_at: {
+            [Op.gte]: (new Date().setDate(new Date().getDate() - 7))
+          }
+        },
+        raw: true
+      });
+      mail.sendActivityDigest(user, revisions);
     });
   });
 }
