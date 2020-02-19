3b85f1e46207bccbdaf095e116599a681c0e4220
boskoradalj <bosko@extensionengine.com>
2020-02-19 17:05:21 +0100

    add logic for loading different tag filter list depending on user type

--- server/tag/tag.controller.js
+++ server/tag/tag.controller.js
@@ -1,10 +1,15 @@
 'use strict';
 
 const { Tag } = require('../shared/database');
+const flatten = require('lodash/flatten');
+const uniqBy = require('lodash/uniqBy');
 
-async function list(_, res) {
-  const tags = await Tag.findAll();
-  return res.json({ data: tags });
+async function list({ user }, res) {
+  if (!user.isAdmin()) return Tag.findAll().then(it => res.json({ data: it }));
+  const repositories = await user.getRepositories({ include: [{ model: Tag }] });
+  const tags = flatten(repositories.map(it => it.tags));
+  const filtered = uniqBy(tags, 'id');
+  return res.json({ data: filtered });
 }
 
 module.exports = {
