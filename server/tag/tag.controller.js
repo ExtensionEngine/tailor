112bb649366ded5af9acb9e7ca80c341369928be
boskoradalj <bosko@extensionengine.com>
2020-02-20 07:57:50 +0100

    remove flag for testing purpose

--- server/tag/tag.controller.js
+++ server/tag/tag.controller.js
@@ -5,7 +5,7 @@ const flatten = require('lodash/flatten');
 const uniqBy = require('lodash/uniqBy');
 
 async function list({ user }, res) {
-  if (!user.isAdmin()) return Tag.findAll().then(it => res.json({ data: it }));
+  if (user.isAdmin()) return Tag.findAll().then(it => res.json({ data: it }));
   const repositories = await user.getRepositories({ include: [{ model: Tag }] });
   const tags = flatten(repositories.map(it => it.tags));
   const filtered = uniqBy(tags, 'id');
