b9a0b99bb1720e8b6491bc4e3d0566ad08d6deef
boskoradalj <bosko@extensionengine.com>
2020-02-26 14:37:53 +0100

    replace 'it' with 'tags' to make function params more descriptive

--- server/tag/tag.controller.js
+++ server/tag/tag.controller.js
@@ -5,7 +5,7 @@ const uniqBy = require('lodash/uniqBy');
 
 async function list({ user, query: { name } }, res) {
   if (name) return Tag.count({ where: { name } }).then(count => res.json({ data: count }));
-  if (user.isAdmin()) return Tag.findAll().then(it => res.json({ data: it }));
+  if (user.isAdmin()) return Tag.findAll().then(tags => res.json({ data: tags }));
   const repositories = await user.getRepositories({ include: [{ model: Tag }] });
   const tags = uniqBy(repositories.reduce((acc, it) => acc.concat(it.tags), []), 'id');
   return res.json({ data: tags });
