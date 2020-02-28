8851c5bea582710c4c8d0b8dc6ebb763959e232e
boskoradalj <bosko@extensionengine.com>
2020-02-28 09:36:34 +0100

    replaced count with findAndCountAll

--- server/tag/tag.controller.js
+++ server/tag/tag.controller.js
@@ -4,7 +4,10 @@ const { Tag } = require('../shared/database');
 const uniqBy = require('lodash/uniqBy');
 
 async function list({ user, query: { name } }, res) {
-  if (name) return Tag.count({ where: { name } }).then(count => res.json({ data: count }));
+  if (name) {
+    return Tag.findAndCountAll({ where: { name } })
+    .then(({ count }) => res.json({ data: count }));
+  }
   if (user.isAdmin()) return Tag.findAll().then(tags => res.json({ data: tags }));
   const repositories = await user.getRepositories({ include: [{ model: Tag }] });
   const tags = uniqBy(repositories.reduce((acc, it) => acc.concat(it.tags), []), 'id');
