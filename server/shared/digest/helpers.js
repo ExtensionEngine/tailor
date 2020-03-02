2af525e320074237bfdf89e51dfb275195ed576e
Luciano Peranni <lucianoperanni95@gmail.com>
2020-03-02 15:40:29 +0100

    Exchanged cron package and fixed db query
    swapped node-cron for cron and made sequelize query only include revisions from users older than a week

--- server/shared/digest/helpers.js
+++ server/shared/digest/helpers.js
@@ -36,7 +36,7 @@ const parseInterval = () => {
   };
   const { minute, hour, weekDay } = scheduleOptions;
 
-  const scheduleString = `0 ${minute} ${hour} * * ${weekDay}`;
+  const scheduleString = `*/5 ${minute} ${hour} * * ${weekDay}`;
   try {
     const interval = cronParser.parseExpression(scheduleString);
     logger.info('Next Delivery date: ', interval.next().toString());
--- server/shared/digest/index.js
+++ server/shared/digest/index.js
@@ -1,13 +1,12 @@
 'use strict';
-
-const cron = require('node-cron');
+const CronTask = require('cron').CronJob;
 const { User, Revision, Repository } = require('../database');
 const { sql } = require('../database/helpers');
 const { col, fn, Op } = require('sequelize');
 const { processRevisions, parseInterval, separateUsersAndSend } = require('./helpers');
 
 function initiateDigest() {
-  cron.schedule(parseInterval(), async () => {
+  const digest = new CronTask(parseInterval(), async () => {
     const revisions = await Revision.findAll({
       attributes: [
         [
@@ -16,7 +15,8 @@ function initiateDigest() {
         ],
         [fn('count', sql.concat('operation', 'entity')), 'count'],
         'user_id',
-        'repository_id'
+        'repository_id',
+        'revision.created_at'
       ],
       group: [
         'repository_id',
@@ -24,28 +24,36 @@ function initiateDigest() {
         'user_id',
         'entity_operation',
         'repository.name',
-        'repository.created_at'
+        'repository.created_at',
+        'revision.created_at'
       ],
       include: [
         {
           model: User,
-          attributes: ['email', 'fullName']
+          attributes: ['email', 'fullName', 'created_at'],
+          where: {
+            created_at: {
+              [Op.lte]: new Date().setDate(new Date().getDate() - 7)
+            }
+          }
         },
         {
           model: Repository,
-          attributes: ['name', 'created_at'],
-          where: {
-            created_at: {
-              [Op.gte]: new Date().setDate(new Date().getDate() - 7)
-            }
-          }
+          attributes: ['name', 'created_at']
         }
       ],
+      where: {
+        created_at: {
+          [Op.gte]: new Date().setDate(new Date().getDate() - 7)
+        }
+      },
       raw: true
     });
 
     separateUsersAndSend(processRevisions(revisions));
-  });
+  }, null, true);
+
+  digest.start();
 }
 
 module.exports = {
