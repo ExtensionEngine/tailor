3484b4d581814b619fe74530b1e3fa2203d8e66d
boskoradalj <bosko@extensionengine.com>
2020-03-02 11:26:05 +0100

    new component for tag filter selected list

---
+++ client/components/catalog/TagFilterSelected.vue
@@ -0,0 +1,38 @@
+<template>
+  <v-row class="selected-tags align-center">
+    <v-btn
+      v-if="tagFilter.length"
+      @click="clearTagFilter" small rounded>
+      Clear all
+    </v-btn>
+    <v-chip
+      v-for="tag in tagFilter"
+      :key="tag.id"
+      @click:close="toggleTagFilter(tag)"
+      close
+      class="ma-2">
+      {{ tag.name }}
+    </v-chip>
+  </v-row>
+</template>
+
+<script>
+import { mapMutations, mapState } from 'vuex';
+export default {
+  computed: {
+    ...mapState('repositories', ['tagFilter'])
+  },
+  methods: {
+    ...mapMutations('repositories', ['clearTagFilter', 'toggleTagFilter'])
+  }
+};
+</script>
+
+<style lang="scss" scoped>
+.v-list {
+  max-height: 18.75rem;
+  padding: 1rem;
+  overflow-y: scroll;
+}
+
+</style>
