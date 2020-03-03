f3e9eeda981de779989b8fdc3abe3b646c773e3d
boskoradalj <bosko@extensionengine.com>
2020-03-03 15:54:15 +0100

    add repository tags

--- server/repository/repository.controller.js
+++ server/repository/repository.controller.js
@@ -35,14 +35,18 @@ const includeRepositoryUser = (user, query) => {
   return { model: RepositoryUser, ...options };
 };
 
+const includeRepositoryTags = query => {
+  const includeTag = { model: Tag };
+  if (query.tagIds) includeTag.where = { id: query.tagIds };
+  return includeTag;
+};
+
 function index({ query, user, opts }, res) {
   if (query.search) opts.where.name = { [Op.iLike]: `%${query.search}%` };
   if (query.schema) opts.where.schema = { [Op.eq]: query.schema };
   if (getVal(opts, 'order.0.0') === 'name') opts.order[0][0] = lowercaseName;
-  const includeTag = { model: Tag };
-  if (query.tagIds) includeTag.where = { id: query.tagIds };
-  opts.include.push(includeTag);
-  opts.include = [includeLastRevision(), includeRepositoryUser(user, query)];
+  opts.include = [includeLastRevision(), includeRepositoryUser(user, query),
+    includeRepositoryTags(query)];
   const repositories = user.isAdmin()
     ? Repository.findAll(opts)
     : user.getRepositories(opts);
