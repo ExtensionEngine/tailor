a143cc417532079e63eb52b454c021614343e778
santicevic <sergijeanticevic@gmail.com>
2020-03-04 16:17:59 +0100

    Db query code clean up

--- server/shared/digest/index.js
+++ server/shared/digest/index.js
@@ -5,62 +5,78 @@ const { Sequelize } = require('../database');
 const { Op } = require('sequelize');
 const { subDays } = require('date-fns');
 
-async function initiateDigest() {
-  await User.findAll({
+function getUsersThatLoggedIn() {
+  return User.findAll({
     attributes: ['id', 'email'],
     where: {
       created_at: {
         [Op.ne]: Sequelize.col('user.updated_at')
       }
     }
-  }).then(users =>
-    users.map(user => {
-      const result = [];
-      user
-        .getRepositories({
-          include: [
-            {
-              model: RepositoryUser,
-              attributes: ['created_at'],
-              where: {
-                created_at: {
-                  [Op.lte]: subDays(new Date(), 3, new Date())
-                }
-              }
-            }
-          ]
-        })
-        .map(async repo => {
+  });
+}
+
+function filterRecentlyAddedRepositories(user) {
+  return user.getRepositories({
+    include: [
+      {
+        model: RepositoryUser,
+        attributes: ['created_at'],
+        where: {
+          created_at: {
+            [Op.lte]: subDays(new Date(), 3, new Date())
+          }
+        }
+      }
+    ]
+  });
+}
+
+function getRevisionsSinceDate(date, repo) {
+  return repo.getRevisions({
+    where: {
+      created_at: {
+        [Op.lte]: date
+      }
+    },
+    raw: true
+  });
+}
+
+function getRevisions() {
+  getUsersThatLoggedIn().then(users => {
+    users.forEach(user => {
+      const revisionsToSend = [];
+      filterRecentlyAddedRepositories(user).then(reposNotAddedRecently => {
+        reposNotAddedRecently.forEach(repo => {
           if (
             repo.repositoryUser.dataValues.createdAt <
             subDays(new Date(), 10, new Date())
           ) {
-            result.push(
-              await repo.getRevisions({
-                where: {
-                  created_at: {
-                    [Op.gte]: subDays(new Date(), 7, new Date())
-                  }
-                },
-                raw: true
-              })
-            );
+            // ako je dodan prije vise od 10 dana
+            getRevisionsSinceDate(
+              subDays(new Date(), 7, new Date(), repo)
+            ).then(revisions => {
+              revisionsToSend.push(revisions);
+            });
           } else {
-            result.push(
-              await repo.getRevisions({
-                where: {
-                  created_at: {
-                    [Op.gte]: repo.repositoryUser.dataValues.createdAt
-                  }
-                },
-                raw: true
-              })
-            );
+            // ako je dodan unutar 10 dana
+            getRevisionsSinceDate(
+              repo.repositoryUser.dataValues.createdAt,
+              repo
+            ).then(revisions => {
+              revisionsToSend.push(revisions);
+            });
           }
         });
-      processDigest(user, result);
-    })
-  );
+      });
+      setTimeout(() => console.log(revisionsToSend, user.dataValues.email), 300);
+    });
+  });
+}
+
+function initiateDigest() {
+  getRevisions();
 }
 
 module.exports = {
