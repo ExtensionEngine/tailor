0b34e560c033cbf8c15620217f2c1e98451705ec
Luciano Peranni <lucianoperanni95@gmail.com>
2020-03-05 15:04:31 +0100

    Expanded Scheduler functionality
    Scheduler now accepts json as well as cron strings. Added support for parsing common aliases for months and weekdays. Removed cron-parser package and implemented own parsing

--- server/shared/scheduler/index.js
+++ server/shared/scheduler/index.js
@@ -2,9 +2,8 @@
 
 const { CronTime, CronJob } = require('cron');
 const logger = require('../logger');
-const cronParser = require('cron-parser');
 
-const parseCronString = scheduleParams => {
+const parseJsonCronParams = scheduleParams => {
   const {
     hour,
     minute,
@@ -20,30 +19,51 @@ const parseCronString = scheduleParams => {
   }
   const time = new CronTime(new Date());
 
-  time._parseField(second || '0', 'second', [0, 59]);
-  time._parseField(minute, 'minute', [0, 59]);
-  time._parseField(hour, 'hour', [0, 23]);
-  time._parseField(dayOfMonth || '*', 'dayOfMonth', [1, 31]);
-  time._parseField(month || '*', 'month', [0, 11]);
-  time._parseField(dayOfWeek || '*', 'dayOfWeek', [0, 6]);
+  const parsedDayOfWeek = (dayOfWeek && dayOfWeek.length === 3)
+    ? CronTime.aliases[dayOfWeek].toString()
+    : dayOfWeek;
 
+  const parsedMonth = (month && month.length === 3)
+    ? (CronTime.aliases[month]).toString()
+    : month;
+
+  time._parseField(second || '*', 'second', CronTime.constraints[0]);
+  time._parseField(minute || '*', 'minute', CronTime.constraints[1]);
+  time._parseField(hour || '*', 'hour', CronTime.constraints[2]);
+  time._parseField(dayOfMonth || '*', 'dayOfMonth', CronTime.constraints[3]);
+  time._parseField(parsedMonth || '*', 'month', [1, 12]);
+  time._parseField(parsedDayOfWeek || '*', 'dayOfWeek', CronTime.constraints[5]);
+  return time;
+};
+
+const parseExpression = scheduleString => {
+  const scheduleParams = getScheduleType(scheduleString);
   try {
-    const interval = cronParser.parseExpression(time.toString());
-    logger.info('Next Delivery date: ', interval.next().toString());
-    return time.toString();
+    scheduleParams._verifyParse();
+    return scheduleParams.toString();
   } catch (err) {
-    logger.error('Invalid cron expression!');
+    logger.error('Invalid cron expression! Scheduled Task cancelled');
     throw new Error(err);
   }
 };
 
+const getScheduleType = settings => {
+  try {
+    const jsonString = parseJsonCronParams(settings);
+    return jsonString;
+  } catch (e) {
+    return new CronTime(settings);
+  }
+};
+
 const schedule = (scheduleSettings, task) => {
-  const job = new CronJob(parseCronString(scheduleSettings), () => {
+  const job = new CronJob(parseExpression(scheduleSettings), () => {
     task();
   });
 
   job.start();
   logger.info(`${task.name} has been started`);
+  logger.info(`Next time scheduled: ${job.nextDate()}`);
 };
 
 module.exports = { schedule };
