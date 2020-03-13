0856464be38cf24baab33b956a4119268efa942b
BaskovicP <pbaskovi@gmail.com>
2020-03-13 13:56:25 +0100

    Ammend this

---
+++ tests/unit/client/store/Modules/Repository/revisions/mutations.spec.js
@@ -0,0 +1,10 @@
+import * as mutations from '@/store/modules/repository/revisions/mutations';
+
+describe('Test for store/modules/repository/revisions/mutations', () => {
+  it('set the right pagination when setPagination is called with state and changes', () => {
+    const state = { $internals: { pagination: { 1: 1, 2: 2, 3: 3 } } };
+    const change = { 2: 21, 3: 31, 6: 61 };
+    mutations.setPagination(state, change);
+    expect(state).toEqual({ $internals: { pagination: { 0: 2, 1: 3, 2: 6, 3: 4 } } });
+  });
+});
