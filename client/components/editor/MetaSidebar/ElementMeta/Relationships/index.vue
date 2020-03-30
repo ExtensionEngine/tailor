4fec9b4d30bfcee845846be83bd019bdcab4fdf0
Damir Perisic <underscope@gmail.com>
2020-03-30 08:30:33 +0200

    Extracted relationship type list into separate component

---
+++ client/components/editor/MetaSidebar/ElementMeta/Relationships/index.vue
@@ -0,0 +1,24 @@
+<template>
+  <v-list v-if="relationships.length">
+    <relationship-type
+      v-for="relationship in relationships"
+      :key="`${element._cid}.${relationship.type}`"
+      :element="element"
+      :relationship="relationship" />
+  </v-list>
+</template>
+
+<script>
+import RelationshipType from './Item';
+
+export default {
+  name: 'relationship-types',
+  props: {
+    element: { type: Object, required: true },
+    relationships: { type: Array, default: () => [] }
+  },
+  components: {
+    RelationshipType
+  }
+};
+</script>
