ed8b1621615f25ac6d779e41cd304c8bd7854c79
Damir Perisic <underscope@gmail.com>
2020-04-01 11:41:57 +0200

    Tweaked login view

--- client/components/auth/Container.vue
+++ client/components/auth/Container.vue
@@ -1,11 +1,11 @@
 <template>
   <div class="auth-container">
     <div class="auth-panel elevation-2">
-      <div class="auth-header pt-6">
+      <div class="auth-header py-6">
         <v-avatar size="50" color="blue-grey darken-3" class="my-1 pa-2">
           <img :src="logo" alt="Logo" class="logo">
         </v-avatar>
-        <h1 class="headline primary--text text--darken-1">{{ title }}</h1>
+        <h1 class="ma-0 headline primary--text text--darken-1">{{ title }}</h1>
       </div>
       <div class="auth-body">
         <router-view />
@@ -29,16 +29,11 @@ export default {
 }
 
 .auth-panel {
-  width: 25.625rem;
+  width: 26.5rem;
   margin: auto;
   background-color: #fff;
   border-radius: 4px;
   border-top: 4px solid var(--v-primary-darken1);
-
-  h1 {
-    margin: 0;
-    font-size: 1.25rem;
-  }
 
   .auth-header {
     padding-bottom: 0;
@@ -55,14 +50,6 @@ export default {
       color: inherit;
       font-weight: 400;
     }
-  }
-
-  .message {
-    min-height: 1rem;
-    margin-bottom: 1.25rem;
-    color: var(--v-secondary-base);
-    font-size: 1rem;
-    line-height: 1rem;
   }
 }
 </style>
