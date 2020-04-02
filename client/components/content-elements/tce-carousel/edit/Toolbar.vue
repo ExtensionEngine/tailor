f6688cbe1c557e6abc42a3fadecddeb361b03f79
boskoradalj <bosko@extensionengine.com>
2020-04-02 13:52:20 +0200

    convert toolbar to vuetify and code refactoring

--- client/components/content-elements/tce-carousel/edit/Toolbar.vue
+++ client/components/content-elements/tce-carousel/edit/Toolbar.vue
@@ -1,36 +1,25 @@
 <template>
-  <div class="tce-carousel-toolbar">
-    <ul>
-      <li @click="add" class="btn btn-link btn-sm">
-        <span class="mdi mdi-plus"></span>
-        Add item
-      </li>
-      <li @click="remove" class="btn btn-link btn-sm">
-        <span class="mdi mdi-minus"></span>
-        Remove item
-      </li>
-      <li class="height">
-        <label for="heightInput">
-          <span class="mdi mdi-arrow-expand"></span>
-          Height
-        </label>
-        <span
-          :class="{ 'has-error': vErrors.has('height') }"
-          class="form-group">
-          <input
-            v-model="height"
-            v-validate="{ required: true, min_value: 300, max_value: 3000 }"
-            id="heightInput"
-            class="form-control"
-            name="height"
-            type="text"
-            placeholder="Height">
-        </span>
-        <span v-show="vErrors.has('height')" class="help-block">
-          {{ vErrors.first('height') }}
-        </span>
-      </li>
-    </ul>
+  <div class="d-flex align-center">
+    <v-btn @click="add" icon small>
+      <v-icon small>mdi-plus</v-icon>
+      Add item
+    </v-btn>
+    <v-btn @click="remove" icon small>
+      <v-icon small>mdi-minus</v-icon>
+      Remove item
+    </v-btn>
+    <v-text-field
+      v-model="height"
+      v-validate="'required|min_value:300|max_value:3000'"
+      :error-messages="vErrors.collect('height')"
+      name="height"
+      label="Height"
+      hide-details solo>
+      <v-icon slot="prepend-inner" class="mr-2" small>mdi-arrow-expand</v-icon>
+    </v-text-field>
+    <div v-show="vErrors.has('height')" class="d-flex pl-3">
+      {{ vErrors.first('height') }}
+    </div>
   </div>
 </template>
 
@@ -43,11 +32,7 @@ export default {
   props: {
     element: { type: Object, required: true }
   },
-  data() {
-    return {
-      height: this.element.data.height || 500
-    };
-  },
+  data: () => ({ height: this.element.data.height || 500 }),
   methods: {
     add() {
       this.$elementBus.emit('add');
@@ -67,69 +52,13 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.tce-carousel-toolbar {
-  position: relative;
-  width: 100%;
-  height: 48px;
+.v-btn {
+  width: 6.875rem;
+  margin-right: 1rem;
+}
 
-  ul {
-    float: left;
-    height: 100%;
-    margin: 0;
-    padding: 0 30px 0 10px;
-
-    li {
-      height: 100%;
-      padding-top: 15px;
-      color: #444;
-
-      .form-group {
-        display: inline-block;
-      }
-
-      .help-block {
-        display: inline-block;
-        padding-left: 10px;
-      }
-
-      .mdi {
-        display: inline-block;
-        margin-right: 5px;
-        font-size: 20px;
-        line-height: 20px;
-        vertical-align: middle;
-      }
-
-      &.active {
-        background-color: #e8e8e8;
-      }
-    }
-  }
-
-  .height {
-    display: inline-block;
-    margin: 0 0 0 10px;
-    padding: 0;
-
-    input {
-      height: 25px;
-      margin: 0;
-      padding: 0;
-      font-size: 14px;
-      line-height: 14px;
-    }
-
-    label {
-      padding: 0 10px;
-      font-size: 12px;
-      line-height: 12px;
-      text-transform: uppercase;
-    }
-
-    .form-group {
-      margin: 0;
-      padding: 0;
-    }
-  }
+.v-input {
+  max-width: 7.5rem;
+  margin-left: 0.625rem;
 }
 </style>
