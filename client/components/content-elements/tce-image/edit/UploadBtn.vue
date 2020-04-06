e88fde9ef5d189f0bcb9d1518471bc21cd9cf0cb
Damir Perisic <underscope@gmail.com>
2020-04-06 08:04:24 +0200

    Created basic image upload btn

---
+++ client/components/content-elements/tce-image/edit/UploadBtn.vue
@@ -0,0 +1,29 @@
+<template>
+  <div>
+    <v-btn
+      @click="onClick"
+      color="primary darken-3"
+      text
+      class="text-uppercase">
+      <v-icon color="secondary darken-1" class="mr-2">mdi-image-outline</v-icon>
+      Click to upload an image
+    </v-btn>
+    <input
+      ref="uploadInput"
+      @change="$emit('change', $event)"
+      type="file"
+      accept="image/*"
+      class="d-none">
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'image-upload-btn',
+  methods: {
+    onClick() {
+      this.$refs.uploadInput.click();
+    }
+  }
+};
+</script>
