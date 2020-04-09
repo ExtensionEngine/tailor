e6910f56c57b1f9e088fcf1238f0d4d9e904ba0a
boskoradalj <bosko@extensionengine.com>
2020-04-09 17:14:40 +0200

    rebuild component from scratch, to be more intuitive and consistent based on other content elements (accordion, carousel)

--- client/components/content-elements/tce-modal/edit/index.vue
+++ client/components/content-elements/tce-modal/edit/index.vue
@@ -1,21 +1,45 @@
 <template>
   <div class="tce-modal">
-    <div v-if="isEditing" class="container-fluid">
-      <div v-if="!hasElements" class="well">
-        Click the button below to Add first teaching element to your modal.
+    <v-sheet v-if="hasElements">
+      <v-chip
+        class="ma-2"
+        color="grey darken-1"
+        text-color="white">
+        <v-avatar left>
+          <v-icon>mdi-dock-window</v-icon>
+        </v-avatar>
+        Modal
+      </v-chip>
+      <button
+        @click="showModal = true"
+        class="btn btn-primary btn-open"
+        type="button">
+        {{ title }}
+      </button>
+    </v-sheet>
+    <v-sheet v-else class="pa-12">
+      <v-avatar size="60" color="blue-grey darken-4">
+        <v-icon :size="isFocused ? 38 : 30" color="white">mdi-window-restore</v-icon>
+      </v-avatar>
+      <div class="headline my-4">Modal component</div>
+      <div class="subtitle-1">
+        <template v-if="!isFocused">Select to edit</template>
+        <template v-else>
+          Use toolbar
+          <v-icon size="22" color="secondary">mdi-transfer-up</v-icon>
+          to add modal content
+        </template>
       </div>
+    </v-sheet>
+    <div v-if="isEditing" class="container-fluid">
+      <v-alert v-if="!hasElements" color="grey lighten-4">
+        Click the button below to Add first content to your modal.
+      </v-alert>
       <embedded-container
         @save="$emit('save', $event)"
         @delete="deleteEmbed($event)"
         :container="element.data" />
     </div>
-    <button
-      v-else
-      @click="showModal = true"
-      class="btn btn-primary btn-open"
-      type="button">
-      {{ title }}
-    </button>
     <preview
       v-if="showModal"
       @close="showModal = false"
@@ -26,6 +50,7 @@
 <script>
 import cloneDeep from 'lodash/cloneDeep';
 import { EmbeddedContainer } from 'tce-core';
+import get from 'lodash/get';
 import Preview from './Preview';
 import values from 'lodash/values';
 
@@ -51,6 +76,9 @@ export default {
     },
     hasElements() {
       return this.embeds.length;
+    },
+    isFocused() {
+      return get(this.$attrs, 'is-focused', false);
     }
   },
   methods: {
