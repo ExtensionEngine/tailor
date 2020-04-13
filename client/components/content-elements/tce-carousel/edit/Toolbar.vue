eef2efe6fe690c37413fed69ad7d9432af11b27a
Damir Perisic <underscope@gmail.com>
2020-04-13 15:14:45 +0200

    Migrated carousel toolbar to vuetify

--- client/components/content-elements/tce-carousel/edit/Toolbar.vue
+++ client/components/content-elements/tce-carousel/edit/Toolbar.vue
@@ -1,38 +1,35 @@
 <template>
-  <div class="toolbar-container">
-    <div class="navigation-sidebar">
-    </div>
-    <div class="carousel-action">
+  <v-toolbar height="72">
+    <v-toolbar-title>Carousel toolbar</v-toolbar-title>
+    <v-toolbar-items class="mx-auto">
       <v-btn @click="add" text>
-        <v-icon small>mdi-plus</v-icon>
-        Add slide
+        <v-icon class="pr-2">mdi-plus</v-icon> Add slide
       </v-btn>
       <v-btn @click="remove" text>
-        <v-icon small>mdi-minus</v-icon>
-        Remove slide
+        <v-icon class="pr-2">mdi-minus</v-icon> Remove current slide
       </v-btn>
       <v-text-field
         v-model="height"
-        v-validate="'required|min_value:300|max_value:3000'"
+        v-validate="'required|min_value:200|max_value:3000'"
         :error-messages="vErrors.collect('height')"
         name="height"
-        label="Height"
-        hide-details solo>
-        <v-icon slot="prepend-inner" class="mr-2" small>mdi-arrow-expand</v-icon>
-      </v-text-field>
-      <div v-show="vErrors.has('height')" class="d-flex pl-3">
-        {{ vErrors.first('height') }}
-      </div>
-    </div>
-  </div>
+        label="Height (px)"
+        placeholder="Height..."
+        prepend-icon="mdi-resize"
+        hide-details filled dense
+        class="mt-2 ml-5" />
+    </v-toolbar-items>
+  </v-toolbar>
 </template>
 
 <script>
 import debounce from 'lodash/debounce';
+import { withValidation } from 'utils/validation';
 
 export default {
   name: 'tce-carousel-toolbar',
-  inject: ['$elementBus', '$validator'],
+  inject: ['$elementBus'],
+  mixins: [withValidation()],
   props: {
     element: { type: Object, required: true }
   },
@@ -56,30 +53,8 @@ export default {
 </script>
 
 <style lang="scss" scoped>
-.toolbar-container {
-  display: flex;
-  flex-wrap: wrap;
-
-  .navigation-sidebar {
-    display: flex;
-    width: 25rem;
-  }
-
-  .carousel-action {
-    display: flex;
-    flex-grow: 1;
-    justify-content: center;
-    align-items: center;
-
-    .v-btn {
-      width: 7.5rem;
-      margin-right: 1rem;
-    }
-
-    .v-input {
-      max-width: 7.5rem;
-      margin-left: 0.625rem;
-    }
-  }
+.v-toolbar__title {
+  min-width: 23.875rem;
+  text-align: left;
 }
 </style>
