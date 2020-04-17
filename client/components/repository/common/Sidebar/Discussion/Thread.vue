0f84f45a7f29d57377ff97e74f4ed2cfbbcc4a74
boskoradalj <bosko@extensionengine.com>
2020-04-17 11:44:13 +0200

    # implement infinite loading functionality for comments display

--- client/components/repository/common/Sidebar/Discussion/Thread.vue
+++ client/components/repository/common/Sidebar/Discussion/Thread.vue
@@ -1,15 +1,24 @@
 <template>
   <ul class="discussion-thread mt-2">
     <thread-comment
-      v-for="comment in visibleItems"
+      v-for="comment in comments"
       :key="comment._cid || comment.id"
       @update="onUpdate"
       @remove="item => remove(item)"
       :comment="comment" />
+    <infinite-loading @infinite="infiniteHandler">
+      <div slot="spinner" class="spinner">
+        <v-progress-circular color="primary" indeterminate />
+      </div>
+      <span slot="no-results">No comments!</span>
+      <span slot="no-more">No more comments.</span>
+    </infinite-loading>
   </ul>
 </template>
 
 <script>
+import dropRight from 'lodash/dropRight';
+import InfiniteLoading from 'vue-infinite-loading';
 import { mapActions } from 'vuex';
 import takeRight from 'lodash/takeRight';
 import ThreadComment from './Comment';
@@ -17,27 +26,46 @@ import ThreadComment from './Comment';
 export default {
   name: 'discussion-thread',
   props: {
-    items: { type: Array, required: true },
-    showAll: { type: Boolean, default: false },
-    minDisplayed: { type: Number, default: 5 }
+    items: { type: Array, required: true }
   },
-  computed: {
-    visibleItems() {
-      return this.showAll ? this.items : takeRight(this.items, this.minDisplayed);
-    }
-  },
+  data: () => ({ comments: [], currentIndex: 6, step: 6 }),
   methods: {
     ...mapActions('repository/comments', ['update', 'remove']),
     onUpdate(comment, content) {
       this.update({ ...comment, content, updatedAt: Date.now() });
+    },
+    infiniteHandler($state) {
+      setTimeout(() => {
+        if (this.step < this.items.length) {
+          const temp = dropRight(this.items, this.step);
+          this.comments = this.comments.concat(takeRight(temp, this.currentIndex));
+          this.step += this.currentIndex;
+          $state.loaded();
+        } else {
+          $state.complete();
+        }
+      }, 1000);
     }
   },
-  components: { ThreadComment }
+  watch: {
+    items(val, oldVal) {
+      if (val.length === oldVal.length) return;
+      this.comments = takeRight(this.items, this.step);
+    }
+  },
+  created() {
+    if (this.items.length) {
+      this.comments = takeRight(this.items, this.step);
+    }
+  },
+  components: { InfiniteLoading, ThreadComment }
 };
 </script>
 
 <style lang="scss" scoped>
 .discussion-thread {
+  max-height: 31.25rem;
+  overflow-y: scroll;
   margin: 0;
   padding: 0;
   list-style: none;
