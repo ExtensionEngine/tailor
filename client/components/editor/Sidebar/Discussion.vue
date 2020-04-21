a2c4c62dc0ef60b84d55fe90cdd8e555151c5de9
boskoradalj <bosko@extensionengine.com>
2020-04-21 11:57:10 +0200

    add logic to set last seen comment

--- client/components/editor/Sidebar/Discussion.vue
+++ client/components/editor/Sidebar/Discussion.vue
@@ -9,13 +9,33 @@
 </template>
 
 <script>
+import { mapGetters, mapMutations } from 'vuex';
 import Discussion from '@/components/repository/common/Sidebar/Discussion/Embed';
+import get from 'lodash/get';
 
 export default {
   name: 'editor-discussion',
   props: {
     activity: { type: Object, required: true },
     isVisible: { type: Boolean, default: false }
+  },
+  computed: {
+    ...mapGetters('repository/comments', ['getActivityComments']),
+    comments: vm => vm.getActivityComments(vm.activity.id),
+    lastCommentAt: vm => new Date(get(vm.comments[0], 'createdAt', 0)).getTime()
+  },
+  methods: {
+    ...mapMutations('repository/comments', ['markSeenComments'])
+  },
+  watch: {
+    isVisible(val) {
+      if (!val) return;
+      const latestComment = {
+        activityUid: this.activity.uid,
+        lastCommentAt: this.lastCommentAt
+      };
+      setTimeout(() => this.markSeenComments(latestComment), 1000);
+    }
   },
   components: {
     Discussion
