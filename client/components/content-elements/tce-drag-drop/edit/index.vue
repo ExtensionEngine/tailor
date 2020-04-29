ba99ec2aceba12556bd7a012252a6a86224e055e
Borna Burazer <bborna00@gmail.com>
2020-04-30 01:45:12 +0200

    Cleanup DD

--- client/components/content-elements/tce-drag-drop/edit/index.vue
+++ client/components/content-elements/tce-drag-drop/edit/index.vue
@@ -1,42 +1,56 @@
 <template>
-  <div :class="['drag-drop', { disabled }]">
+  <div>
     <v-row>
-      <v-col
-        v-for="(groupName, groupKey) in groups"
-        :key="groupKey"
-        cols="6">
+      <v-col v-for="(groupName, groupKey) in groups" :key="groupKey" cols="6">
         <v-text-field
           @change="updateGroupName(groupKey, $event)"
           :value="groupName"
-          :error="hasError(`groups${groupKey}`)"
-          placeholder="Group">
+          :disabled="disabled"
+          :placeholder="groupPlaceholder"
+          :error="hasError(`groups${groupKey}`)">
           <template slot="append">
-            <v-btn @click="removeGroup(groupKey)" small icon tile>
-              <v-icon small>mdi-close</v-icon>
+            <v-btn
+              v-if="isEditing && groupCount > 2"
+              @click="removeGroup(groupKey)"
+              small icon>
+              <v-icon small>mdi-delete</v-icon>
             </v-btn>
           </template>
         </v-text-field>
         <v-text-field
-          v-for="(answer, answerKey) in getGroupAnswers(groupKey)"
+          v-for="(answer, answerKey) in getAnswers(groupKey)"
           :key="answerKey"
           @change="updateAnswer(answerKey, $event)"
           :value="answer"
+          :disabled="disabled"
+          :placeholder="answerPlaceholder"
           :error="hasError(`answers${answerKey}`)"
-          placeholder="Answer">
+          hide-details filled class="my-2">
           <template slot="append">
-            <v-btn @click="removeAnswer(groupKey, answerKey)" small icon tile>
+            <v-btn
+              v-if="isEditing && answerCount(groupKey) > 1"
+              @click="removeAnswer(groupKey, answerKey)"
+              small icon>
               <v-icon small>mdi-close</v-icon>
             </v-btn>
           </template>
         </v-text-field>
-        <v-btn @click="addAnswer(groupKey)" :disabled="disabled" small icon>
-          <v-icon small>mdi-plus</v-icon>
-        </v-btn>
+        <div class="text-center">
+          <v-btn
+            v-if="isEditing"
+            @click="addAnswer(groupKey)"
+            small icon class="my-2">
+            <v-icon small>mdi-plus</v-icon>
+          </v-btn>
+        </div>
       </v-col>
     </v-row>
-    <v-btn @click="addGroup" :disabled="disabled">
-      <v-icon small>mdi-plus</v-icon> Add Group
-    </v-btn>
+    <div class="d-flex justify-center">
+      <v-btn v-if="isEditing" @click="addGroup" text class="px-2">
+        <v-icon small>mdi-plus</v-icon>
+        {{ addGroupLabel }}
+      </v-btn>
+    </div>
   </div>
 </template>
 
@@ -47,38 +61,45 @@ import { defaults } from 'utils/assessment';
 import forEach from 'lodash/forEach';
 import pick from 'lodash/pick';
 import pull from 'lodash/pull';
+import size from 'lodash/size';
+
+const GROUP_PLACEHOLDER = 'Group';
+const ANSWER_PLACEHOLDER = 'Answer';
+const ADD_GROUP_LABEL = 'Add group';
 
 export default {
   props: {
     assessment: { type: Object, default: defaults.DD },
-    isEditing: { type: Boolean, default: false },
-    errors: { type: Array, default: () => ([]) }
-  },
-  data() {
-    return {
-      focused: { groupKey: null, answerKey: null }
-    };
+    errors: { type: Array, default: () => ([]) },
+    isEditing: { type: Boolean, default: false }
   },
   computed: {
     disabled: vm => !vm.isEditing,
-    groups: vm => vm.assessment.groups || {},
-    answers: vm => vm.assessment.answers || {},
-    correct: vm => vm.assessment.correct || {}
+    correct: vm => vm.assessment.correct,
+    answers: vm => vm.assessment.answers,
+    groups: vm => vm.assessment.groups,
+    groupCount: vm => size(vm.groups),
+    groupPlaceholder: () => GROUP_PLACEHOLDER,
+    answerPlaceholder: () => ANSWER_PLACEHOLDER,
+    addGroupLabel: () => ADD_GROUP_LABEL
   },
   methods: {
-    getGroupAnswers(groupKey) {
-      const keys = this.correct[groupKey] || [];
-      return pick(this.answers, keys);
-    },
     updateGroupName(groupKey, value) {
       const groups = cloneDeep(this.groups);
       groups[groupKey] = value;
-      this.update({ groups }, true);
+      this.update({ groups });
+    },
+    getAnswers(groupKey) {
+      const keys = this.correct[groupKey];
+      return pick(this.answers, keys);
+    },
+    answerCount(groupKey) {
+      return size(this.correct[groupKey]);
     },
     updateAnswer(answerKey, value) {
       const answers = cloneDeep(this.answers);
       answers[answerKey] = value;
-      this.update({ answers }, true);
+      this.update({ answers });
     },
     addGroup() {
       const groups = cloneDeep(this.groups);
@@ -99,21 +120,6 @@ export default {
       correct[groupKey].push(answerKey);
       this.update({ answers, correct });
     },
-    minGroups(groupKey) {
-      return Object.keys(this.groups).length <= 2;
-    },
-    minAnswers(groupKey) {
-      return this.correct[groupKey].length <= 1;
-    },
-    focus(groupKey, answerKey) {
-      this.focused = this.isFocused(groupKey, answerKey)
-        ? {}
-        : { groupKey, answerKey };
-    },
-    isFocused(groupKey, answerKey) {
-      const state = this.focused;
-      return state.groupKey === groupKey && state.answerKey === answerKey;
-    },
     removeAnswer(groupKey, answerKey) {
       const answers = cloneDeep(this.answers);
       const correct = cloneDeep(this.correct);
@@ -130,22 +136,18 @@ export default {
       delete correct[groupKey];
       this.update({ groups, answers, correct });
     },
-    hasError(key) {
-      return this.errors.includes(key);
-    },
     update(data) {
       this.$emit('update', data, true);
+    },
+    hasError(key) {
+      return this.errors.includes(key);
     }
   }
 };
 </script>
 
 <style lang="scss" scoped>
-.drag-drop ::v-deep input {
+::v-deep input {
   text-align: center;
-}
-
-.disabled {
-  pointer-events: none;
 }
 </style>
