60c8ecdf1c768d7bb3a277efcb68f60efb0583e9
boskoradalj <bosko@extensionengine.com>
2020-05-05 10:23:09 +0200

    add activeUsers api

---
+++ client/api/activeUsers.js
@@ -0,0 +1,26 @@
+import request from './request';
+
+const url = {
+  root: repositoryId => `/repository/${repositoryId}/active-users`
+};
+
+function fetch(repositoryId) {
+  return request.get(url.root(repositoryId)).then(res => res.data.data);
+}
+
+function add(context) {
+  const { repositoryId } = context;
+  return request.post(url.root(repositoryId), { context });
+}
+
+function remove(context) {
+  const { repositoryId } = context;
+  return request.delete(url.root(repositoryId), { data: { context } });
+}
+
+export default {
+  url,
+  fetch,
+  add,
+  remove
+};
