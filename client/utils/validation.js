7580558180e386855b1048377d2497e349d04921
boskoradalj <bosko@extensionengine.com>
2020-05-26 11:45:41 +0200

    import all rules and custom rules definition

--- client/utils/validation.js
+++ client/utils/validation.js
@@ -1,35 +1,30 @@
+import * as rules from 'vee-validate/dist/rules';
+import { extend } from 'vee-validate';
+import { messages } from 'vee-validate/dist/locale/en.json';
 import userApi from '@/api/user';
-import VeeValidate from 'vee-validate';
 
-const alphanumerical = {
-  getMessage: field => {
-    return `The ${field} field must contain at least 1 letter and 1 numeric value.`;
-  },
-  validate: value => {
+Object.keys(rules).forEach(rule => {
+  extend(rule, {
+    ...rules[rule], // copies rule configuration
+    message: messages[rule] // assign message
+  });
+});
+
+extend('alphanumerical', {
+  validate(value) {
     return (/\d/.test(value) && /[a-zA-Z]/.test(value));
+  },
+  message: fieldName => {
+    return `The ${fieldName} field must contain at least 1 letter and 1 numeric value.`;
   }
-};
+});
 
-const uniqueEmail = {
-  getMessage: field => `The ${field} is not unique.`,
+extend('uniqueEmail', {
   validate: (email, user) => {
     if (user && email === user.email) return true;
     return userApi.fetch({ email }).then(({ total }) => ({ valid: !total }));
+  },
+  message: fieldName => {
+    return `The ${fieldName} is not unique.`;
   }
-};
-
-VeeValidate.Validator.extend('alphanumerical', alphanumerical);
-VeeValidate.Validator.extend('unique-email', uniqueEmail);
-
-export default VeeValidate;
-
-const mixin = ({ inherit = false } = {}) => {
-  if (inherit) return { inject: ['$validator'] };
-  return {
-    $_veeValidate: {
-      validator: 'new'
-    }
-  };
-};
-
-export const withValidation = mixin;
+});
