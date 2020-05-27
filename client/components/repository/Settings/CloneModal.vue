6bc7da0d4cc5d4460f33c31b3fdda0115a02b2d7
boskoradalj <bosko@extensionengine.com>
2020-05-27 17:16:16 +0200

    replaced old validation with vee-validate 3.3.0

--- client/components/repository/Settings/CloneModal.vue
+++ client/components/repository/Settings/CloneModal.vue
@@ -2,26 +2,36 @@
   <tailor-dialog :value="show" header-icon="mdi-content-copy" persistent>
     <template v-slot:header>Clone {{ schema.toLowerCase() }}</template>
     <template v-slot:body>
-      <v-text-field
-        v-model="name"
-        v-validate="{ required: true, min: 2, max: 250 }"
-        :disabled="inProgress"
-        :error-messages="vErrors.collect('name')"
-        label="Name"
-        placeholder="Enter name..."
-        data-vv-name="name"
-        outlined
-        class="mb-4" />
-      <v-textarea
-        v-model="description"
-        v-validate="{ required: true, min: 2, max: 2000 }"
-        :disabled="inProgress"
-        :error-messages="vErrors.collect('description')"
-        label="Description"
-        placeholder="Enter description..."
-        data-vv-name="description"
-        outlined
-        class="mb-4" />
+      <validation-observer ref="form">
+        <validation-provider
+          v-slot="{ errors }"
+          name="name"
+          rules="required|min:2|max:250">
+          <v-text-field
+            v-model="name"
+            :error-messages="errors"
+            :disabled="inProgress"
+            label="Name"
+            placeholder="Enter name..."
+            data-vv-name="name"
+            outlined
+            class="mb-4" />
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          name="description"
+          rules="required|min:2|max:2000">
+          <v-textarea
+            v-model="description"
+            :error-messages="errors"
+            :disabled="inProgress"
+            label="Description"
+            placeholder="Enter description..."
+            data-vv-name="description"
+            outlined
+            class="mb-4" />
+        </validation-provider>
+      </validation-observer>
     </template>
     <template v-slot:actions>
       <v-btn @click="close" :disabled="inProgress" text>Cancel</v-btn>
@@ -40,7 +50,6 @@
 import { mapActions, mapGetters } from 'vuex';
 import pick from 'lodash/pick';
 import TailorDialog from '@/components/common/TailorDialog';
-import { withValidation } from 'utils/validation';
 
 const getDefaultState = () => ({
   inProgress: false,
@@ -49,7 +58,6 @@ const getDefaultState = () => ({
 });
 
 export default {
-  mixins: [withValidation()],
   props: {
     show: { type: Boolean, default: true }
   },
@@ -60,10 +68,10 @@ export default {
     close() {
       this.$emit('close');
       Object.assign(this, getDefaultState());
-      this.$validator.reset();
+      this.$refs.form.reset();
     },
     async cloneRepository() {
-      const isValid = await this.$validator.validateAll();
+      const isValid = await this.$refs.form.validate();
       if (!isValid) return;
       this.inProgress = true;
       const { repositoryId } = this.$route.params;
