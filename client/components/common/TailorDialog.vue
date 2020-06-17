af15246fc2d8b725fc0140a8b021b83ddce95472
boskoradalj <bosko@extensionengine.com>
2020-06-17 14:29:11 +0200

    # define validation observer wrapper on TailorDialog with 'invalid' property

--- client/components/common/TailorDialog.vue
+++ client/components/common/TailorDialog.vue
@@ -6,29 +6,31 @@
     <template v-for="(_, slot) in $scopedSlots" v-slot:[slot]="scope">
       <slot :name="slot" v-bind="scope"></slot>
     </template>
-    <v-form @submit.prevent>
-      <v-card>
-        <v-card-title primary-title class="dialog-title primary darken-1">
-          <v-avatar
-            v-if="headerIcon"
-            color="secondary"
-            size="38"
-            class="mr-3">
-            <v-icon dark>{{ headerIcon }}</v-icon>
-          </v-avatar>
-          <div class="text-truncate">
-            <slot name="header"></slot>
-          </div>
-        </v-card-title>
-        <v-card-text class="pt-7">
-          <slot name="body"></slot>
-        </v-card-text>
-        <v-card-actions class="px-4 pb-3">
-          <v-spacer />
-          <slot name="actions"></slot>
-        </v-card-actions>
-      </v-card>
-    </v-form>
+    <validation-observer v-slot="{ invalid }">
+      <v-form @submit.prevent>
+        <v-card>
+          <v-card-title primary-title class="dialog-title primary darken-1">
+            <v-avatar
+              v-if="headerIcon"
+              color="secondary"
+              size="38"
+              class="mr-3">
+              <v-icon dark>{{ headerIcon }}</v-icon>
+            </v-avatar>
+            <div class="text-truncate">
+              <slot name="header"></slot>
+            </div>
+          </v-card-title>
+          <v-card-text class="pt-7">
+            <slot name="body"></slot>
+          </v-card-text>
+          <v-card-actions class="px-4 pb-3">
+            <v-spacer />
+            <slot v-bind="{ invalid }" name="actions"></slot>
+          </v-card-actions>
+        </v-card>
+      </v-form>
+    </validation-observer>
   </v-dialog>
 </template>
 
