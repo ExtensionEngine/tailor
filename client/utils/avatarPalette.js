d2a68a63c703a71362133595de6837f657603e05
boskoradalj <bosko@extensionengine.com>
2020-07-01 10:50:24 +0200

    remove unused palette properties

--- client/utils/avatarPalette.js
+++ client/utils/avatarPalette.js
@@ -1,8 +1,8 @@
 export default [
-  { id: 0, background: '#86c3b5', text: '#fff', border: '#EC407A' },
-  { id: 1, background: '#86c3b5', text: '#fff', border: '#03A9F4' },
-  { id: 2, background: '#beb48d', text: '#fff', border: '#E53935' },
-  { id: 3, background: '#b78661', text: '#fff', border: '#8E24AA' },
-  { id: 4, background: '#91af4c', text: '#fff', border: '#2196F3' },
-  { id: 5, background: '#d8e63d', text: '#fff', border: '#009688' }
+  { id: 0, border: '#EC407A' },
+  { id: 1, border: '#03A9F4' },
+  { id: 2, border: '#E53935' },
+  { id: 3, border: '#8E24AA' },
+  { id: 4, border: '#2196F3' },
+  { id: 5, border: '#009688' }
 ];
