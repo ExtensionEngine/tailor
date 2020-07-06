d8892bbd7d0fc8b63e6ff2a19317ad9d604f53b3
boskoradalj <bosko@extensionengine.com>
2020-07-06 12:58:15 +0200

    remove validation-observer (use tailor-dialog wrapper)

--- client/components/catalog/Create.vue
+++ client/components/catalog/Create.vue
@@ -14,61 +14,60 @@
     </template>
     <template v-slot:header>New</template>
     <template v-slot:body>
-      <validation-observer ref="form" slim>
-        <validation-provider name="alert">
-          <v-alert
-            :value="error.show"
-            color="error"
-            icon="mdi-alert-outline"
-            outlined>
-            {{ error.message }}
-          </v-alert>
-        </validation-provider>
-        <validation-provider
-          v-slot="{ errors }"
-          rules="required"
-          name="schema">
-          <v-select
-            v-model="repository.schema"
-            :items="schemas"
-            :error-messages="errors"
-            item-value="id"
-            item-text="name"
-            data-vv-name="schema"
-            outlined
-            class="mb-3" />
-        </validation-provider>
-        <validation-provider
-          v-slot="{ errors }"
-          rules="required|min:2|max:250"
-          name="repositoryName">
-          <v-text-field
-            v-model.trim="repository.name"
-            :error-messages="errors"
-            label="Name"
-            placeholder="Enter name..."
-            data-vv-name="name"
-            outlined />
-        </validation-provider>
-        <validation-provider
-          v-slot="{ errors }"
-          rules="required|min:2|max:2000"
-          name="description">
-          <v-textarea
-            v-model.trim="repository.description"
-            :error-messages="errors"
-            label="Description"
-            placeholder="Enter description..."
-            data-vv-name="description"
-            outlined />
-        </validation-provider>
-      </validation-observer>
+      <validation-provider name="alert">
+        <v-alert
+          :value="error.show"
+          color="error"
+          icon="mdi-alert-outline"
+          outlined>
+          {{ error.message }}
+        </v-alert>
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required"
+        name="schema">
+        <v-select
+          v-model="repository.schema"
+          :items="schemas"
+          :error-messages="errors"
+          item-value="id"
+          item-text="name"
+          data-vv-name="schema"
+          outlined
+          class="mb-3" />
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required|min:2|max:250"
+        name="repositoryName">
+        <v-text-field
+          v-model.trim="repository.name"
+          :error-messages="errors"
+          label="Name"
+          placeholder="Enter name..."
+          data-vv-name="name"
+          outlined />
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required|min:2|max:2000"
+        name="description">
+        <v-textarea
+          v-model.trim="repository.description"
+          :error-messages="errors"
+          label="Description"
+          placeholder="Enter description..."
+          data-vv-name="description"
+          outlined />
+      </validation-provider>
     </template>
-    <template v-slot:actions>
+    <template #actions="{ invalid }">
       <v-btn @click="hide" :disabled="showLoader" text>Cancel</v-btn>
       <v-btn
         @click="submit"
         :loading="showLoader"
+        :disabled="invalid"
         color="blue-grey darken-4"
         text>
         Create
@@ -106,8 +105,6 @@ export default {
   },
   methods: {
     async submit() {
-      const valid = await this.$refs.form.validate();
-      if (!valid) return;
       this.showLoader = true;
       return api.save(this.repository)
         .then(() => this.$emit('created') && this.hide())
