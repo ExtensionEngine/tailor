d6ecba9d9033d29f41b9381b2fa63d7767f370f8
boskoradalj <bosko@extensionengine.com>
2020-07-06 13:17:32 +0200

    remove validation observer from tailor-dialog

--- client/components/common/TailorDialog.vue
+++ client/components/common/TailorDialog.vue
@@ -6,32 +6,30 @@
     <template v-for="(_, slot) in $scopedSlots" v-slot:[slot]="scope">
       <slot :name="slot" v-bind="scope"></slot>
     </template>
-    <validation-observer v-slot="props" slim>
-      <v-form @submit.prevent>
-        <v-card>
-          <v-card-title primary-title class="dialog-title primary darken-1">
-            <v-avatar
-              v-if="headerIcon"
-              color="secondary"
-              size="38"
-              class="mr-3">
-              <v-icon dark>{{ headerIcon }}</v-icon>
-            </v-avatar>
-            <div class="text-truncate">
-              <slot name="header"></slot>
-            </div>
-          </v-card-title>
-          <v-card-text class="pt-7">
-            <slot name="body"></slot>
-          </v-card-text>
-          <v-card-actions class="px-4 pb-3">
-            <v-spacer />
-            <slot v-bind="props" name="actions"></slot>
-          </v-card-actions>
-        </v-card>
-      </v-form>
-    </validation-observer>
-  </v-dialog>
+    <v-form @submit.prevent>
+      <v-card>
+        <v-card-title primary-title class="dialog-title primary darken-1">
+          <v-avatar
+            v-if="headerIcon"
+            color="secondary"
+            size="38"
+            class="mr-3">
+            <v-icon dark>{{ headerIcon }}</v-icon>
+          </v-avatar>
+          <div class="text-truncate">
+            <slot name="header"></slot>
+          </div>
+        </v-card-title>
+        <v-card-text class="pt-7">
+          <slot name="body"></slot>
+        </v-card-text>
+        <v-card-actions class="px-4 pb-3">
+          <v-spacer />
+          <slot v-bind="props" name="actions"></slot>
+        </v-card-actions>
+      </v-card>
+    </v-form>
+</v-dialog>
 </template>
 
 <script>
