1d94ec6fc44d97d729ee6594d58c4a2eccd19369
boskoradalj <bosko@extensionengine.com>
2020-07-06 14:56:54 +0200

    add handleSubmit on validation-observer with ref prop used to reset form

--- client/components/catalog/Create.vue
+++ client/components/catalog/Create.vue
@@ -1,79 +1,84 @@
 <template>
-  <tailor-dialog
-    v-if="isAdmin"
-    v-model="isVisible"
-    header-icon="mdi-folder-plus-outline">
-    <template v-slot:activator="{ on }">
-      <v-btn
-        v-on="on"
-        color="secondary"
-        fab dark absolute
-        class="add-repo">
-        <v-icon>mdi-plus</v-icon>
-      </v-btn>
-    </template>
-    <template v-slot:header>New</template>
-    <template v-slot:body>
-      <validation-provider name="alert">
-        <v-alert
-          :value="error.show"
-          color="error"
-          icon="mdi-alert-outline"
-          outlined>
-          {{ error.message }}
-        </v-alert>
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        rules="required"
-        name="schema">
-        <v-select
-          v-model="repository.schema"
-          :items="schemas"
-          :error-messages="errors"
-          item-value="id"
-          item-text="name"
-          data-vv-name="schema"
-          outlined
-          class="mb-3" />
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        rules="required|min:2|max:250"
-        name="repositoryName">
-        <v-text-field
-          v-model.trim="repository.name"
-          :error-messages="errors"
-          label="Name"
-          placeholder="Enter name..."
-          data-vv-name="name"
-          outlined />
-      </validation-provider>
-      <validation-provider
-        v-slot="{ errors }"
-        rules="required|min:2|max:2000"
-        name="description">
-        <v-textarea
-          v-model.trim="repository.description"
-          :error-messages="errors"
-          label="Description"
-          placeholder="Enter description..."
-          data-vv-name="description"
-          outlined />
-      </validation-provider>
-    </template>
-    <template #actions="{ invalid }">
-      <v-btn @click="hide" :disabled="showLoader" text>Cancel</v-btn>
-      <v-btn
-        @click="submit"
-        :loading="showLoader"
-        :disabled="invalid"
-        color="blue-grey darken-4"
-        text>
-        Create
-      </v-btn>
-    </template>
-  </tailor-dialog>
+  <validation-observer v-slot="{ handleSubmit }" ref="form" slim>
+    <tailor-dialog
+      v-if="isAdmin"
+      v-model="isVisible"
+      header-icon="mdi-folder-plus-outline">
+      <template v-slot:activator="{ on }">
+        <v-btn
+          v-on="on"
+          color="secondary"
+          fab dark absolute
+          class="add-repo">
+          <v-icon>mdi-plus</v-icon>
+        </v-btn>
+      </template>
+      <template v-slot:header>New</template>
+      <template v-slot:body>
+        <validation-provider name="alert">
+          <v-alert
+            :value="error.show"
+            mode="eager"
+            color="error"
+            icon="mdi-alert-outline"
+            outlined>
+            {{ error.message }}
+          </v-alert>
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          mode="eager"
+          rules="required"
+          name="schema">
+          <v-select
+            v-model="repository.schema"
+            :items="schemas"
+            :error-messages="errors"
+            item-value="id"
+            item-text="name"
+            data-vv-name="schema"
+            outlined
+            class="mb-3" />
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          mode="eager"
+          rules="required|min:2|max:250"
+          name="repositoryName">
+          <v-text-field
+            v-model.trim="repository.name"
+            :error-messages="errors"
+            label="Name"
+            placeholder="Enter name..."
+            data-vv-name="name"
+            outlined />
+        </validation-provider>
+        <validation-provider
+          v-slot="{ errors }"
+          mode="eager"
+          rules="required|min:2|max:2000"
+          name="description">
+          <v-textarea
+            v-model.trim="repository.description"
+            :error-messages="errors"
+            label="Description"
+            placeholder="Enter description..."
+            data-vv-name="description"
+            outlined />
+        </validation-provider>
+      </template>
+      <template #actions>
+        <v-btn @click="hide" :disabled="showLoader" text>Cancel</v-btn>
+        <v-btn
+          @click.prevent="handleSubmit(submit)"
+          :loading="showLoader"
+          color="blue-grey darken-4"
+          text>
+          Create
+        </v-btn>
+      </template>
+    </tailor-dialog>
+  </validation-observer>
 </template>
 
 <script>
