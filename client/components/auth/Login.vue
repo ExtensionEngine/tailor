b1fb23c8e6a03d2ab837b65718d9008449e292c6
Damir Perisic <underscope@gmail.com>
2020-07-22 11:03:41 +0200

    Login form cleanup
    - Removed extra nesting level
    - Removed eager mode (default)

--- client/components/auth/Login.vue
+++ client/components/auth/Login.vue
@@ -8,53 +8,50 @@
       class="mb-7 text-left">
       {{ message }}
     </v-alert>
-    <validation-observer v-slot="{ handleSubmit }" slim>
-      <form @submit.prevent="handleSubmit(submit)">
-        <validation-provider
-          v-slot="{ errors }"
-          mode="eager"
-          rules="required|email"
-          name="email">
-          <v-text-field
-            v-model="email"
-            :error-messages="errors"
-            type="email"
-            name="email"
-            label="Email"
-            placeholder="Email"
-            autocomplete="username"
-            prepend-inner-icon="mdi-email-outline"
-            outlined
-            class="mb-1" />
-        </validation-provider>
-        <validation-provider
-          v-slot="{ errors }"
-          mode="eager"
-          rules="required"
-          name="password">
-          <v-text-field
-            v-model="password"
-            :error-messages="errors"
-            type="password"
-            name="password"
-            label="Password"
-            placeholder="Password"
-            prepend-inner-icon="mdi-lock-outline"
-            autocomplete="current-password"
-            outlined />
-        </validation-provider>
-        <div class="d-flex">
-          <v-spacer />
-          <v-btn type="submit" color="primary darken-1">
-            Log in
-          </v-btn>
-        </div>
-        <div class="options">
-          <router-link :to="{ name: 'forgot-password' }">
-            Forgot password?
-          </router-link>
-        </div>
-      </form>
+    <validation-observer
+      ref="form"
+      @submit.prevent="$refs.form.handleSubmit(submit)"
+      tag="form">
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required|email"
+        name="email">
+        <v-text-field
+          v-model="email"
+          :error-messages="errors"
+          type="email"
+          name="email"
+          label="Email"
+          placeholder="Email"
+          autocomplete="username"
+          prepend-inner-icon="mdi-email-outline"
+          outlined
+          class="mb-1" />
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required"
+        name="password">
+        <v-text-field
+          v-model="password"
+          :error-messages="errors"
+          type="password"
+          name="password"
+          label="Password"
+          placeholder="Password"
+          prepend-inner-icon="mdi-lock-outline"
+          autocomplete="current-password"
+          outlined />
+      </validation-provider>
+      <div class="d-flex">
+        <v-spacer />
+        <v-btn type="submit" color="primary darken-1">Log in</v-btn>
+      </div>
+      <div class="options">
+        <router-link :to="{ name: 'forgot-password' }">
+          Forgot password?
+        </router-link>
+      </div>
     </validation-observer>
   </div>
 </template>
