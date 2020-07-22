94ae8356bd9d817b7a440dc62cbe2424717c646c
Damir Perisic <underscope@gmail.com>
2020-07-22 11:47:51 +0200

    Tweaked ResetPassword component
    - Flatter markup by using tag on validation observer
    - Removed default validation mode
    - Fixed confirmation
    - Fixed error msg labels

--- client/components/auth/ResetPassword.vue
+++ client/components/auth/ResetPassword.vue
@@ -7,49 +7,50 @@
       class="mb-5">
       {{ error }}
     </v-alert>
-    <validation-observer v-slot="{ invalid, handleSubmit }" slim>
-      <form @submit.prevent="handleSubmit(submit)">
-        <validation-provider
-          v-slot="{ errors }"
-          mode="eager"
-          rules="required|alphanumerical|min:6"
-          name="password">
-          <v-text-field
-            ref="password"
-            v-model="password"
-            :error-messages="errors"
-            prepend-inner-icon="mdi-lock"
-            type="password"
-            name="password"
-            label="Password"
-            placeholder="Password"
-            outlined
-            class="mb-1" />
-        </validation-provider>
-        <validation-provider
-          v-slot="{ errors }"
-          mode="eager"
-          rules="required|confirmed:password"
-          name="passwordConfirmation">
-          <v-text-field
-            v-model="passwordConfirmation"
-            :error-messages="errors"
-            type="password"
-            name="passwordConfirmation"
-            label="Re-enter password"
-            placeholder="Password confirmation"
-            data-vv-as="password"
-            prepend-inner-icon="mdi-lock-outline"
-            outlined />
-        </validation-provider>
-        <v-btn
-          :disabled="invalid"
-          type="submit"
-          color="primary darken-1"
-          class="my-1">
-          Change password
-        </v-btn>
-      </form>
+    <validation-observer
+      v-slot="{ invalid }"
+      ref="form"
+      @submit.prevent="$refs.form.handleSubmit(submit)"
+      tag="form">
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required|alphanumerical|min:6"
+        vid="password"
+        name="password">
+        <v-text-field
+          ref="password"
+          v-model="password"
+          :error-messages="errors"
+          type="password"
+          name="password"
+          label="Password"
+          placeholder="Password"
+          prepend-inner-icon="mdi-lock"
+          outlined
+          class="mb-1" />
+      </validation-provider>
+      <validation-provider
+        v-slot="{ errors }"
+        rules="required|confirmed:password"
+        vid="passwordConfirmation"
+        name="password">
+        <v-text-field
+          v-model="passwordConfirmation"
+          :error-messages="errors"
+          type="password"
+          name="passwordConfirmation"
+          label="Re-enter password"
+          placeholder="Password confirmation"
+          prepend-inner-icon="mdi-lock-outline"
+          outlined />
+      </validation-provider>
+      <v-btn
+        :disabled="invalid"
+        type="submit"
+        color="primary darken-1"
+        class="my-1">
+        Change password
+      </v-btn>
     </validation-observer>
   </div>
 </template>
@@ -65,7 +66,7 @@ export default {
   }),
   methods: {
     ...mapActions(['resetPassword']),
-    async submit() {
+    submit() {
       const { token } = this.$route.params;
       return this.resetPassword({ password: this.password, token })
         .then(() => this.$router.push('/'))
