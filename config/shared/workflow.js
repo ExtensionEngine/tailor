191b99e20cba33421379c3011ff8cb3c669027d6
Miro Dojkic <mdojkic@gmail.com>
2020-08-07 18:07:50 +0200

    Remove unused task’s meta config 🔥

--- config/shared/workflow.js
+++ config/shared/workflow.js
@@ -12,47 +12,10 @@ const priorities = [
   { id: 'TRIVIAL', label: 'Trivial', icon: 'priorityTrivial' }
 ];
 
-const priorityOptions = priorities.map(it => ({ value: it.id, label: it.label }));
-
-const meta = {
-  ASSIGNEE: {
-    key: 'assigneeId',
-    type: 'SELECT',
-    label: 'Assignee',
-    options: [],
-    placeholder: 'Click to assign...',
-    validate: { required: false }
-  },
-  STATUS: {
-    key: 'statusId',
-    type: 'SELECT',
-    label: 'Status',
-    options: [],
-    placeholder: 'Click to set status...',
-    validate: { required: false }
-  },
-  PRIORITY: {
-    key: 'priorityId',
-    type: 'SELECT',
-    label: 'Priority',
-    options: priorityOptions,
-    placeholder: 'Click to set priority...',
-    validate: { required: false },
-    defaultValue: priorityOptions[2]
-  },
-  DUE_DATE: {
-    key: 'dueDate',
-    type: 'DATE',
-    label: 'Due date',
-    placeholder: 'Click to set due date...',
-    validate: { required: false }
-  }
-};
-
 function getWorkflow(id) {
   return find(WORKFLOWS, { id });
 }
 
 validateWorkflow(WORKFLOWS);
 
-module.exports = { meta, priorities, getWorkflow, WORKFLOWS };
+module.exports = { priorities, getWorkflow };
