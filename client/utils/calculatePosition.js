1b09cc88d8384a37374e2b62a094b1817199d256
Hrvoje <hrvoje.vucic@hotmail.com>
2020-08-17 12:27:04 +0200

    Update calculatePosition to handle activity prepend

--- client/utils/calculatePosition.js
+++ client/utils/calculatePosition.js
@@ -1,11 +1,21 @@
-export default function ({ newPosition, items, isFirstChild, insert = false }) {
+export default function ({
+  newPosition,
+  items,
+  isFirstChild,
+  insert = false,
+  prepend = false
+}) {
   const next = items[newPosition + 1];
+  const current = items[newPosition];
   const count = items.length;
   let position, first, prev;
 
   if (insert) {
     first = items[0];
     prev = items[newPosition];
+  } else if (prepend) {
+    first = items[0];
+    prev = items[newPosition - 1];
   } else {
     first = items[1];
     prev = items[newPosition - 1];
@@ -13,6 +23,8 @@ export default function ({ newPosition, items, isFirstChild, insert = false }) {
 
   if (isFirstChild) {
     position = first ? first.position * 0.5 : 1;
+  } else if (prepend) {
+    position = ((prev ? prev.position : 0) + current.position) * 0.5;
   } else if (newPosition + 1 === count) {
     position = prev.position + 1;
   } else {
