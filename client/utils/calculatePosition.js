923e07b11dd940b89157d3baa9b8a457e2e9b9c1
Hrvoje <hrvoje.vucic@hotmail.com>
2020-10-01 11:47:23 +0200

    Cleanup js-doc indentation and typos

--- client/utils/calculatePosition.js
+++ client/utils/calculatePosition.js
@@ -5,14 +5,15 @@ const { ADD_AFTER, ADD_BEFORE, REORDER } = InsertLocation;
 /**
  * Calculates item position based on the options provided.
  * @param {number} newPosition The position of the anchor item by which the new
- * position will be calculated.
+ *     position will be calculated.
  * @param {Object[]} items An array of objects in which we are determining
- * item's new position.
- * @param {boolean} isFirstChild Boolean value denoting wether the item should
- * be placed as the first child of its parent.
- * @param {string} [action=REORDER] A string value determining where the item should
- * be placed in relation to `newPosition`. Defaults to `REORDER` in case of items reorder.
- * @returns {number} Calculated item position.
+ *     item's new position.
+ * @param {boolean} isFirstChild Boolean value denoting whether the item should
+ *     be placed as the first child of its parent.
+ * @param {string} [action=REORDER] A string value determining where the item
+ *     should be placed in relation to `newPosition`. Defaults to `REORDER` in
+ *     case of items reorder.
+ * @return {number} Calculated item position.
  */
 export default function ({ newPosition, items, isFirstChild, action = REORDER }) {
   let prev = items[newPosition - 1];
