e881e3036658b49f371b15b603f0cf73eb45de3e
Damir Perisic <underscope@gmail.com>
2020-10-13 11:34:54 +0200

    :lipstick:

--- client/components/editor/structure/ContentContainers/Container.vue
+++ client/components/editor/structure/ContentContainers/Container.vue
@@ -27,7 +27,7 @@
       class="element-list">
       <template v-slot:list-item="{ element, isDragged, position }">
         <inline-activator @click.native="showElementDrawer(position - 1)" />
-        <content-element v-bind="{ isDragged, element, setWidth: false }" />
+        <content-element v-bind="{ element, isDragged, setWidth: false }" />
       </template>
       <template v-slot:list-add="{ position: lastPosition, ...slotProps }">
         <div class="add-element-container mt-5">
