4b19b9d724d77af1dd704a93a74397fd90789fa0
Marin Capeta <marin.capeta@gmail.com>
2020-10-18 19:50:06 +0200

    Fix func invocation
    Pass arguments to decorated function

--- client/components/common/loader.js
+++ client/components/common/loader.js
@@ -3,7 +3,7 @@ import pMinDelay from 'p-min-delay';
 export default function loader(action, name, minDuration = 0) {
   return function () {
     this[name] = true;
-    return pMinDelay(Promise.resolve(action.call(this)), minDuration)
+    return pMinDelay(Promise.resolve(action.call(this, ...arguments)), minDuration)
       .finally(() => (this[name] = false));
   };
 }
