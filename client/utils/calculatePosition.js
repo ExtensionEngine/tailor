305677d068ca33e05b8933488c6dfba86ea63164
Marin Capeta <marin.capeta@gmail.com>
2020-11-02 23:40:18 +0100

    Refactor `calculatePosition` util
    - enable calculating multiple positions
    - expose additional helper

--- client/utils/calculatePosition.js
+++ client/utils/calculatePosition.js
@@ -1,43 +1,45 @@
 import InsertLocation from './InsertLocation';
 
-const { ADD_AFTER, ADD_BEFORE, REORDER } = InsertLocation;
+const { ADD_BEFORE, REORDER } = InsertLocation;
+
+const distributePositions = ({ lower = 0, upper }, count) => {
+  const delta = upper ? (upper - lower) / (count + 1) : 1;
+  return Array.from({ length: count }).map((_, i) => delta * (i + 1) + lower);
+};
+
+function getDeprecationWarning(config) {
+  if (!Object.prototype.hasOwnProperty.call(config, 'isFirstChild')) return;
+  console.warn(`Deprecation notice:
+    'isFirstChild' option is deprecated and no longer used!
+    Providing it does not affect this function.`);
+}
+
+export const getPositions = (items, index, count = 1) => {
+  const { position: lower } = items[index - 1] || {};
+  const { position: upper } = items[index] || {};
+  return distributePositions({ lower, upper }, count);
+};
 
 /**
- * Calculates item position based on the options provided.
- * @param {number} newPosition The position of the anchor item by which the new
- *     position will be calculated.
+ * Calculates item position(s) based on the options provided.
+ * @param {number} newPosition The index of the anchor item by which the new
+ *     positions will be calculated.
  * @param {Object[]} items An array of objects in which we are determining
  *     item's new position.
- * @param {boolean} isFirstChild Boolean value denoting whether the item should
- *     be placed as the first child of its parent.
+ * @param {boolean} isFirstChild Deprecated: Boolean value denoting whether
+ *     the item should be placed as the first child of its parent.
  * @param {string} [action=REORDER] A string value determining where the item
  *     should be placed in relation to `newPosition`. Defaults to `REORDER` in
  *     case of items reorder.
- * @return {number} Calculated item position.
+ * @param {number} count A number of positions to return.
+ * @return {(number|Array)} Single position if `count` is 1 or an array containing
+ * `count` positions.
  */
-export default function ({ newPosition, items, isFirstChild, action = REORDER }) {
-  let prev = items[newPosition - 1];
-
-  if (action === ADD_BEFORE) {
-    const current = items[newPosition];
-    return ((prev ? prev.position : 0) + current.position) * 0.5;
-  }
-
-  const next = items[newPosition + 1];
-  const isLastChild = newPosition + 1 === items.length;
-  let first = items[0];
-
-  if (action === ADD_AFTER) {
-    prev = items[newPosition];
-  } else if (action === REORDER) {
-    first = items[1];
-  }
-
-  if (isFirstChild) {
-    return first ? first.position * 0.5 : 1;
-  } else if (isLastChild) {
-    return prev.position + 1;
-  }
-
-  return (prev.position + next.position) * 0.5;
+export default function ({ newPosition, items, action = REORDER, count = 1 }) {
+  getDeprecationWarning(...arguments);
+  const arr = [...items];
+  if (action === REORDER) arr.splice(newPosition, count);
+  const index = action === ADD_BEFORE ? newPosition - 1 : newPosition;
+  const positions = getPositions(arr, index, count);
+  return count === 1 ? positions[0] : positions;
 }
