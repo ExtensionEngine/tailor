aafc430057d708120a12a809e34978395f77f9ed
Damir Perisic <underscope@gmail.com>
2020-11-11 09:59:31 +0100

    Tweaked auth container UI

--- client/components/auth/Container.vue
+++ client/components/auth/Container.vue
@@ -1,25 +1,31 @@
 <template>
   <div class="auth-container">
-    <div class="auth-panel elevation-2">
+    <v-sheet elevation="0" rounded class="auth-panel">
       <div class="auth-header py-8">
-        <v-avatar size="50" color="blue-grey darken-3" class="my-1 pa-2">
+        <v-avatar size="80" color="blue-grey darken-4" class="pa-2">
           <img :src="logo" alt="Logo" class="logo">
         </v-avatar>
-        <h1 class="ma-0 headline primary--text text--darken-1">{{ title }}</h1>
+        <h1 class="ma-2 headline">{{ title }}</h1>
       </div>
       <div class="auth-body">
         <router-view />
       </div>
+    </v-sheet>
+    <div class="waves-container">
+      <waves height="15rem" color="31,58,84" slow />
     </div>
   </div>
 </template>
 
 <script>
+import Waves from '@/components/common/Waves';
+
 export default {
   computed: {
     title: () => BRAND_CONFIG.TITLE,
     logo: () => BRAND_CONFIG.LOGO_FULL
-  }
+  },
+  components: { Waves }
 };
 </script>
 
@@ -28,28 +34,39 @@ export default {
   padding-top: 5%;
 }
 
-.auth-panel {
-  width: 26.5rem;
+.auth-container .auth-panel.v-sheet {
+  width: 26rem;
   margin: auto;
-  background-color: #fff;
-  border-radius: 4px;
-  border-top: 4px solid var(--v-primary-darken1);
+  background: #eee;
+  border: 1px dashed #777;
+  transition: all 0.2s cubic-bezier(0.25, 0.8, 0.25, 1);
+
+  &:hover {
+    background: #f4f4f4;
+  }
 
   .auth-header {
     padding-bottom: 0;
 
     .logo {
-      width: 2.75rem;
+      max-width: 3.5rem;
     }
   }
 
   .auth-body {
-    padding: 0.25rem 1.5rem 0.875rem;
+    padding: 0.25rem 1.5rem 2rem;
 
     a {
       color: inherit;
       font-weight: 400;
     }
   }
+}
+
+.auth-container .waves-container {
+  position: absolute;
+  bottom: 0;
+  left: 0;
+  width: 100%;
 }
 </style>
