02b7ad09fcc83c2ea90e95684c179b1a957fbf6d
Miro Dojkic <mdojkic@gmail.com>
2020-11-19 02:30:58 +0100

    Refactor preview overlay ♻️

---
+++ client/components/common/tce-core/PreviewOverlay.vue
@@ -0,0 +1,28 @@
+<template functional>
+  <div class="overlay d-flex justify-center align-center">
+    <div class="message grey--text text--lighten-2">Click to preview</div>
+  </div>
+</template>
+
+<script>
+export default { name: 'tce-preview-overlay' };
+</script>
+
+<style lang="scss" scoped>
+.overlay {
+  position: absolute;
+  z-index: 3;
+  width: 100%;
+  height: 100%;
+  background-color: #111;
+  opacity: 0.9;
+
+  &:hover {
+    cursor: pointer;
+  }
+
+  .message {
+    font-size: 1.125rem;
+  }
+}
+</style>
