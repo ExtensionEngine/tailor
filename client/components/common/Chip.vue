e6d1d680b0e18c7c681bbf407ac56dfc7d6e5183
Ivo Kovacevic <ikovacevic96k@gmail.com>
2020-11-20 15:34:14 +0100

    Add custom chip component

---
+++ client/components/common/Chip.vue
@@ -0,0 +1,18 @@
+<template>
+  <v-chip v-on="$listeners" v-bind="$attrs" class="chip">
+    <slot></slot>
+  </v-chip>
+</template>
+
+<script>
+export default {
+  name: 'custom-chip',
+  inheritAttrs: false
+};
+</script>
+
+<style lang="scss" scoped>
+.chip::before {
+  display: none;
+}
+</style>
