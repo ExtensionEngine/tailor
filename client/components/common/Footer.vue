61bd50507cc68a4a4d8210f66f95b5a21677afd5
Damir Perisic <underscope@gmail.com>
2020-11-26 15:46:33 +0100

    Added compact logo to app footer

--- client/components/common/Footer.vue
+++ client/components/common/Footer.vue
@@ -1,10 +1,10 @@
 <template>
   <v-footer height="46" color="blue-grey darken-4" absolute>
-    <waves class="waves" />
     <v-row justify="center" no-gutters>
       <v-col
         cols="12"
         class="body-2 grey--text text--lighten-4">
+        <img :src="logo" width="30px" height="30px" alt="Logo" class="py-1 mx-1">
         <v-chip
           color="grey lighten-4"
           label
@@ -21,7 +21,6 @@
 
 <script>
 import { codename, version } from '@/../package.json';
-import Waves from '@/components/common/Waves';
 
 const capitalize = str => str.charAt(0).toUpperCase() + str.substr(1);
 
@@ -29,10 +28,8 @@ export default {
   name: 'app-footer',
   computed: {
     version: () => version,
-    codename: () => capitalize(codename)
-  },
-  components: {
-    Waves
+    codename: () => capitalize(codename),
+    logo: () => BRAND_CONFIG.LOGO_COMPACT
   }
 };
 </script>
@@ -40,11 +37,5 @@ export default {
 <style lang="scss" scoped>
 .v-footer {
   box-shadow: 0 -1px 1px 0 rgba(0,0,0,0.2);
-}
-
-.waves {
-  position: absolute;
-  left: 0;
-  width: 100%;
 }
 </style>
