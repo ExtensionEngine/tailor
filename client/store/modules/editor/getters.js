c5150105e374a1fb4115fc6d0e8b29d1a1cf550e
Damir Perisic <underscope@gmail.com>
2020-11-27 16:10:06 +0100

    Added selectedElements getter

--- client/store/modules/editor/getters.js
+++ client/store/modules/editor/getters.js
@@ -4,7 +4,10 @@ import flatMap from 'lodash/flatMap';
 import get from 'lodash/get';
 import { getDescendants as getDeepChildren } from 'utils/activity';
 import { getSupportedContainers } from 'shared/activities';
+import map from 'lodash/map';
 import reduce from 'lodash/reduce';
+import uniqBy from 'lodash/uniqBy';
+import without from 'lodash/without';
 
 export const activity = (_state, _getters, { route, repository }) => {
   const id = parseInt(get(route, 'params.activityId'), 10);
@@ -30,4 +33,23 @@ export const contentContainers = (_state, getters, { repository }) => {
     acc.push(parent, ...getDeepChildren(activities, parent));
     return acc;
   }, []);
+};
+
+export const selectedElements = (_state, { activity }, rootState, rootGetters) => {
+  if (!activity) return [];
+  const {
+    auth: { user },
+    repository: { userTracking: { users: userTracking } }
+  } = rootState;
+  const entityState = rootGetters['repository/userTracking/activityByEntity'].activity;
+  const activeUserIds = without(map(entityState[activity.id], 'id'), user.id);
+  if (!activeUserIds.length) return [];
+  const selections = Object.values(userTracking)
+    .filter(it => activeUserIds.includes(it.id))
+    .reduce((acc, { contexts = [], ...user }) => [
+      ...acc,
+      ...contexts.map(it => ({ ...user, ...it }))
+    ], [])
+    .filter(it => it.elementId && (it.activityId === activity.id));
+  return uniqBy(selections, it => `${it.elementId}-${it.id}`);
 };
