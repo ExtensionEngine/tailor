4697b14b5e7bc57acb49f5068d1027946c370614
Ivo Kovacevic <ikovacevic96k@gmail.com>
2020-12-02 13:01:38 +0100

    Initial pulumi setup

---
+++ pulumi/index.js
@@ -0,0 +1,9 @@
+'use strict';
+
+const aws = require('@pulumi/aws');
+
+// Create an AWS resource (S3 Bucket)
+const bucket = new aws.s3.Bucket('my-bucket');
+
+// Export the name of the bucket
+exports.bucketName = bucket.id;
