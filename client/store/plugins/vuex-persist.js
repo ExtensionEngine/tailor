c62a083802a9bcc76bc9bc72098b38c9ddeede24
Luka Saric <rpg.ari7@gmail.com>
2020-12-02 15:52:38 +0100

    Migrate seen state on boot :rocket:

--- client/store/plugins/vuex-persist.js
+++ client/store/plugins/vuex-persist.js
@@ -7,14 +7,17 @@ const OBSERVED_MUTATIONS = [
   'repository/comments/markSeenComments'
 ];
 
+const STORAGE_KEY = 'TAILOR_APP_STATE';
+migrateSeenState();
+
 export default new VuexPersistence({
-  key: 'TAILOR_APP_STATE',
+  key: STORAGE_KEY,
   reducer: ({ auth, repository }) => ({
     auth,
     repository: {
       comments: {
         seenByActivity: repository.comments.seen.activity,
-        seen: migrateSeenState(repository)
+        seen: repository.comments.seen
       }
     }
   }),
@@ -22,10 +25,11 @@ export default new VuexPersistence({
   filter: mutation => OBSERVED_MUTATIONS.includes(mutation.type)
 }).plugin;
 
-function migrateSeenState(repository) {
-  const storage = window.localStorage.getItem('TAILOR_APP_STATE');
-  const { seenByActivity } = JSON.parse(storage).repository.comments;
-  const { seen } = repository.comments;
-  const activity = { ...seenByActivity, ...seen.activity };
-  return { activity, contentElement: seen.contentElement };
+function migrateSeenState() {
+  const storage = window.localStorage;
+  const state = JSON.parse(storage.getItem(STORAGE_KEY));
+  const { seenByActivity, seen } = state.repository.comments;
+  if (seenByActivity) state.repository.comments = { seen: seenByActivity };
+  if (seen) state.repository.comments = { seenByActivity: seen };
+  storage.setItem(STORAGE_KEY, JSON.stringify(state));
 }
