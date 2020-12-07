0a57cfccb30d0a76952c958878b67a718a70c2fb
Miro Dojkic <mdojkic@gmail.com>
2020-12-07 02:06:36 +0100

    Remove type query param 🔥

--- server/shared/storage/proxy/mw.js
+++ server/shared/storage/proxy/mw.js
@@ -9,11 +9,7 @@ const router = require('express').Router();
 function getFile(req, res) {
   const hasValidCookies = proxy.verifyCookies(req.cookies);
   if (!hasValidCookies) return res.status(403).end();
-  const { returnType } = req.query;
   const key = req.params[0];
-  if (returnType === 'url') {
-    return storage.getFileUrl(key).then(url => res.json({ url }));
-  }
   const readStream = storage.createReadStream(key);
   res.setHeader('Content-Type', mime.lookup(key));
   readStream.pipe(res);
