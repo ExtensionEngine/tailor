72a22b916230f47a2a24b0ad46fe90d3e858d01c
Miro Dojkic <mdojkic@gmail.com>
2020-12-07 20:39:11 +0100

    Revert GET /assets endpoint deletion ⏪

--- server/shared/storage/storage.controller.js
+++ server/shared/storage/storage.controller.js
@@ -1,23 +1,25 @@
 'use strict';
 
+const { getFileUrl, saveFile } = require('./');
 const { readFile, sha256 } = require('./util');
-const config = require('../../../config/server');
+const config = require('../../../config/server').storage;
 const path = require('path');
-const storage = require('./');
+
+function getUrl(req, res) {
+  const { query: { key } } = req;
+  return getFileUrl(key).then(url => res.json({ url }));
+}
 
 async function upload({ file }, res) {
   const buffer = await readFile(file);
   const hash = sha256(file.originalname, buffer);
   const extension = path.extname(file.originalname);
   const name = path.basename(file.originalname, extension).substring(0, 180).trim();
-  const key = path.join(config.storage.path, `${hash}___${name}${extension}`);
-  const headers = {
-    ContentType: file.mimetype,
-    ContentLength: Buffer.byteLength(buffer)
-  };
-  await storage.saveFile(key, buffer, headers);
-  const publicUrl = await storage.getFileUrl(key);
+  const key = path.join(config.path, `${hash}___${name}${extension}`);
+  const headers = { ContentType: file.mimetype };
+  await saveFile(key, buffer, headers);
+  const publicUrl = await getFileUrl(key);
   return res.json({ key, url: `storage://${key}`, publicUrl });
 }
 
-module.exports = { upload };
+module.exports = { getUrl, upload };
--- server/shared/storage/storage.router.js
+++ server/shared/storage/storage.router.js
@@ -3,9 +3,11 @@
 const ctrl = require('./storage.controller');
 const multer = require('multer');
 const router = require('express').Router();
+
 const upload = multer({ storage: multer.memoryStorage() });
 
 router
+  .get('/', ctrl.getUrl)
   .post('/', upload.single('file'), ctrl.upload);
 
 module.exports = {
