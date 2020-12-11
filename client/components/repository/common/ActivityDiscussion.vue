80c5d48f374109e9f2680c2b762b3ae90ea6db0a
Luka Saric <rpg.ari7@gmail.com>
2020-12-11 12:05:50 +0100

    Added outline discussion seen marker :rocket:

--- client/components/repository/common/ActivityDiscussion.vue
+++ client/components/repository/common/ActivityDiscussion.vue
@@ -1,9 +1,21 @@
 <template>
   <div v-intersect="onIntersect" class="activity-discussion">
+    <transition name="slide-fade">
+      <v-btn
+        v-if="showSeenMarker"
+        @click="setLastSeenComment"
+        color="secondary"
+        text x-small
+        class="seen-marker">
+        <v-icon class="mr-1" x-small>mdi-check</v-icon>
+        Mark All as Seen
+      </v-btn>
+    </transition>
     <discussion
       @save="saveComment"
       @update="saveComment"
       @remove="remove"
+      @mark-seen="setLastSeenComment"
       v-bind="{ comments, user, showHeading, scrollTarget: 'editor' }"
       show-notifications show-all-comments />
   </div>
@@ -21,30 +33,37 @@ export default {
     activity: { type: Object, required: true },
     showHeading: { type: Boolean, default: false }
   },
-  data: () => ({ isVisible: false }),
+  data: () => ({ isVisible: false, showSeenMarker: false }),
   computed: {
-    ...mapGetters('repository/comments', ['getComments']),
+    ...mapGetters('repository/comments', ['getComments', 'getUnseenActivityComments']),
     ...mapState({ user: state => state.auth.user }),
     comments() {
       const comments = this.getComments({ activityId: this.activity.id });
       return orderBy(comments, 'createdAt', 'desc');
     },
+    unseenComments: vm => vm.getUnseenActivityComments(vm.activity),
     lastCommentAt: vm => new Date(get(vm.comments[0], 'createdAt', 0)).getTime()
   },
   methods: {
     ...mapActions('repository/comments', ['fetch', 'save', 'update', 'remove']),
     ...mapMutations('repository/comments', ['markSeenComments']),
-    saveComment(comment) {
+    async saveComment(comment) {
       const action = comment.id ? 'update' : 'save';
       const { activity, user: author } = this;
-      return this[action]({ ...comment, author, activityId: activity.id });
+      await this[action]({ ...comment, author, activityId: activity.id });
+      setTimeout(() => Object.assign(this, {
+        isVisible: true,
+        showSeenMarker: false
+      }), 2500);
     },
-    setLastSeenComment(timeout) {
+    setLastSeenComment(timeout = 0) {
       const { activity, lastCommentAt } = this;
       const payload = { activityUid: activity.uid, lastCommentAt };
       setTimeout(() => this.markSeenComments(payload), timeout);
+      this.showSeenMarker = false;
     },
     onIntersect(_entries, _observer, isIntersected) {
+      if (this.showHeading) return;
       this.isVisible = isIntersected;
     }
   },
@@ -56,6 +75,13 @@ export default {
     comments(val, oldVal) {
       if (!this.isVisible || val === oldVal) return;
       this.setLastSeenComment(2000);
+    },
+    unseenComments: {
+      immediate: true,
+      handler(comments) {
+        if (!this.showHeading || !comments.length) return;
+        setTimeout(() => (this.showSeenMarker = true), 2000);
+      }
     }
   },
   created() {
@@ -67,8 +93,20 @@ export default {
 
 <style lang="scss" scoped>
 .activity-discussion {
+  position: relative;
   margin: 1rem 0 1.75rem;
   padding: 0.375rem 1rem;
   border: 1px solid #bbb;
+
+  .seen-marker {
+    position: absolute;
+    top: 1.5625rem;
+    right: 8.5rem;
+  }
+
+  .slide-fade-enter, .slide-fade-leave-to {
+    transform: translateX(0.625rem);
+    opacity: 0;
+  }
 }
 </style>
