bbb2034a3538a4659cabe54621f254bede990dc2
Ivo Kovacevic <ikovacevic96k@gmail.com>
2020-12-16 21:26:59 +0100

    Lint content element hooks folder as commonjs

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   overrides: [{
     files: ['extensions/**'],
-    excludedFiles: ['extensions/**/util.js'],
+    excludedFiles: ['extensions/**/hooks/**/*'],
     parserOptions: {
       parser: '@babel/eslint-parser',
       sourceType: 'module'
