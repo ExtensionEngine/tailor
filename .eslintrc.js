6e545c3cff9147cdfd0ec2bc279fdf08dd261c4b
Ivo Kovacevic <ikovacevic96k@gmail.com>
2020-12-17 11:34:39 +0100

    Rename tce server folder from hooks to server

--- .eslintrc.js
+++ .eslintrc.js
@@ -14,7 +14,7 @@ module.exports = {
   },
   overrides: [{
     files: ['extensions/**'],
-    excludedFiles: ['extensions/**/hooks/**/*'],
+    excludedFiles: ['extensions/**/server/**/*'],
     parserOptions: {
       parser: '@babel/eslint-parser',
       sourceType: 'module'
