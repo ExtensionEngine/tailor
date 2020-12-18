aa9098db679953f3adfca6f28ec6d98f5e917d74
Luka Saric <rpg.ari7@gmail.com>
2020-12-18 19:44:11 +0100

    Minor condition correction

--- client/store/modules/repository/comments/getters.js
+++ client/store/modules/repository/comments/getters.js
@@ -14,7 +14,7 @@ export const getUnseenActivityComments = (state, _, { auth }) => activity => {
     const isCurrentActivityComment = it.activityId === activity.id;
     const isAuthor = it.authorId === auth.user.id;
     const createdAt = new Date(it.createdAt).getTime();
-    if (!isCurrentActivityComment || isAuthor || lastActivitySeen > createdAt) return;
+    if (!isCurrentActivityComment || isAuthor || lastActivitySeen >= createdAt) return;
     if (!it.contentElement) return true;
     const lastElementSeen = get(seen.contentElement, it.contentElement.uid, 0);
     return lastElementSeen < createdAt;
