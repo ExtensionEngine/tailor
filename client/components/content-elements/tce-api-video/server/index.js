635e2db4ebed929587bda0b6a8c4bf52966f51db
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-01-18 10:07:53 +0100

    Change property order in export object

--- client/components/content-elements/tce-api-video/server/index.js
+++ client/components/content-elements/tce-api-video/server/index.js
@@ -3,4 +3,4 @@
 const { options } = require('tce-apivideo/dist/tce-apivideo.umd');
 const server = require('tce-apivideo/dist/server/index.js');
 
-module.exports = { type: options.type, ...server };
+module.exports = { ...server, type: options.type };
