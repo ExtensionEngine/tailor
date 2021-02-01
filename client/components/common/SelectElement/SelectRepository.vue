8b804cee80edeaaf07006095ec8953428306221c
Marin Capeta <marin.capeta@gmail.com>
2021-02-01 12:46:55 +0100

    Repository selection component improvements

--- client/components/common/SelectElement/SelectRepository.vue
+++ client/components/common/SelectElement/SelectRepository.vue
@@ -15,10 +15,10 @@
 </template>
 
 <script>
+import debounce from 'lodash/debounce';
+import find from 'lodash/find';
 import loader from '@/components/common/loader';
 import repositoryApi from '@/api/repository';
-import throttle from 'lodash/throttle';
-import uniqBy from 'lodash/uniqBy';
 
 const SEARCH_MIN_LENGTH = 3;
 
@@ -27,21 +27,22 @@ export default {
   props: {
     repository: { type: Object, default: null }
   },
-  data: vm => ({
+  data: () => ({
     repositories: [],
     loading: false
   }),
   methods: {
     select(repository) {
-      if (this.repositories.includes(repository)) {
+      if (find(this.repositories, { id: repository.id })) {
         this.$emit('selected', repository);
       }
     },
-    fetchRepositories: throttle(loader(function (search) {
-      if (!search || search.length < SEARCH_MIN_LENGTH) return;
-      return repositoryApi.getRepositories({ search }).then(repos => {
-        this.repositories = uniqBy([...this.repositories, ...repos], 'id');
-      });
+    fetchRepositories: debounce(loader(function (search) {
+      if (!search || search.length < SEARCH_MIN_LENGTH) {
+        return (this.repositories = []);
+      }
+      return repositoryApi.getRepositories({ search })
+        .then(repositories => (this.repositories = repositories));
     }, 'loading'), 500)
   }
 };
