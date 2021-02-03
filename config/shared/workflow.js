2942ba2519b7c2f2fbc7c8b6f1108ad2480e7774
Miro Dojkic <mdojkic@gmail.com>
2021-02-03 10:16:16 +0100

    Fallback to first status as default 🔧

--- config/shared/workflow.js
+++ config/shared/workflow.js
@@ -27,7 +27,7 @@ function getPriority(id) {
 
 function getDefaultWorkflowStatus(id) {
   const { statuses } = getWorkflow(id);
-  const { id: status } = statuses.find(it => it.default);
+  const { id: status } = statuses.find(it => it.default) || statuses[0];
   const { id: priority } = priorities.find(it => it.default);
   return { status, priority };
 }
