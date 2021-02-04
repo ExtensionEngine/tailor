8bc0c0ee10b08bbad746be7d32141065e99405f9
Luka Saric <rpg.ari7@gmail.com>
2021-02-04 13:02:37 +0100

    Decoupled edited at column hydration

--- server/shared/database/migrations/20210125094759-add-edited_at_column_to_comment.js
+++ server/shared/database/migrations/20210125094759-add-edited_at_column_to_comment.js
@@ -1,16 +1,8 @@
 'use strict';
 
-const { Comment } = require('../');
-
 const TABLE_NAME = 'comment';
 const COLUMN_NAME = 'edited_at';
 
-exports.up = async (qi, { Op, col, DATE }) => {
-  await qi.addColumn(TABLE_NAME, COLUMN_NAME, { type: DATE });
-  const attributes = ['id', 'createdAt', 'updatedAt'];
-  const where = { createdAt: { [Op.ne]: col('updated_at') } };
-  return Comment.findAll({ where, attributes })
-    .map(comment => comment.update({ editedAt: comment.updatedAt }));
-};
+exports.up = (qi, { DATE }) => qi.addColumn(TABLE_NAME, COLUMN_NAME, { type: DATE });
 
 exports.down = qi => qi.removeColumn(TABLE_NAME, COLUMN_NAME);
---
+++ server/shared/database/migrations/20210204115515-hydrate-edited_at-column.js
@@ -0,0 +1,12 @@
+'use strict';
+
+const { Comment } = require('../');
+
+exports.up = (_, { Op, col }) => {
+  const attributes = ['id', 'createdAt', 'updatedAt'];
+  const where = { createdAt: { [Op.ne]: col('updated_at') } };
+  return Comment.findAll({ where, attributes })
+    .map(comment => comment.update({ editedAt: comment.updatedAt }));
+};
+
+exports.down = () => {};
