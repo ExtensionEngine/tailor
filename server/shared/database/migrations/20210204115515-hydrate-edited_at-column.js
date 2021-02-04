e2b9e81d53f866f040440293d19451f38eaf22ea
Luka Saric <rpg.ari7@gmail.com>
2021-02-04 13:41:14 +0100

    Coverted migration to raw query approach

--- server/shared/database/migrations/20210204115515-hydrate-edited_at-column.js
+++ server/shared/database/migrations/20210204115515-hydrate-edited_at-column.js
@@ -1,12 +1,24 @@
 'use strict';
 
-const { Comment } = require('../');
+const head = require('lodash/head');
 
-exports.up = (_, { Op, col }) => {
-  const attributes = ['id', 'createdAt', 'updatedAt'];
-  const where = { createdAt: { [Op.ne]: col('updated_at') } };
-  return Comment.findAll({ where, attributes })
-    .map(comment => comment.update({ editedAt: comment.updatedAt }));
+exports.up = async ({ sequelize }) => {
+  const selectSql = `
+    SELECT
+      id,
+      created_at AS "createdAt",
+      updated_at AS "updatedAt"
+    FROM
+      comment
+    WHERE
+      created_at != updated_at;
+  `;
+  const comments = head(await sequelize.query(selectSql, { raw: true }));
+  Promise.all(comments.map(({ id, updatedAt }) => {
+    const updateSql = 'UPDATE comment SET edited_at = :updatedAt WHERE id = :id';
+    const replacements = { id, updatedAt };
+    return sequelize.query(updateSql, { replacements });
+  }));
 };
 
 exports.down = () => {};
