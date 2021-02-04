900c033896b4c6048a66ee60c0f7e7509367139f
Luka Saric <rpg.ari7@gmail.com>
2021-02-04 12:17:16 +0100

    Migrate prev updatedAt data to editedAt

--- server/shared/database/migrations/20210125094759-add-edited_at_column_to_comment.js
+++ server/shared/database/migrations/20210125094759-add-edited_at_column_to_comment.js
@@ -1,8 +1,16 @@
 'use strict';
 
+const { Comment } = require('../');
+
 const TABLE_NAME = 'comment';
 const COLUMN_NAME = 'edited_at';
 
-exports.up = (qi, { DATE }) => qi.addColumn(TABLE_NAME, COLUMN_NAME, { type: DATE });
+exports.up = async (qi, { Op, col, DATE }) => {
+  await qi.addColumn(TABLE_NAME, COLUMN_NAME, { type: DATE });
+  const attributes = ['id', 'createdAt', 'updatedAt'];
+  const where = { createdAt: { [Op.ne]: col('updated_at') } };
+  return Comment.findAll({ where, attributes })
+    .map(comment => comment.update({ editedAt: comment.updatedAt }));
+};
 
 exports.down = qi => qi.removeColumn(TABLE_NAME, COLUMN_NAME);
