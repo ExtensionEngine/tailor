7f9982631ecd90684240b370c3490d4169575ce5
ikovac <ikovacevic96k@gmail.com>
2021-03-17 12:49:16 +0100

    Document env variables 📝

--- config/server/store.js
+++ config/server/store.js
@@ -3,12 +3,12 @@
 module.exports = {
   provider: process.env.STORE_PROVIDER || 'local',
   local: {
-    maxAge: parseInt(process.env.LOCAL_STORE_MAXAGE, 10) || 0
+    maxAge: parseInt(process.env.STORE_TTL, 10) || 0
   },
   redis: {
-    ttl: parseInt(process.env.REDIS_STORE_TTL, 10) || 0,
-    port: parseInt(process.env.REDIS_STORE_PORT, 10) || 6379,
-    host: process.env.REDIS_STORE_HOST || 'localhost',
-    password: process.env.REDIS_STORE_PASSWORD
+    ttl: parseInt(process.env.STORE_TTL, 10) || 0,
+    port: parseInt(process.env.REDIS_PORT, 10) || 6379,
+    host: process.env.REDIS_HOST || 'localhost',
+    password: process.env.REDIS_PASSWORD
   }
 };
