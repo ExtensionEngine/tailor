a7780c46df02403623b02802669da84d138cbac5
Luka Saric <rpg.ari7@gmail.com>
2021-04-02 14:58:52 +0200

    Polished user api urls :lipstick:

--- client/api/auth.js
+++ client/api/auth.js
@@ -1,45 +1,47 @@
+import path from 'path';
 import request from './request';
 
 const urls = {
-  login: '/users/login',
-  logout: '/users/logout',
-  forgotPassword: '/users/forgot-password',
-  resetPassword: '/users/reset-password',
-  validateResetToken: '/users/reset-token-validation',
-  profile: '/users/me',
-  changePassword: '/users/me/change-password'
+  root: '/users',
+  login: () => path.join(urls.root, 'login'),
+  logout: () => path.join(urls.root, 'logout'),
+  forgotPassword: () => path.join(urls.root, 'forgot-password'),
+  resetPassword: () => path.join(urls.root, 'reset-password'),
+  validateResetToken: () => path.join(urls.root, 'reset-token-validation'),
+  profile: () => path.join(urls.root, 'me'),
+  changePassword: () => path.join(urls.profile(), 'change-password')
 };
 
 function login(credentials) {
-  return request.base.post(urls.login, credentials);
+  return request.base.post(urls.login(), credentials);
 }
 
 function logout() {
-  return request.get(urls.logout);
+  return request.get(urls.logout());
 }
 
 function forgotPassword(email) {
-  return request.post(urls.forgotPassword, { email });
+  return request.post(urls.forgotPassword(), { email });
 }
 
 function resetPassword(token, password) {
-  return request.base.post(urls.resetPassword, { token, password });
+  return request.base.post(urls.resetPassword(), { token, password });
 }
 
-function validateResetToken(token = '') {
-  return request.base.post(urls.validateResetToken, { token });
+function validateResetToken(token) {
+  return request.base.post(urls.validateResetToken(), { token });
 }
 
 function changePassword(currentPassword, newPassword) {
-  return request.post(urls.changePassword, { currentPassword, newPassword });
+  return request.post(urls.changePassword(), { currentPassword, newPassword });
 }
 
 function getUserInfo() {
-  return request.base.get(urls.profile);
+  return request.base.get(urls.profile());
 }
 
 function updateUserInfo(userData) {
-  return request.patch(urls.profile, userData);
+  return request.patch(urls.profile(), userData);
 }
 
 export default {
