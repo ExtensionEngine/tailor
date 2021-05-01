ef86b88e72d980339a9838bb06c6163eb595efd6
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-05-01 11:52:59 +0200

    Use tapster as default request limiter store ✨

--- server/shared/request/mw.js
+++ server/shared/request/mw.js
@@ -1,11 +1,46 @@
 'use strict';
 
 const rateLimit = require('express-rate-limit');
+const Tapster = require('@extensionengine/tapster');
+const { provider, ...options } = require('../../../config/server').store;
 
 const DEFAULT_WINDOW_MS = 15 * 60 * 1000; // every 15 minutes
 
-function requestLimiter({ max = 10, windowMs = DEFAULT_WINDOW_MS, ...opts } = {}) {
-  return rateLimit({ max, windowMs, ...opts });
+class Store {
+  constructor() {
+    this.cache = new Tapster({
+      ...options[provider],
+      store: provider,
+      namespace: 'request-limiter'
+    });
+  }
+
+  async incr(key, cb) {
+    const record = await this.cache.has(key) ? await this.cache.get(key) : { hits: 0 };
+    const hits = record.hits + 1;
+    await this.cache.set(key, { ...record, hits });
+    cb(null, hits);
+  }
+
+  async decrement(key) {
+    const record = await this.cache.get(key);
+    if (!record) return;
+    const hits = record.hits - 1;
+    await this.cache.set(key, { ...record, hits });
+  }
+
+  resetKey(key) {
+    this.cache.delete(key);
+  }
+}
+
+function requestLimiter({
+  max = 10,
+  windowMs = DEFAULT_WINDOW_MS,
+  store = new Store(),
+  ...opts
+} = {}) {
+  return rateLimit({ max, windowMs, store, ...opts });
 }
 
 module.exports = { requestLimiter };
