171454cd537bf37f03f7a5527a34cc062498721e
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-05-03 11:14:19 +0200

    Rename req.key ♻️

--- server/user/mw.js
+++ server/user/mw.js
@@ -3,16 +3,16 @@
 const crypto = require('crypto');
 const { requestLimiter } = require('../shared/request/mw');
 
-const loginRequestLimiter = requestLimiter({ keyGenerator: req => req.key });
+const loginRequestLimiter = requestLimiter({ keyGenerator: req => req.userKey });
 
 function getKeyFromRequest(req, res, next) {
   const key = [req.ip, req.body.email].join(':');
-  req.key = crypto.createHash('sha256').update(key).digest('base64');
+  req.userKey = crypto.createHash('sha256').update(key).digest('base64');
   return next();
 }
 
 function resetLoginAttempts(req, res, next) {
-  return loginRequestLimiter.resetKey(req.key)
+  return loginRequestLimiter.resetKey(req.userKey)
     .then(() => next());
 }
 
