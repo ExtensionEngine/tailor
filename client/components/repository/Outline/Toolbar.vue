6b6fc953fce2e7b1868a601eb203c665102457d9
Ivo Kovacevic <ikovacevic96k@gmail.com>
2021-05-06 18:20:38 +0200

    Change color variant, lighten by 1 💄

--- client/components/repository/Outline/Toolbar.vue
+++ client/components/repository/Outline/Toolbar.vue
@@ -24,7 +24,7 @@
           @click="$emit('toggle:graph')"
           :input-value="isGraph"
           icon>
-          <v-icon :color="isGraph ? 'secondary lighten-1' : 'currentColor'">
+          <v-icon :color="isGraph ? 'secondary lighten-2' : 'currentColor'">
             mdi-graph-outline
           </v-icon>
         </v-btn>
