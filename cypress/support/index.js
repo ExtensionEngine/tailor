d14c5a66d31f0478ea9ea07c939b083b227738c9
underscope <underscope@gmail.com>
2021-05-10 15:09:12 +0200

    Installed testing-library

--- cypress/support/index.js
+++ cypress/support/index.js
@@ -14,7 +14,8 @@
 // ***********************************************************
 
 // Import commands.js using ES2015 syntax:
-import './commands'
+import '@testing-library/cypress/add-commands';
+import './commands';
 
 // Alternatively you can use CommonJS syntax:
 // require('./commands')
