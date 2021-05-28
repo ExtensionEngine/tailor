fd21ac4037ff959fabe8b9b76dfce1df90629814
underscope <underscope@gmail.com>
2021-05-28 13:26:43 +0200

    Confirmation dialog commands

--- cypress/support/commands.js
+++ cypress/support/commands.js
@@ -29,3 +29,22 @@
 Cypress.Commands.add('getApp', () => cy.window().then(win => win.__app__));
 Cypress.Commands.add('getStore', () => cy.getApp().then(app => app.$store));
 Cypress.Commands.add('getRoute', () => cy.getApp().then(app => app.$route));
+
+// Confirmation dialog actions
+Cypress.Commands.add('confirmAction', dialogTitle => {
+  return getDialogByTitle(dialogTitle)
+    .findByText(/confirm/i)
+    .click();
+});
+Cypress.Commands.add('cancelAction', dialogTitle => {
+  return getDialogByTitle(dialogTitle)
+    .findByText(/cancel/i)
+    .click();
+});
+
+const getDialogByTitle = dialogTitle => {
+  return cy.root()
+    .findAllByRole('document')
+    .filter(`:contains("${dialogTitle}")`)
+    .eq(0);
+};
