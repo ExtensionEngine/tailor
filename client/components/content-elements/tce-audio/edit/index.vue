117b16e4d5c2d8cfae0c4b72377a8712cc65d844
Zdravko Curic <zcuric@gmail.com>
2021-06-09 13:39:59 +0200

    Adjust z-index on tce-audio

--- client/components/content-elements/tce-audio/edit/index.vue
+++ client/components/content-elements/tce-audio/edit/index.vue
@@ -87,9 +87,16 @@ export default {
   justify-content: center;
   position: relative;
   min-height: 4.5rem;
+  z-index: 6;
 
-  ::v-deep .element-placeholder {
-    padding: 0.5rem !important;
+  ::v-deep {
+    .element-placeholder {
+      padding: 0.5rem !important;
+    }
+
+    .plyr__menu__container {
+      background: #fff;
+    }
   }
 
   .audio-container {
