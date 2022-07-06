5fde387b7a2a7e76e15014a949668722261c48ae
underscope <underscope@gmail.com>
2022-07-06 20:31:04 +0200

    Update vite config
    - Lint
    - Provide umd bundle

--- packages/core-components/vite.config.js
+++ packages/core-components/vite.config.js
@@ -1,7 +1,7 @@
-import { defineConfig } from 'vite'
-import { createVuePlugin } from 'vite-plugin-vue2'
-import path from 'path';
+import { createVuePlugin } from 'vite-plugin-vue2';
+import { defineConfig } from 'vite';
 import { fileURLToPath } from 'url';
+import path from 'path';
 
 const _filename = fileURLToPath(import.meta.url);
 const _dirname = path.dirname(_filename);
@@ -14,30 +14,20 @@ const config = {
     lib: {
       entry: './src/index.js',
       name: 'TailorCoreComponents',
-      formats: ['cjs', 'es']
+      formats: ['es', 'cjs', 'umd']
     }
   },
   resolve: {
     alias: [{
       find: '@/',
-      replacement: path.join(_dirname, './src/'),
+      replacement: path.join(_dirname, './src/')
     }]
   },
-  plugins: [
-    createVuePlugin(),
-  ],
+  plugins: [createVuePlugin()],
   rollupOptions: {
     // Externalize deps that shouldn't be bundled
-    external: ['vue', 'vuetify', 'vee-validate'],
-    output: {
-      // Provide global variables to use in the UMD build
-      // for externalized deps
-      globals: {
-        vue: 'Vue',
-        vuetify: 'Vuetify'
-      }
-    }
+    external: ['vue', 'vuetify', 'vee-validate']
   }
-}
+};
 
 export default () => defineConfig(config);
