9d621c34e8209e06655e4db66ead8e673821b760
underscope <underscope@gmail.com>
2022-07-12 12:24:21 +0200

    Migrate cypress config files

---
+++ cypress.config.js
@@ -0,0 +1,15 @@
+'use strict';
+
+const { defineConfig } = require('cypress');
+
+module.exports = defineConfig({
+  viewportWidth: 1400,
+  viewportHeight: 800,
+  e2e: {
+    baseUrl: 'http://localhost:3000',
+    specPattern: 'cypress/e2e/**/*.spec.js',
+    setupNodeEvents(on, config) {
+      return require('./cypress/plugins/index.js')(on, config);
+    }
+  }
+});
