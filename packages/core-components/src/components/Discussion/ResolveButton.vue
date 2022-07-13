11743017d0dd19dc9924b7c05e694562a6720185
Marko Jevtic <marjevtic@gmail.com>
2022-07-13 13:07:01 +0200

    Fix icon code; Bump the version of @tailor-cms/core-components

--- packages/core-components/src/components/Discussion/ResolveButton.vue
+++ packages/core-components/src/components/Discussion/ResolveButton.vue
@@ -8,7 +8,7 @@
           small text
           class="px-1">
           <v-icon size="24" color="teal accent-4" class="mr-2">
-            mdi-check-box-outline
+            mdi-checkbox-outline
           </v-icon>
           Resolve All
         </v-btn>
