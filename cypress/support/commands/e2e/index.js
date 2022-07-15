7ca95649707e5254b3ff9840270662fb068f4086
underscope <underscope@gmail.com>
2022-07-15 08:27:01 +0200

    Add cmd example to the root

--- cypress/support/commands/e2e/index.js
+++ cypress/support/commands/e2e/index.js
@@ -1,17 +1,28 @@
-// ***********************************************************
-// This example support/index.js is processed and
-// loaded automatically before your test files.
+// ***********************************************
+// This example shows you how to
+// create various custom commands and overwrite
+// existing commands.
 //
-// This is a great place to put global configuration and
-// behavior that modifies Cypress.
+// For more comprehensive examples of custom
+// commands please read more here:
+// https://on.cypress.io/custom-commands
+// ***********************************************
 //
-// You can change the location of this file or turn off
-// automatically serving support files with the
-// 'supportFile' configuration option.
 //
-// You can read more here:
-// https://on.cypress.io/configuration
-// ***********************************************************
+// -- This is a parent command --
+// Cypress.Commands.add('login', (email, password) => { ... })
+//
+//
+// -- This is a child command --
+// Cypress.Commands.add('drag', { prevSubject: 'element'}, (subject, options) => { ... })
+//
+//
+// -- This is a dual command --
+// Cypress.Commands.add('dismiss', { prevSubject: 'optional'}, (subject, options) => { ... })
+//
+//
+// -- This will overwrite an existing command --
+// Cypress.Commands.overwrite('visit', (originalFn, url, options) => { ... })
 
 import '@testing-library/cypress/add-commands';
 import './api';
