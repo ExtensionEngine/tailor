096747d148c6afd0b80b977f2ac26b85402c89a9
underscope <underscope@gmail.com>
2022-07-15 15:09:57 +0200

    Updated command location

--- cypress.config.js
+++ cypress.config.js
@@ -8,6 +8,7 @@ module.exports = defineConfig({
   e2e: {
     baseUrl: 'http://localhost:3000',
     specPattern: 'cypress/e2e/**/*.cy.js',
+    supportFile: 'cypress/support/e2e/index.js',
     setupNodeEvents(on, config) {
       return require('./cypress/plugins/index.js')(on, config);
     }
