aed11a3b14a12fddc74800e29c0c55214e8fe121
underscope <underscope@gmail.com>
2022-07-16 23:43:59 +0200

    :lipstick:

--- cypress/support/e2e/repository.js
+++ cypress/support/e2e/repository.js
@@ -2,11 +2,10 @@ const generateName = () => `Test repository - ${(new Date).getTime()}`;
 
 Cypress.Commands.add('createRepository', (name = generateName()) => {
   return cy.getTestSchema().then(schema => {
-    const payload = {
+    return cy.getStore().invoke('dispatch', 'repositories/create', {
       schema: schema.id,
       name,
       description: 'Test repository'
-    };
-    return cy.getStore().invoke('dispatch', 'repositories/create', payload);
+    });
   });
 });
