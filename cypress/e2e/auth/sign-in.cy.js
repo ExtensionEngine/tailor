f0c7ca148e260a1cfa23ffa66b5acebf54fa68e6
underscope <underscope@gmail.com>
2022-07-17 10:41:50 +0200

    Reuse route based location check

--- cypress/e2e/auth/sign-in.cy.js
+++ cypress/e2e/auth/sign-in.cy.js
@@ -1,8 +1,3 @@
-const assertHomepageAccess = () => {
-  cy.location()
-    .should(location => expect(location.hash).to.eq('#/'));
-};
-
 describe('Sign in view', () => {
   beforeEach(() => cy.visit('/'));
 
@@ -11,14 +6,15 @@ describe('Sign in view', () => {
       .type(Cypress.env('USERNAME'));
     cy.findByLabelText(/password/i)
       .type(Cypress.env('PASSWORD'));
-    cy.findByText(/Log In$/i)
+    cy.findByRole('button', { name: /Log In$/i })
       .click();
-    assertHomepageAccess();
+    cy.visit('/');
+    cy.assertRoute('catalog');
   });
 
   it('should sign in an existing user by dispatching an action', () => {
     cy.login();
     cy.visit('/');
-    assertHomepageAccess();
+    cy.assertRoute('catalog');
   });
 });
