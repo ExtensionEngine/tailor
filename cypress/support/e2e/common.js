ed74ceeb372520e0ef19959f742787840db364be
underscope <underscope@gmail.com>
2022-07-21 23:00:19 +0200

    Expose app router via command

--- cypress/support/e2e/common.js
+++ cypress/support/e2e/common.js
@@ -1,6 +1,7 @@
 // App instance and routing
 Cypress.Commands.add('getApp', () => cy.window().its('__app__'));
 Cypress.Commands.add('getStore', () => cy.getApp().its('$store'));
+Cypress.Commands.add('getRouter', () => cy.getApp().its('$router'));
 Cypress.Commands.add('getRoute', () => cy.getApp().its('$route'));
 Cypress.Commands.add('getRouteName', () => cy.getRoute().its('name'));
 Cypress.Commands.add('assertRoute', name => cy.getRouteName().should('eq', name));
