ef8f603b9358182bdb2ca7faa2765b20d89cefde
underscope <underscope@gmail.com>
2022-07-22 13:44:50 +0200

    Update root activity creation
    - Use extracted fun
    - Update the desc

--- cypress/e2e/repository/structure/root-activity-creation.cy.js
+++ cypress/e2e/repository/structure/root-activity-creation.cy.js
@@ -1,16 +1,11 @@
+import {
+  createRootActivity,
+  generateActivityName,
+  getRootActivityDialog
+} from './utils';
+
 const ROOT_ACTIVIY_TYPES = ['Module', 'Page'];
 const CHILD_ACTIVIY_TYPES = ['Lesson', 'Knowledge check'];
-
-const generateActivityName = type => `${type} - ${(new Date()).getTime()}`;
-
-function createActivity(name, type) {
-  cy.findByRole('button', { name: /add/i }).click();
-  cy.findByRole('dialog').within(() => {
-    cy.vSelect('Type', type);
-    cy.findByLabelText('Name').type(`${name}{enter}`);
-    cy.findByRole('button', { name: /create/i }).click();
-  });
-}
 
 describe('ability to create root activities', () => {
   before(function () {
@@ -27,17 +22,17 @@ describe('ability to create root activities', () => {
   });
 
   ROOT_ACTIVIY_TYPES.forEach(type => {
-    it(`create a "${type}" using the add button`, function () {
+    it(`should create a "${type}" using the add button`, function () {
       const name = generateActivityName(type);
-      createActivity(name, type);
+      createRootActivity(name, type);
       cy.findAllByText(name);
     });
   });
 
   CHILD_ACTIVIY_TYPES.forEach(type => {
-    it(`not able to create a "${type}" using the add button`, () => {
-      cy.findByRole('button', { name: /add/i }).click();
-      cy.findByRole('dialog').within(() => {
+    it(`should not be able to create a "${type}" using the add button`, () => {
+      cy.findByTestId('repository__createRootActivityBtn').click();
+      getRootActivityDialog().within(() => {
         cy.findByLabelText('Type').click();
         cy.findByRole('option', { name: type }).should('not.exist');
       });
