204d4d0b62386bd3d9c9d39b3b197d38822c53a5
underscope <underscope@gmail.com>
2022-07-22 13:36:30 +0200

    Add command for outline activity retrieval

--- cypress/support/e2e/repository.js
+++ cypress/support/e2e/repository.js
@@ -1,3 +1,5 @@
+const toTestIdAttr = val => `[data-testid="${val}"]`;
+
 const generateName = () => `Test repository - ${(new Date()).getTime()}`;
 
 Cypress.Commands.add('createRepository', (name = generateName()) => {
@@ -13,4 +15,8 @@ Cypress.Commands.add('createRepository', (name = generateName()) => {
 Cypress.Commands.add('openRepository', repositoryId => {
   return cy.getRouter()
     .then(router => router.push({ name: 'repository', params: { repositoryId } }));
+});
+
+Cypress.Commands.add('findActivityItem', name => {
+  return cy.contains(toTestIdAttr('repository__structureActivity'), name);
 });
