71fefb39ad58e9e3a0dd66f45f8cef48b9db0681
Marko Lovric <lovric.split@gmail.com>
2022-07-28 14:19:28 +0200

    Replaced generate repository name function

--- cypress/support/e2e/repository.js
+++ cypress/support/e2e/repository.js
@@ -1,6 +1,6 @@
-export const generateRepositoryName = () => `Test repository - ${(new Date()).getTime()}`;
+const chance = require('chance').Chance();
 
-Cypress.Commands.add('createRepository', (name = generateRepositoryName()) => {
+Cypress.Commands.add('createRepository', (name = chance.sentence({ words: 5 })) => {
   return cy.getTestSchema().then(schema => {
     return cy.getStore().invoke('dispatch', 'repositories/create', {
       schema: schema.id,
