54e9815ccd8669deb02d6e65f127986101f4a7c7
Josip Torić <jtoric@extensionengine.com>
2023-07-13 17:14:36 +0200

    Update tce jodit

--- client/components/content-elements/tce-jodit/index.js
+++ client/components/content-elements/tce-jodit/index.js
@@ -1,5 +1,5 @@
-import '@extensionengine/tce-jodit/dist/tce-jodit.css';
-import { Edit, options, Toolbar } from '@extensionengine/tce-jodit/dist/tce-jodit.umd';
+import '@extensionengine/tce-jodit/tce-jodit.css';
+import { Edit, options, Toolbar } from '@extensionengine/tce-jodit';
 
 export default {
   ...options,
