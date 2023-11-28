f7ac33edf89c1f02371456404bcee2acbd9a0f07
underscope <underscope@gmail.com>
2023-11-28 09:31:11 +0100

    Enable routes for integration user
    - If flag has been set

--- server/repository/index.js
+++ server/repository/index.js
@@ -10,6 +10,7 @@ import path from 'node:path';
 import processQuery from '../shared/util/processListQuery.js';
 import proxy from './proxy.js';
 import proxyMw from '../shared/storage/proxy/mw.js';
+import roleConfig from '../../config/shared/role.js';
 import storage from './storage.js';
 
 /* eslint-disable */
@@ -17,6 +18,7 @@ import activity from '../activity/index.js';
 import comment from '../comment/index.js';
 import revision from '../revision/index.js';
 import contentElement from '../content-element/index.js';
+const { user: role } = roleConfig;
 import storageRouter from '../shared/storage/storage.router.js';
 /* eslint-enable */
 
@@ -24,12 +26,19 @@ const { Repository } = db;
 const router = express.Router();
 const { setSignedCookies } = proxyMw(storage, proxy);
 
+const {
+  EXTERNAL_ACCESS_MANAGEMENT: isExternalAccessManagement
+} = process.env;
+
+const authorizeAdminUser = isExternalAccessManagement
+  ? authorize(role.INTEGRATION)
+  : authorize();
 // NOTE: disk storage engine expects an object to be passed as the first argument
 // https://github.com/expressjs/multer/blob/6b5fff5/storage/disk.js#L17-L18
 const upload = multer({ storage: multer.diskStorage({}) });
 
 router
-  .post('/import', authorize(), upload.single('archive'), ctrl.import);
+  .post('/import', authorizeAdminUser, upload.single('archive'), ctrl.import);
 
 router
   .param('repositoryId', getRepository)
@@ -46,15 +55,15 @@ router.route('/:repositoryId')
 
 router
   .post('/:repositoryId/pin', ctrl.pin)
-  .post('/:repositoryId/clone', authorize(), ctrl.clone)
+  .post('/:repositoryId/clone', authorizeAdminUser, ctrl.clone)
   .post('/:repositoryId/publish', ctrl.publishRepoInfo)
-  .get('/:repositoryId/users', ctrl.getUsers)
+  .get('/:repositoryId/users', authorizeAdminUser, ctrl.getUsers)
   .get('/:repositoryId/export/setup', ctrl.initiateExportJob)
   .post('/:repositoryId/export/:jobId', ctrl.export)
-  .post('/:repositoryId/users', ctrl.upsertUser)
-  .delete('/:repositoryId/users/:userId', ctrl.removeUser)
-  .post('/:repositoryId/tags', ctrl.addTag)
-  .delete('/:repositoryId/tags/:tagId', ctrl.removeTag);
+  .post('/:repositoryId/users', authorizeAdminUser, ctrl.upsertUser)
+  .delete('/:repositoryId/users/:userId', authorizeAdminUser, ctrl.removeUser)
+  .post('/:repositoryId/tags', authorizeAdminUser, ctrl.addTag)
+  .delete('/:repositoryId/tags/:tagId', authorizeAdminUser, ctrl.removeTag);
 
 mount(router, '/:repositoryId', feed);
 mount(router, '/:repositoryId', activity);
