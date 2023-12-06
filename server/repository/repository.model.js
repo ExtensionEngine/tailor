885b9a317ce0ab2163e49c66edc107a77d43e354
underscope <underscope@gmail.com>
2023-12-06 09:36:11 +0100

    💅

--- server/repository/repository.model.js
+++ server/repository/repository.model.js
@@ -70,7 +70,6 @@ class Repository extends Model {
       foreignKey: { name: 'repositoryId', field: 'repository_id' }
     });
     this.hasMany(RepositoryTag, {
-      as: 'repositoryTags',
       foreignKey: { name: 'repositoryId', field: 'repository_id' }
     });
     this.belongsToMany(Tag, {
