04773c55db5a6ccc736742892f3d239e1a71365e
underscope <underscope@gmail.com>
2023-12-08 16:09:57 +0100

    Add tag CRUD

--- server/tag/index.js
+++ server/tag/index.js
@@ -1,10 +1,39 @@
+import { authorize } from '../shared/auth/mw.js';
+import { createError } from '../shared/error/helpers.js';
 import ctrl from './tag.controller.js';
+import db from '../shared/database/index.js';
 import express from 'express';
+import { NOT_FOUND } from 'http-status-codes';
+import roleConfig from '../../config/shared/role.js';
 
 const router = express.Router();
+const { Tag } = db;
+const { user: role } = roleConfig;
+
+const authorizeUser = authorize(role.INTEGRATION);
 
 router
-  .get('/', ctrl.list);
+  .get('/', ctrl.list)
+  .post('/', authorizeUser, ctrl.create);
+
+router
+  .param('tagId', getTag)
+  .use('/:tagId', authorizeUser);
+
+router.route('/:tagId')
+  .get(ctrl.get)
+  .patch(ctrl.patch)
+  .delete(ctrl.remove);
+
+function getTag(req, _res, next, tagId) {
+  return Tag
+    .findByPk(tagId, { paranoid: false })
+    .then(tag => tag || createError(NOT_FOUND, 'Tag not found'))
+    .then(tag => {
+      req.tag = tag;
+      next();
+    });
+}
 
 export default {
   path: '/tags',
--- server/tag/tag.controller.js
+++ server/tag/tag.controller.js
@@ -1,4 +1,6 @@
 import db from '../shared/database/index.js';
+import { NO_CONTENT } from 'http-status-codes';
+import pick from 'lodash/pick.js';
 import yn from 'yn';
 
 const { Tag } = db;
@@ -10,6 +12,34 @@ async function list({ user, query: { associated } }, res) {
   return res.json({ data: tags });
 }
 
+async function get({ tag }, res) {
+  return res.json({ data: tag });
+}
+
+function create({ body }, res) {
+  const attrs = ['name', 'isAccessTag'];
+  const payload = pick(body, attrs);
+  return Tag.create(payload)
+    .then(data => res.json({ data }));
+}
+
+function patch({ tag, body }, res) {
+  const attrs = ['name', 'isAccessTag'];
+  const payload = pick(body, attrs);
+  return tag.update(payload)
+    .then(tag => tag.reload())
+    .then(data => res.json({ data }));
+}
+
+function remove({ tag }, res) {
+  return tag.destroy()
+    .then(data => res.status(NO_CONTENT).end());
+}
+
 export default {
-  list
+  list,
+  get,
+  create,
+  patch,
+  remove
 };
