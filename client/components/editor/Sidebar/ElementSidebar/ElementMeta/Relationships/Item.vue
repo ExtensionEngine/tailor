a462017eeba95cee157d3853a5ab772da28900ac
rkusan00 <renato.kusanovic@gmail.com>
2024-01-27 14:51:13 +0100

    Store linked elements

--- client/components/editor/Sidebar/ElementSidebar/ElementMeta/Relationships/Item.vue
+++ client/components/editor/Sidebar/ElementSidebar/ElementMeta/Relationships/Item.vue
@@ -44,7 +44,7 @@
 </template>
 
 <script>
-import { mapGetters } from 'vuex';
+import { mapGetters, mapMutations } from 'vuex';
 import { mapRequests } from '@extensionengine/vue-radio';
 import pluralize from 'pluralize';
 import { SelectElement } from '@tailor-cms/core-components';
@@ -78,6 +78,7 @@ export default {
   },
   methods: {
     ...mapRequests('app', ['showConfirmationModal']),
+    ...mapMutations('repository/contentElements', { save: 'fetch' }),
     removeAll() {
       let label = this.label.toLowerCase();
       label = this.multiple ? pluralize(label) : label;
@@ -88,6 +89,7 @@ export default {
       });
     },
     select(elements) {
+      this.save(elements);
       const items = elements.map(it => {
         if (!it.activity) return it;
         const { id, activity, activityId: containerId } = it;
